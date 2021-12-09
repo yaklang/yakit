d8d818b9760e9b877e0d19d257716babd0a88a50
nongiht <378099757@qq.com>
2021-12-09 14:55:43 +0800

    调整：基础爬虫页面更改判断

--- app/renderer/src/main/src/pages/yakitStore/viewers/base.tsx
+++ app/renderer/src/main/src/pages/yakitStore/viewers/base.tsx
@@ -97,7 +97,7 @@ export const PluginResultUI: React.FC<PluginResultUIProp> = (props) => {
                     >
                         <Timeline pending={loading} style={{marginTop: 10, marginBottom: 10}}>
                             {(results || []).filter(i => {
-                                return i.level.startsWith("json-feature") || i.level.startsWith("feature-")
+                                return !(i.level.startsWith("json-feature") || i.level.startsWith("feature-"))
                             }).splice(0, 25).map((e,index) => {
                                 return <Timeline.Item key={index} color={LogLevelToCode(e.level)}>
                                     <YakitLogFormatter data={e.data} level={e.level} timestamp={e.timestamp}/>
