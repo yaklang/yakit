f6b1dfeba85bc40b30ce8b2a2452be3d4a9222ed
nongiht <378099757@qq.com>
2021-12-30 16:54:48 +0800

    fix: 修复无效引用css报错

--- app/renderer/src/main/src/components/PerformanceDisplay.tsx
+++ app/renderer/src/main/src/components/PerformanceDisplay.tsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react"
 import { Typography } from "antd"
 import { Sparklines, SparklinesCurve } from "react-sparklines"
 
-import "./PerformanceDisplay.css"
-
 const { ipcRenderer } = window.require("electron")
 
 const { Text } = Typography
@@ -28,6 +26,10 @@ export const PerformanceDisplay: React.FC = () => {
 
     return (
         <div style={{ display: "inline-block", marginRight: 5 }}>
+            {/* <div style={{ width: 50, height: 64, position: "relative", display: "inline-block", textAlign: "right" }}>
+                <span style={{ height: 25, lineHeight: "25px", position: "absolute", top: 5, right: 0 }}>CPU</span>
+                <span>{`${cpu[cpu.length - 1] || 0}%`}</span>
+            </div> */}
             <span style={{ height: 20, marginRight: 5 }}>{`CPU:${cpu[cpu.length - 1] || 0}%`}</span>
 
             <div style={{ width: 100, height: "100%", display: "inline-block" }}>
