adec6d3aee86df90d32dd601234a4e9860b4fdbf
v1ll4n <v1ll4n@qq.com>
2022-01-18 18:14:01 +0800

    fix mitm plugin

--- app/renderer/src/main/src/utils/timeUtil.ts
+++ app/renderer/src/main/src/utils/timeUtil.ts
@@ -1,7 +1,14 @@
 import moment from "moment";
 
-export const formatTimestamp = (i: number) => {
+export const formatTimestamp = (i: number, onlyTime?: boolean) => {
+    if (onlyTime) {
+        return formatTime(i)
+    }
     return moment.unix(i).format("YYYY-MM-DD HH:mm:ss")
+}
+
+export const formatTime = (i: number) => {
+    return moment.unix(i).format("HH:mm:ss")
 }
 
 export const formatDate = (i: number) => {
