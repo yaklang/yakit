2f7fbfa198d82b823af50afea0cad65719d500ea
v1ll4n <v1ll4n@qq.com>
2022-03-17 10:47:20 +0800

    尝试优化一些表内性能

--- app/renderer/src/main/src/pages/fuzzer/HTTPFuzzerResultsCard.tsx
+++ app/renderer/src/main/src/pages/fuzzer/HTTPFuzzerResultsCard.tsx
@@ -29,8 +29,8 @@ export const HTTPFuzzerResultsCard: React.FC<HTTPFuzzerResultsCardProp> = (props
         title={<Space>
             Responses
             <SelectOne size={"small"} label={" "} colon={false} data={[
-                {value: true, text: "成功请求"},
-                {value: false, text: "失败请求"},
+                {value: true, text: `成功[${(props.successResponses || []).length}]个`},
+                {value: false, text: `失败[${(props.failedResponses || []).length}]个`},
             ]} value={showSuccess} setValue={setShowSuccess}
                        formItemStyle={{marginBottom: 0}}
             />
