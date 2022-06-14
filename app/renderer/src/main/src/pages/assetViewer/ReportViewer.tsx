d5252d728820d8303ba4487a9645d055b02360e9
TangHang123 <1425735414@qq.com>
2022-06-14 12:01:54 +0800

    html转word

--- app/renderer/src/main/src/pages/assetViewer/ReportViewer.tsx
+++ app/renderer/src/main/src/pages/assetViewer/ReportViewer.tsx
@@ -1,21 +1,24 @@
-import React, {useEffect, useState} from "react";
-import {Report} from "./models";
-import {failed} from "../../utils/notification";
-import {AutoCard} from "../../components/AutoCard";
-import {Button, Empty, Space, Tag} from "antd";
-import {showModal} from "../../utils/showModal";
-import {YakEditor} from "../../utils/editors";
-import {ReportItem} from "./reportRenders/schema";
-import {ReportItemRender} from "./reportRenders/render";
+import React, {useEffect, useState} from "react"
+import {Report} from "./models"
+import {failed} from "../../utils/notification"
+import {AutoCard} from "../../components/AutoCard"
+import {Button, Empty, Space, Tag} from "antd"
+import {showModal} from "../../utils/showModal"
+import {YakEditor} from "../../utils/editors"
+import {ReportItem} from "./reportRenders/schema"
+import {ReportItemRender} from "./reportRenders/render"
+
+import htmlDocx from "html-docx-js/dist/html-docx"
+import saveAs from "file-saver"
 
 export interface ReportViewerProp {
     id?: number
 }
 
-const {ipcRenderer} = window.require("electron");
+const {ipcRenderer} = window.require("electron")
 
 export const ReportViewer: React.FC<ReportViewerProp> = (props) => {
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false)
     const [report, setReport] = useState<Report>({
         From: "",
         Hash: "",
@@ -24,9 +27,8 @@ export const ReportViewer: React.FC<ReportViewerProp> = (props) => {
         Owner: "-",
         PublishedAt: 0,
         Title: "-"
-    });
-    const [reportItems, setReportItems] = useState<ReportItem[]>([]);
-
+    })
+    const [reportItems, setReportItems] = useState<ReportItem[]>([])
 
     useEffect(() => {
         if ((props?.id || 0) <= 0) {
@@ -34,16 +36,20 @@ export const ReportViewer: React.FC<ReportViewerProp> = (props) => {
         }
 
         setLoading(true)
-        ipcRenderer.invoke("QueryReport", {Id: props.id}).then((r: Report) => {
-            if (r) setReport(r);
-        }).catch(e => {
-            failed(`Query Report[${props.id}] failed`)
-        }).finally(() => setTimeout(() => setLoading(false), 300))
+        ipcRenderer
+            .invoke("QueryReport", {Id: props.id})
+            .then((r: Report) => {
+                if (r) setReport(r)
+            })
+            .catch((e) => {
+                failed(`Query Report[${props.id}] failed`)
+            })
+            .finally(() => setTimeout(() => setLoading(false), 300))
     }, [props.id])
 
     useEffect(() => {
         try {
-            const items = JSON.parse(report.JsonRaw) as ReportItem[];
+            const items = JSON.parse(report.JsonRaw) as ReportItem[]
             if (!!items && items.length > 0) {
                 setReportItems(items)
             }
@@ -54,30 +60,72 @@ export const ReportViewer: React.FC<ReportViewerProp> = (props) => {
     }, [report])
 
     if (report.Id <= 0) {
-        return <AutoCard loading={loading}>
-            <Empty>{"选择报告以在此查看内容"}</Empty>
-        </AutoCard>
+        return (
+            <AutoCard loading={loading}>
+                <Empty>{"选择报告以在此查看内容"}</Empty>
+            </AutoCard>
+        )
+    }
+    const toPdf = () => {
+        const targetDom = document.getElementById("toPdf")
+        // console.log("targetDom", targetDom?.innerHTML)
+        // debugger
+        //     var content = ` <h1>This is an about page</h1>
+        //   <h2>This is an about page</h2>`
+        const content = targetDom?.innerHTML
+        var page =
+            '<!DOCTYPE html><html><head><meta charset="UTF-8">' +
+            '<link href="./antd.css" rel="stylesheet" type="text/css" />' +
+            '<link href="./index.less" rel="stylesheet/less" type="text/less" />' +
+            "<style> code{ background-color: aliceblue;}</style></head>" +
+            "<body>" +
+            content +
+            "</body></html>"
+        console.log("page", page)
+
+        var converted = htmlDocx.asBlob(page)
+        // 用 FielSaver.js里的保存方法 进行输出
+        saveAs(converted, "test.docx")
     }
 
-    return <AutoCard size={"small"} bordered={false} loading={loading} title={<Space>
-        {report.Title} <Tag>{props.id}</Tag>
-    </Space>} bodyStyle={{overflow: "auto"}} extra={<Space>
-        <a href={"#"} onClick={() => {
-            showModal({
-                title: "RAW DATA", content: (
-                    <div style={{height: 300}}>
-                        <YakEditor value={report.JsonRaw}/>
-                    </div>
-                ), width: "50%",
-            })
-        }}>RAW</a>
-    </Space>}>
-        <Space direction={"vertical"}
-               style={{width: "100%"}}
+    return (
+        <AutoCard
+            size={"small"}
+            bordered={false}
+            loading={loading}
+            title={
+                <Space>
+                    {report.Title} <Tag>{props.id}</Tag>
+                </Space>
+            }
+            bodyStyle={{overflow: "auto"}}
+            extra={
+                <Space>
+                    <a
+                        href={"#"}
+                        onClick={() => {
+                            showModal({
+                                title: "RAW DATA",
+                                content: (
+                                    <div style={{height: 300}}>
+                                        <YakEditor value={report.JsonRaw} />
+                                    </div>
+                                ),
+                                width: "50%"
+                            })
+                        }}
+                    >
+                        RAW
+                    </a>
+                    <Button onClick={toPdf}>md-pdf</Button>
+                </Space>
+            }
         >
-            {reportItems.map((i, index) => {
-                return <ReportItemRender item={i} key={index}/>
-            })}
-        </Space>
-    </AutoCard>
-};
+            <Space direction={"vertical"} style={{width: "100%"}} id='toPdf'>
+                {reportItems.map((i, index) => {
+                    return <ReportItemRender item={i} key={index} />
+                })}
+            </Space>
+        </AutoCard>
+    )
+}
