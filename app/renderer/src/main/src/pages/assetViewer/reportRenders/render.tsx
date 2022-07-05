d48d8462f8e2226f91855fcc3bc030a77134bc9c
VillanCh <188228725@qq.com>
2022-07-06 00:27:32 +0800

    增加饼图样式

--- app/renderer/src/main/src/pages/assetViewer/reportRenders/render.tsx
+++ app/renderer/src/main/src/pages/assetViewer/reportRenders/render.tsx
@@ -5,6 +5,7 @@ import {YakEditor} from "../../../utils/editors";
 import {AutoCard} from "../../../components/AutoCard";
 import {Tag} from "antd";
 import {JSONTableRender} from "./jsonTableRender";
+import {PieGraph} from "../../graph/PieGraph";
 
 export interface ReportItemRenderProp {
     item: ReportItem
@@ -16,12 +17,26 @@ export const ReportItemRender: React.FC<ReportItemRenderProp> = (props) => {
             return <ReportMarkdownBlock item={props.item}/>
         case "json-table":
             return <JSONTableRender item={props.item}/>
+        case "pie-graph":
+            try {
+                return <PieGraph type={"pie"} height={300}
+                                 data={JSON.parse(props.item.content) as { key: string, value: number }[]}/>
+            } catch (e) {
+                console.info("渲染图失败")
+                console.info(e)
+                return <div style={{height: 300}}>
+                    <YakEditor value={props.item.content}/>
+                </div>
+            }
         default:
             return <AutoCard
-                style={{width: "100%", height: 400}}
+                style={{width: "100%"}}
+                size={"small"}
                 extra={<Tag color={"red"}>{props.item.type}</Tag>}
             >
-                <YakEditor value={props.item.content}/>
+                <div style={{height: 300}}>
+                    <YakEditor value={props.item.content}/>
+                </div>
             </AutoCard>
     }
 };
