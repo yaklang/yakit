0f14defa2c8e3335530ce1c68fc7f5c295840ff6
TangHang123 <1425735414@qq.com>
2022-07-06 15:06:51 +0800

    a47a129... fix: 修复没有key值控制台报错

--- app/renderer/src/main/src/components/PluginList.tsx
+++ app/renderer/src/main/src/components/PluginList.tsx
@@ -9,7 +9,7 @@ import {showModal} from "../utils/showModal"
 import {InputInteger, InputItem, OneLine} from "../utils/inputUtil"
 
 import "./PluginList.css"
-import {YakEditor} from "../utils/editors";
+import {YakEditor} from "../utils/editors"
 
 const {Text} = Typography
 
@@ -22,7 +22,7 @@ export interface PluginListProp extends AutoCardProps {
     allSelectScript: (flag: boolean) => any
     selectScript: (info: YakScript) => any
     unSelectScript: (info: YakScript) => any
-    search: (params: { limit: number; keyword: string }) => any
+    search: (params: {limit: number; keyword: string}) => any
     extra?: React.ReactNode
     disabled?: boolean
     readOnly?: boolean
@@ -39,67 +39,78 @@ interface YakScriptCheckboxProp {
 }
 
 const YakScriptCheckbox: React.FC<YakScriptCheckboxProp> = React.memo((props) => {
-    const {info, selected, vlistWidth, selectScript, unSelectScript,} = props;
+    const {info, selected, vlistWidth, selectScript, unSelectScript} = props
 
-    return <div key={info.ScriptName} className='list-opt'>
-        {props.readOnly ? <OneLine width={vlistWidth} overflow={"hidden"}>
-            <div>
-                {info.ScriptName}
-            </div>
-        </OneLine> : <Checkbox
-            disabled={props.disabled}
-            checked={selected.includes(info.ScriptName)}
-            onChange={(r) => {
-                if (r.target.checked) selectScript(info)
-                else unSelectScript(info)
-            }}
-        >
-            <OneLine width={vlistWidth} overflow={"hidden"}>
-                <div>
-                    {info.ScriptName}
-                </div>
-            </OneLine>
-        </Checkbox>}
-        <div style={{flex: 1, textAlign: "right"}}>
-            {info.Help && (
-                <a
-                    onClick={() => {
-                        showModal({
-                            width: "40%",
-                            title: "Help",
-                            content: <>{info.Help}</>
-                        })
+    return (
+        <div key={info.ScriptName} className='list-opt'>
+            {props.readOnly ? (
+                <OneLine width={vlistWidth} overflow={"hidden"}>
+                    <div>{info.ScriptName}</div>
+                </OneLine>
+            ) : (
+                <Checkbox
+                    disabled={props.disabled}
+                    checked={selected.includes(info.ScriptName)}
+                    onChange={(r) => {
+                        if (r.target.checked) selectScript(info)
+                        else unSelectScript(info)
                     }}
-                    href={"#"} style={{marginLeft: 2, marginRight: 2}}
-                ><QuestionCircleOutlined/></a>
+                >
+                    <OneLine width={vlistWidth} overflow={"hidden"}>
+                        <div>{info.ScriptName}</div>
+                    </OneLine>
+                </Checkbox>
             )}
-            {info.Author && (
-                <Tooltip title={info.Author}>
-                    <a href={"#"} style={{marginRight: 2, marginLeft: 2}}><UserOutlined/></a>
-                </Tooltip>
-            )}
-            {!!info.Content && props.readOnly && (
-                <a href={"#"}
-                   style={{marginRight: 2, marginLeft: 2}}
-                   onClick={() => {
-                       showModal({
-                           title: info.ScriptName, width: "60%",
-                           content: (
-                               <div style={{height: 400}}>
-                                   <YakEditor
-                                       type={info.Type === "nuclei" ? "yaml" : "yak"}
-                                       readOnly={true}
-                                       value={info.Content}
-                                   />
-                               </div>
-                           )
-                       })
-                   }}
-                ><CodeOutlined/></a>
-            )}
+            <div style={{flex: 1, textAlign: "right"}}>
+                {info.Help && (
+                    <a
+                        onClick={() => {
+                            showModal({
+                                width: "40%",
+                                title: "Help",
+                                content: <>{info.Help}</>
+                            })
+                        }}
+                        href={"#"}
+                        style={{marginLeft: 2, marginRight: 2}}
+                    >
+                        <QuestionCircleOutlined />
+                    </a>
+                )}
+                {info.Author && (
+                    <Tooltip title={info.Author}>
+                        <a href={"#"} style={{marginRight: 2, marginLeft: 2}}>
+                            <UserOutlined />
+                        </a>
+                    </Tooltip>
+                )}
+                {!!info.Content && props.readOnly && (
+                    <a
+                        href={"#"}
+                        style={{marginRight: 2, marginLeft: 2}}
+                        onClick={() => {
+                            showModal({
+                                title: info.ScriptName,
+                                width: "60%",
+                                content: (
+                                    <div style={{height: 400}}>
+                                        <YakEditor
+                                            type={info.Type === "nuclei" ? "yaml" : "yak"}
+                                            readOnly={true}
+                                            value={info.Content}
+                                        />
+                                    </div>
+                                )
+                            })
+                        }}
+                    >
+                        <CodeOutlined />
+                    </a>
+                )}
+            </div>
         </div>
-    </div>
-});
+    )
+})
 
 export const PluginList: React.FC<PluginListProp> = React.memo((props) => {
     const {
@@ -153,78 +164,84 @@ export const PluginList: React.FC<PluginListProp> = React.memo((props) => {
                 bordered={false}
                 {...restCard}
                 extra={
-                    !props.readOnly && <Space>
-                        <Popover
-                            title={"额外设置"}
-                            trigger={["click"]}
-                            content={
-                                <div>
-                                    <Form
-                                        size={"small"}
-                                        onSubmitCapture={(e) => {
-                                            e.preventDefault()
-                                            search({limit: limit, keyword: keyword})
-                                        }}
-                                    >
-                                        <InputInteger
-                                            label={"插件展示数量"}
-                                            value={limit}
-                                            setValue={setLimit}
-                                            formItemStyle={{marginBottom: 4}}
-                                        />
-                                        <Form.Item colon={false} label={""} style={{marginBottom: 10}}>
-                                            <Button type='primary' htmlType='submit'>
-                                                刷新
-                                            </Button>
-                                        </Form.Item>
-                                    </Form>
-                                </div>
-                            }
-                        >
-                            <Button size={"small"} icon={<SettingOutlined/>} type={"link"}/>
-                        </Popover>
-                        <Popover
-                            title={"搜索插件关键字"}
-                            trigger={["click"]}
-                            content={
-                                <div>
-                                    <Form
-                                        size={"small"}
-                                        onSubmitCapture={(e) => {
-                                            e.preventDefault()
-                                            search({limit: limit, keyword: keyword})
-                                        }}
-                                    >
-                                        <InputItem
-                                            label={""}
-                                            extraFormItemProps={{
-                                                style: {marginBottom: 4},
-                                                colon: false
+                    !props.readOnly && (
+                        <Space>
+                            <Popover
+                                title={"额外设置"}
+                                trigger={["click"]}
+                                content={
+                                    <div>
+                                        <Form
+                                            size={"small"}
+                                            onSubmitCapture={(e) => {
+                                                e.preventDefault()
+                                                search({limit: limit, keyword: keyword})
                                             }}
-                                            value={keyword}
-                                            setValue={setKeyword}
-                                        />
-                                        <Form.Item colon={false} label={""} style={{marginBottom: 10}}>
-                                            <Button type='primary' htmlType='submit'>
-                                                搜索
-                                            </Button>
-                                        </Form.Item>
-                                    </Form>
-                                </div>
-                            }
-                        >
-                            <Button size={"small"} type={!!keyword ? "primary" : "link"} icon={<SearchOutlined/>}/>
-                        </Popover>
-                        <Checkbox
-                            disabled={props.disabled}
-                            indeterminate={indeterminate}
-                            onChange={(r) => allSelectScript(r.target.checked)}
-                            checked={checked}
-                        >
-                            全选
-                        </Checkbox>
-                        {extra || <></>}
-                    </Space>
+                                        >
+                                            <InputInteger
+                                                label={"插件展示数量"}
+                                                value={limit}
+                                                setValue={setLimit}
+                                                formItemStyle={{marginBottom: 4}}
+                                            />
+                                            <Form.Item colon={false} label={""} style={{marginBottom: 10}}>
+                                                <Button type='primary' htmlType='submit'>
+                                                    刷新
+                                                </Button>
+                                            </Form.Item>
+                                        </Form>
+                                    </div>
+                                }
+                            >
+                                <Button size={"small"} icon={<SettingOutlined />} type={"link"} />
+                            </Popover>
+                            <Popover
+                                title={"搜索插件关键字"}
+                                trigger={["click"]}
+                                content={
+                                    <div>
+                                        <Form
+                                            size={"small"}
+                                            onSubmitCapture={(e) => {
+                                                e.preventDefault()
+                                                search({limit: limit, keyword: keyword})
+                                            }}
+                                        >
+                                            <InputItem
+                                                label={""}
+                                                extraFormItemProps={{
+                                                    style: {marginBottom: 4},
+                                                    colon: false
+                                                }}
+                                                value={keyword}
+                                                setValue={setKeyword}
+                                            />
+                                            <Form.Item colon={false} label={""} style={{marginBottom: 10}}>
+                                                <Button type='primary' htmlType='submit'>
+                                                    搜索
+                                                </Button>
+                                            </Form.Item>
+                                        </Form>
+                                    </div>
+                                }
+                            >
+                                <Button
+                                    size={"small"}
+                                    type={!!keyword ? "primary" : "link"}
+                                    icon={<SearchOutlined />}
+                                />
+                            </Popover>
+                            <Checkbox
+                                disabled={props.disabled}
+                                indeterminate={indeterminate}
+                                onChange={(r) => allSelectScript(r.target.checked)}
+                                checked={checked}
+                            >
+                                全选
+                            </Checkbox>
+                            {extra || <></>}
+                        </Space>
+                    )
                 }
             >
                 <ReactResizeDetector
@@ -241,11 +258,20 @@ export const PluginList: React.FC<PluginListProp> = React.memo((props) => {
                     refreshRate={50}
                 />
                 <div ref={containerRef as any} style={{height: vlistHeigth, overflow: "auto"}}>
-                    <div ref={wrapperRef as any}>{list.map((i) => <YakScriptCheckbox
-                        key={i.data.ScriptName} readOnly={props.readOnly}
-                        info={i.data} selectScript={selectScript} unSelectScript={unSelectScript}
-                        vlistWidth={vlistWidth} selected={selected} disabled={disabled}
-                    />)}</div>
+                    <div ref={wrapperRef as any}>
+                        {list.map((i) => (
+                            <YakScriptCheckbox
+                                key={i.data.ScriptName}
+                                readOnly={props.readOnly}
+                                info={i.data}
+                                selectScript={selectScript}
+                                unSelectScript={unSelectScript}
+                                vlistWidth={vlistWidth}
+                                selected={selected}
+                                disabled={disabled}
+                            />
+                        ))}
+                    </div>
                 </div>
             </AutoCard>
         </div>
