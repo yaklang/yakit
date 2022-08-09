fa6c99d146dd5945ebcde057148407803ff2b1bb
TangHang123 <1425735414@qq.com>
2022-08-09 18:04:10 +0800

    -

--- app/renderer/src/main/src/components/RollingLoadList/index.tsx
+++ app/renderer/src/main/src/components/RollingLoadList/index.tsx
@@ -27,6 +27,7 @@ interface RollingLoadListProps<T> {
     defItemHeight: number
     numberRoll?: number
     isGridLayout?: boolean
+    defCol?: number
 }
 
 export const RollingLoadList = <T extends any>(props: RollingLoadListProps<T>) => {
@@ -43,27 +44,30 @@ export const RollingLoadList = <T extends any>(props: RollingLoadListProps<T>) =
         classNameList,
         defItemHeight,
         numberRoll,
-        isGridLayout
+        isGridLayout,
+        defCol = 1
     } = props
     const [vlistHeigth, setVListHeight] = useState(600)
-    // const [itemHeight, setItemHeight] = useState<number>(defItemHeight)
-    // const itemHeightRef = useRef<number>(defItemHeight)
-    // const overScanRef = useRef<number>(10)
-    const colRef = useRef<number>(1)
+    const [col, setCol] = useState<number>(defCol)
     const containerRef = useRef<any>(null)
     const wrapperRef = useRef<any>(null)
-    // const originalList = useCreation(() => Array.from(Array(data.length/colRef.current).keys()), [data.length]);
-    // useEffect(() => {
-    //     console.log('data',data);
-
-    //    console.log('originalList',originalList);
-    // }, [originalList.length])
-    const [list, scrollTo] = useVirtualList(data || [], {
+    const originalList = useCreation(() => {
+        const listByLength: number[] = []
+        data.forEach((ele, index) => {
+            if (index % col === 0) {
+                listByLength.push(index)
+            }
+        })
+        return listByLength
+    }, [data.length, col])
+    useEffect(() => {
+        console.log("defItemHeight", defItemHeight)
+    }, [originalList.length])
+    const [list, scrollTo] = useVirtualList(originalList, {
         containerTarget: containerRef,
         wrapperTarget: wrapperRef,
-        // itemHeight: itemHeightRef.current,
-        overscan: 1 * colRef.current,
-        itemHeight: defItemHeight / colRef.current
+        itemHeight: defItemHeight
+        // overscan: 1 * col
     })
     useDebounceEffect(
         () => {
@@ -84,7 +88,7 @@ export const RollingLoadList = <T extends any>(props: RollingLoadListProps<T>) =
     const isFirstNumberRoll = useRef(true) // 初次不执行
     useEffect(() => {
         onRollNumber()
-    }, [numberRoll, colRef.current])
+    }, [numberRoll, col])
     const onRollNumber = useMemoizedFn(() => {
         if (isFirstNumberRoll.current) {
             isFirstNumberRoll.current = false
@@ -95,26 +99,29 @@ export const RollingLoadList = <T extends any>(props: RollingLoadListProps<T>) =
         }
     })
     const {width} = useSize(document.querySelector("body")) || {width: 0, height: 0}
-    useEffect(() => {
-        if (isGridLayout) {
-            onComputeItemHeight()
-        } else {
-            colRef.current = 1
-        }
-    }, [isGridLayout, width])
+    useDebounceEffect(
+        () => {
+            if (isGridLayout) {
+                onComputeItemHeight()
+            } else {
+                setCol(defCol)
+            }
+        },
+        [isGridLayout, width],
+        {wait: 200, leading: true}
+    )
     const onComputeItemHeight = useMemoizedFn(() => {
         if (!width) return
-        const col = 1
         if (width <= 800) {
-            colRef.current = col
+            setCol(defCol)
         } else if (width > 800 && width < 1024) {
-            colRef.current = col * 2
+            setCol(defCol * 2)
         } else if (width >= 1024 && width < 1440) {
-            colRef.current = col * 3
+            setCol(defCol * 3)
         } else if (width >= 1440 && width < 1920) {
-            colRef.current = col * 4
+            setCol(defCol * 4)
         } else if (width >= 1920) {
-            colRef.current = col * 5
+            setCol(defCol * 5)
         }
     })
 
@@ -130,11 +137,9 @@ export const RollingLoadList = <T extends any>(props: RollingLoadListProps<T>) =
                 const clientHeight = dom.clientHeight //可视区域
                 const scrollHeight = dom.scrollHeight //滚动条内容的总高度
                 const scrollBottom = scrollHeight - contentScrollTop - clientHeight
-                // console.log(
-                //     "contentScrollTop",
-                //     // defItemHeight / colRef.current,
-                //     Math.floor(contentScrollTop / defItemHeight) + 1
-                // )
+                console.log(Math.floor(contentScrollTop / defItemHeight) + 1)
+                console.log("clientHeight", wrapperRef.current.clientHeight)
+
                 if (scrollBottom <= 500) {
                     loadMoreData() // 获取数据的方法
                 }
@@ -142,7 +147,81 @@ export const RollingLoadList = <T extends any>(props: RollingLoadListProps<T>) =
         },
         {wait: 200, leading: false}
     )
-
+    const renderContent = useMemoizedFn((i) => {
+        switch (col) {
+            case 2:
+                return renderItem2(i)
+            case 3:
+                return renderItem3(i)
+            case 4:
+                return renderItem4(i)
+            case 5:
+                return renderItem5(i)
+            default:
+                return renderDef(i)
+        }
+    })
+    const renderItem2 = (i) =>
+        data[i.data] && (
+            <div className='display-flex' key={data[i.data][rowKey || "Id"]}>
+                <div className={`width-50 ${classNameRow}`}>{renderRow(data[i.data], i.data)}</div>
+                {data[i.data + 1] && (
+                    <div className={`width-50 ${classNameRow}`}>{renderRow(data[i.data + 1], i.data + 1)}</div>
+                )}
+            </div>
+        )
+    const renderItem3 = (i) =>
+        data[i.data] && (
+            <div className='display-flex' style={{position: "relative"}} key={data[i.data][rowKey || "Id"]}>
+                <div style={{position: "absolute", zIndex: 9, color: "red"}}>{i.index}</div>
+                <div className={`width-33 ${classNameRow}`}>{renderRow(data[i.data], i.data)}</div>
+                {data[i.data + 1] && (
+                    <div className={`width-33 ${classNameRow}`}>{renderRow(data[i.data + 1], i.data + 1)}</div>
+                )}
+                {data[i.data + 2] && (
+                    <div className={`width-33 ${classNameRow}`}>{renderRow(data[i.data + 2], i.data + 2)}</div>
+                )}
+            </div>
+        )
+    const renderItem4 = (i) =>
+        data[i.data] && (
+            <div className='display-flex' key={data[i.data][rowKey || "Id"]}>
+                <div className={`width-25 ${classNameRow}`}>{renderRow(data[i.data], i.data)}</div>
+                {data[i.data + 1] && (
+                    <div className={`width-25 ${classNameRow}`}>{renderRow(data[i.data + 1], i.data + 1)}</div>
+                )}
+                {data[i.data + 2] && (
+                    <div className={`width-25 ${classNameRow}`}>{renderRow(data[i.data + 2], i.data + 2)}</div>
+                )}
+                {data[i.data + 3] && (
+                    <div className={`width-25 ${classNameRow}`}>{renderRow(data[i.data + 3], i.data + 3)}</div>
+                )}
+            </div>
+        )
+    const renderItem5 = (i) =>
+        data[i.data] && (
+            <div className='display-flex' key={data[i.data][rowKey || "Id"]}>
+                <div className={`width-25 ${classNameRow}`}>{renderRow(data[i.data], i.data)}</div>
+                {data[i.data + 1] && (
+                    <div className={`width-25 ${classNameRow}`}>{renderRow(data[i.data + 1], i.data + 1)}</div>
+                )}
+                {data[i.data + 2] && (
+                    <div className={`width-25 ${classNameRow}`}>{renderRow(data[i.data + 2], i.data + 2)}</div>
+                )}
+                {data[i.data + 3] && (
+                    <div className={`width-25 ${classNameRow}`}>{renderRow(data[i.data + 3], i.data + 3)}</div>
+                )}
+                {data[i.data + 4] && (
+                    <div className={`width-25 ${classNameRow}`}>{renderRow(data[i.data + 4], i.data + 4)}</div>
+                )}
+            </div>
+        )
+    const renderDef = (i) =>
+        data[i.data] && (
+            <div key={data[i.data][rowKey || "Id"]} className={classNameRow}>
+                {renderRow(data[i.data], i.data)}
+            </div>
+        )
     return (
         <>
             <ReactResizeDetector
@@ -157,78 +236,16 @@ export const RollingLoadList = <T extends any>(props: RollingLoadListProps<T>) =
                 refreshMode={"debounce"}
                 refreshRate={50}
             />
+            {/* container */}
             <div
-                className={`container ${classNameList}`}
+                className={` ${classNameList}`}
                 style={{height: vlistHeigth}}
                 ref={containerRef}
                 onScroll={() => onScrollCapture.run()}
             >
-                <div ref={wrapperRef} className={`${isGridLayout ? "list-grid" : ""} `}>
+                <div ref={wrapperRef}>
                     {list.map((i) => {
-                        return (
-                            <div key={i.data[rowKey || "Id"]} className={classNameRow}>
-                                {renderRow(i.data, i.index)}
-                            </div>
-                        )
-
-                        // if (!isGridLayout || colRef.current === 1) {
-                        //     return (
-                        //         <div key={i.data[rowKey || "Id"]} className={classNameRow}>
-                        //             {renderRow(i.data, i.index)}
-                        //         </div>
-                        //     )
-                        // }
-
-                        // if (colRef.current === 2) {
-                        //     return (
-                        //         i.index / 2 === 0 && (
-                        //             <div style={{display: "flex"}}>
-                        //                 <div
-                        //                     key={i.data[rowKey || "Id"]}
-                        //                     className={classNameRow}
-                        //                 >
-                        //                     {renderRow(data[i.index], i.index)}
-                        //                 </div>
-                        //                 <div
-                        //                     key={i.data[rowKey || "Id"]}
-                        //                     className={classNameRow}
-                        //                 >
-                        //                     {renderRow(data[i.index + 1], i.index + 1)}
-                        //                 </div>
-                        //             </div>
-                        //         )
-                        //     )
-                        // }
-                        // if (colRef.current === 3) {
-                        //     return (
-                        //         i.index / 3 === 0 && (
-                        //             <div style={{display: "flex"}}>
-                        //                 <div
-                        //                     key={i.data[rowKey || "Id"]}
-                        //                     className={classNameRow}
-                        //                 >
-                        //                     {renderRow(data[i.index], i.index)}
-                        //                 </div>
-                        //                 {data[i.index + 1] && (
-                        //                     <div
-                        //                         key={i.data[rowKey || "Id"]}
-                        //                         className={classNameRow}
-                        //                     >
-                        //                         {renderRow(data[i.index + 1], i.index + 1)}
-                        //                     </div>
-                        //                 )}
-                        //                 {data[i.index + 2] && (
-                        //                     <div
-                        //                         key={i.data[rowKey || "Id"]}
-                        //                         className={classNameRow}
-                        //                     >
-                        //                         {renderRow(data[i.index + 2], i.index + 2)}
-                        //                     </div>
-                        //                 )}
-                        //             </div>
-                        //         )
-                        //     )
-                        // }
+                        return renderContent(i)
                     })}
                     {loading && hasMore && (
                         <div className='grid-block text-center'>
