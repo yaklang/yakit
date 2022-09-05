7f93f083a4ac6581a96f3850ec0ac835cc3946df
TangHang123 <1425735414@qq.com>
2022-09-05 17:46:45 +0800

    日志详情页面绘制

--- app/renderer/src/main/src/pages/yakitStore/YakitPluginOnlineJournal/YakitPluginJournalDetails.tsx
+++ app/renderer/src/main/src/pages/yakitStore/YakitPluginOnlineJournal/YakitPluginJournalDetails.tsx
@@ -1,10 +1,14 @@
-import { YakScriptFormContent } from "@/pages/invoker/YakScriptCreator"
+import { FromLayoutProps, YakScriptFormContent, YakScriptLargeEditor } from "@/pages/invoker/YakScriptCreator"
 import { Route } from "@/routes/routeSpec"
-import { Card } from "antd"
+import { Button, Card, Form, Space } from "antd"
 import React, { useEffect, useState } from "react"
 import { useCreation, useGetState, useMemoizedFn } from "ahooks"
 import './YakitPluginJournalDetails.scss'
 import { YakScript } from "@/pages/invoker/schema"
+import { FullscreenOutlined } from "@ant-design/icons"
+import { showDrawer } from "@/utils/showModal"
+import { YakEditor } from "@/utils/editors"
+import { DataCompare } from "@/pages/compare/DataCompare"
 
 const { ipcRenderer } = window.require("electron")
 
@@ -39,12 +43,90 @@ const defParams = {
 
 
 export const YakitPluginJournalDetails: React.FC<YakitPluginJournalDetailsProps> = (props) => {
+    const defFromLayout = useCreation(() => {
+        const col: FromLayoutProps = {
+            labelCol: { span: 5 },
+            wrapperCol: { span: 14 }
+        }
+        return col
+    }, [])
     const { YakitPluginJournalDetailsId } = props;
     const [params, setParams, getParams] = useGetState<YakScript>(defParams)
+    const [fullscreen, setFullscreen] = useState(false)
     return (
         <div>
             <Card title="修改详情" bordered={false}>
-                <YakScriptFormContent params={params} setParams={setParams} />
+                <Form {...defFromLayout}>
+                    <YakScriptFormContent params={params} setParams={setParams} />
+                    <Form.Item
+                        label={"源码"}
+                        help={
+                            <>
+                                <Space>
+                                    <Button
+                                        icon={<FullscreenOutlined />}
+                                        onClick={() => {
+                                            setFullscreen(true)
+                                            let m = showDrawer({
+                                                title: "Edit Code",
+                                                width: "100%",
+                                                closable: false,
+                                                keyboard: false,
+                                                content: (
+                                                    <>
+                                                        <DataCompare />
+                                                    </>
+                                                )
+                                            })
+                                        }}
+                                        type={"link"}
+                                        style={{
+                                            marginBottom: 12,
+                                            marginTop: 6
+                                        }}
+                                    >
+                                        大屏模式
+                                    </Button>
+                                </Space>
+                            </>
+                        }
+                    >
+                        {!fullscreen && (
+                            <div className="yak-editor-content">
+                                <div className="yak-editor-item">
+                                    <p>提交的代码</p>
+                                    <div className="yak-editor">
+                                        <YakEditor
+                                            type={"yak"}
+                                            setValue={(Content) => setParams({ ...params, Content })}
+                                            value={params.Content}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="yak-editor-item">
+                                    <p>最新的代码</p>
+                                    <div className="yak-editor">
+                                        <YakEditor
+                                            type={"yak"}
+                                            setValue={(Content) => setParams({ ...params, Content })}
+                                            value={params.Content}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                    </Form.Item>
+                    <Form.Item colon={false} label={" "}>
+                        <Space>
+                            <Button type='primary' danger onClick={() => { }} >
+                                拒绝
+                            </Button>
+                            <Button type='primary' onClick={() => { }} >
+                                同意
+                            </Button>
+                        </Space>
+                    </Form.Item>
+                </Form>
             </Card>
         </div>
     )
