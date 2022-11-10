3c5d04c43584db9fddf6e2a5585061ea51d816c9
TangHang123 <1425735414@qq.com>
2022-11-10 14:17:23 +0800

    -

--- app/renderer/src/main/src/utils/yakQueryHTTPFlow.tsx
+++ app/renderer/src/main/src/utils/yakQueryHTTPFlow.tsx
@@ -1,11 +1,3 @@
-/*
- * @Author: TangHang123 1425735414@qq.com
- * @Date: 2022-11-04 10:44:32
- * @LastEditors: TangHang123 1425735414@qq.com
- * @LastEditTime: 2022-11-07 10:46:05
- * @FilePath: \yakit\app\renderer\src\main\src\utils\yakQueryHTTPFlow.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import React from "react"
 
 // @ts-ignore
