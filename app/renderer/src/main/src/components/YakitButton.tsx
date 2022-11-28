e4930f9b50a1b4f443596424941dea5f7b2c5fc8
nonight <378099757@qq.com>
2022-11-28 18:02:56 +0800

    暂存

--- app/renderer/src/main/src/components/YakitButton.tsx
+++ app/renderer/src/main/src/components/YakitButton.tsx
@@ -4,16 +4,33 @@ import {Button, ButtonProps} from "antd"
 import classnames from "classnames"
 import styles from "./yakitButton.module.scss"
 
-type buttsd = Omit<ButtonProps, "size">
-type buttsds = Omit<buttsd, "type">
+type RemoveSizeButton = Omit<ButtonProps, "size">
+type RemoveTypeButton = Omit<RemoveSizeButton, "type">
 
-export interface YakitPopoverProp extends buttsds {
-    type?: "" | "123"
+export interface YakitButtonProp extends RemoveTypeButton {
+    type?: "danger"
     size?: "big" | "small"
 }
 
-export const YakitPopover: React.FC<YakitPopoverProp> = React.memo((props) => {
+export const YakitButton: React.FC<YakitButtonProp> = React.memo((props) => {
     const {size, type, children, ...resePopover} = props
 
-    return <Button>{children}</Button>
+    const typeClass = useMemo(() => {
+        return "yakit-button-primary"
+    }, [type])
+
+    const sizeClass = useMemo(() => {
+        if (!size) return "yakit-button-size"
+        if (size === "big") return "yakit-button-big-size"
+        if (size === "small") return "yakit-button-small-size"
+        return ""
+    }, [size])
+
+    return (
+        <div className={classnames(styles["yakit-button-wrapper"], styles[typeClass])}>
+            <Button {...resePopover} className={classnames(styles[sizeClass], styles["yakit-button-type"])}>
+                {children}
+            </Button>
+        </div>
+    )
 })
