034859216fbc21e0cfb9801841ef42861892a51f
TangHang123 <1425735414@qq.com>
2022-12-05 15:45:00 +0800

    电信菜单

--- app/renderer/src/main/src/routes/routeSpec.tsx
+++ app/renderer/src/main/src/routes/routeSpec.tsx
@@ -75,7 +75,11 @@ import {
     MenuSubDomainCollectionIcon,
     MenuVulnerabilityRiskIcon,
     MenuWebsocketFuzzerIcon,
+<<<<<<< HEAD
     MenuDefaultPluginIcon
+=======
+    MenuYakRunnerIcon
+>>>>>>> 4f281dd5 (电信菜单)
 } from "@/pages/customizeMenu/icon/menuIcon"
 import {EngineConsole} from "@/pages/engineConsole/EngineConsole"
 import {
@@ -463,9 +467,46 @@ export const ContentByRoute = (r: Route | string, yakScriptId?: number, params?:
  */
 export const DefaultRouteMenuData: MenuDataProps[] = [
     {
-        id: "1",
-        label: "手工渗透",
+        id: "2",
+        label: "信息收集",
+        key: Route.GeneralModule,
         subMenuData: [
+            {
+                id: "2-4",
+                key: Route.Mod_ScanPort,
+                label: "端口扫描",
+                icon: <MenuPortScanningIcon />,
+                describe: "对 IP、IP段、域名等端口进行 SYN、指纹检测、可编写插件进行检测、满足更个性化等需求"
+            }
+        ]
+    },
+    {
+        id: "3",
+        label: "Web安全",
+        subMenuData: [
+            {id: "3-1", key: Route.PoC, label: "专项漏洞检测", icon: <MenuSpecialVulnerabilityDetectionIcon />},
+            {
+                id: "3-2",
+                key: Route.Mod_Brute,
+                label: "爆破与未授权",
+                icon: <MenuBlastingAndUnauthorizedTestingIcon />,
+                describe: "对目标的登录账号、密码等进行爆破，在爆破前会进行未授权检测"
+            }
+        ]
+    },
+    {
+        id: "4",
+        label: "自动化漏洞检测模块",
+        subMenuData: [
+            //
+            {id: "4-2", key: Route.BatchExecutorPage, label: "插件批量执行", icon: <MenuPluginBatchExecutionIcon />}
+        ]
+    },
+    {
+        id: "1",
+        label: "专家模式",
+        subMenuData: [
+            {id: "1-5", key: Route.ModManager, label: "插件仓库", icon: <MenuPluginWarehouseIcon />},
             {
                 id: "1-1",
                 key: Route.HTTPHacker,
@@ -482,13 +523,8 @@ export const DefaultRouteMenuData: MenuDataProps[] = [
                 hoverIcon: <MenuSolidWebFuzzerIcon />,
                 describe: "通过核心模糊测试标签语法，实现了对 Burpsuite 的 Repeater 和 Intruder 的完美整合"
             },
-            {
-                id: "1-3",
-                key: Route.WebsocketFuzzer,
-                label: "Websocket Fuzzer",
-                icon: <MenuWebsocketFuzzerIcon />,
-                hoverIcon: <MenuSolidWebsocketFuzzerIcon />
-            }
+            // {id: "1-6", key: Route.YakScript, label: "Yak Runner", icon: <MenuYakRunnerIcon />}
+            // {id: "1-3", key: Route.WebsocketFuzzer, label: "Websocket Fuzzer", icon: <MenuWebsocketFuzzerIcon />},
             // {
             //     id: "1-4",
             //     key: Route.PayloadGenerater_New,
@@ -498,163 +534,18 @@ export const DefaultRouteMenuData: MenuDataProps[] = [
             // }
         ]
     },
-    {
-        id: "2",
-        label: "基础工具",
-        isNovice: true,
-        key: Route.GeneralModule,
-        subMenuData: [
-            {
-                id: "2-4",
-                key: Route.Mod_ScanPort,
-                label: "扫描端口/指纹",
-                icon: <MenuPortScanningIcon />,
-                describe: "对 IP、IP段、域名等端口进行 SYN、指纹检测、可编写插件进行检测、满足更个性化等需求"
-            },
-            {
-                id: "2-1",
-                key: Route.Mod_Brute,
-                label: "爆破与未授权",
-                icon: <MenuBlastingAndUnauthorizedTestingIcon />,
-                hoverIcon: <MenuSolidBlastingAndUnauthorizedTestingIcon />,
-                describe: "对目标的登录账号、密码等进行爆破，在爆破前会进行未授权检测"
-            },
-            {
-                id: "2-2",
-                key: undefined,
-                label: "基础爬虫",
-                yakScripName: "基础爬虫",
-                icon: <MenuBasicCrawlerIcon />,
-                hoverIcon: <MenuSolidBasicCrawlerIcon />,
-                describe: "通过爬虫可快速了解网站的整体架构"
-            },
-            {
-                id: "2-3",
-                key: undefined,
-                label: "空间引擎: Hunter",
-                yakScripName: "空间引擎: Hunter",
-                icon: <MenuSpaceEngineHunterIcon />,
-                hoverIcon: <MenuSolidSpaceEngineHunterIcon />
-            },
-            {
-                id: "2-4",
-                key: Route.Mod_ScanPort,
-                label: "端口/指纹扫描",
-                icon: <MenuPortScanningIcon />,
-                hoverIcon: <MenuSolidPortScanningIcon />,
-                describe: "对 IP、IP段、域名等端口进行 SYN、指纹检测、可编写插件进行检测、满足更个性化等需求"
-            },
-            {
-                id: "2-5",
-                key: undefined,
-                label: "子域名收集",
-                yakScripName: "子域名收集",
-                icon: <MenuSubDomainCollectionIcon />,
-                hoverIcon: <MenuSolidSubDomainCollectionIcon />
-            },
-            {
-                id: "2-6",
-                key: undefined,
-                label: "综合目录扫描与爆破",
-                yakScripName: "综合目录扫描与爆破",
-                icon: <MenuComprehensiveCatalogScanningAndBlastingIcon />,
-                hoverIcon: <MenuSolidComprehensiveCatalogScanningAndBlastingIcon />,
-                describe: "带有内置字典的综合目录扫描与爆破"
-            }
-        ]
-    },
-    {
-        id: "3",
-        label: "专项漏洞检测",
-        isNovice: true,
-        subMenuData: [
-            {
-                id: "3-1",
-                key: Route.PoC,
-                label: "专项漏洞检测",
-                icon: <MenuSpecialVulnerabilityDetectionIcon />,
-                hoverIcon: <MenuSolidSpecialVulnerabilityDetectionIcon />,
-                describe: "通过预制漏洞源码，对特定目标进行专项漏洞检测，可以自定义新增 POC 种类"
-            }
-        ]
-    },
-    {
-        id: "4",
-        label: "插件",
-        isNovice: true,
-        subMenuData: [
-            {
-                id: "4-1",
-                key: Route.ModManager,
-                label: "插件仓库",
-                icon: <MenuPluginWarehouseIcon />,
-                hoverIcon: <MenuSolidPluginWarehouseIcon />,
-                describe: "目前插件为 6 大类型，可根据需要灵活编写插件，支持从 GitHub 加载插件"
-            },
-            {
-                id: "4-2",
-                key: Route.BatchExecutorPage,
-                label: "插件批量执行",
-                icon: <MenuPluginBatchExecutionIcon />,
-                hoverIcon: <MenuSolidPluginBatchExecutionIcon />,
-                describe: "自由选择需要的 POC 进行批量漏洞检测"
-            }
-        ]
-    },
-    // {id: "5", key: Route.PayloadManager, label: "Payload 管理", icon: <AuditOutlined />},
-    // {id: "6", key: Route.YakScript, label: "Yak Runner", icon: <CodeOutlined />},
+    {id: "5", key: Route.PayloadManager, label: "Payload 管理", icon: <AuditOutlined />},
+    {id: "6", key: Route.YakScript, label: "Yak Runner", icon: <CodeOutlined />},
     {
         id: "7",
-        label: "反连",
+        label: "反连管理",
         subMenuData: [
-            {
-                id: "7-1",
-                key: Route.ShellReceiver,
-                label: "端口监听器",
-                icon: <MenuPortListenerIcon />,
-                hoverIcon: <MenuSolidPortListenerIcon />,
-                describe: "反弹 Shell 接收工具，可以在服务器上开启一个端口，进行监听，并进行交互"
-            },
-            {
-                id: "7-2",
-                key: Route.ReverseServer_New,
-                label: "反连服务器",
-                icon: <MenuReverseConnectionServerIcon />,
-                hoverIcon: <MenuSolidReverseConnectionServerIcon />,
-                describe: "使用协议端口复用技术，同时在一个端口同时实现 HTTP / RMI / HTTPS 等协议的反连"
-            },
-            {
-                id: "7-3",
-                key: Route.DNSLog,
-                label: "DNSLog",
-                icon: <MenuDNSLogIcon />,
-                hoverIcon: <MenuSolidDNSLogIcon />,
-                describe: "自动生成一个子域名，任何查询到这个子域名的 IP 被集合展示在列表中"
-            },
-            {
-                id: "7-4",
-                key: Route.ICMPSizeLog,
-                label: "ICMP-SizeLog",
-                icon: <MenuICMPSizeLogIcon />,
-                hoverIcon: <MenuSolidICMPSizeLogIcon />,
-                describe: "使用 ping 携带特定长度数据包判定 ICMP 反连"
-            },
-            {
-                id: "7-5",
-                key: Route.TCPPortLog,
-                label: "TCP-PortLog",
-                icon: <MenuTCPPortLogIcon />,
-                hoverIcon: <MenuSolidTCPPortLogIcon />,
-                describe: "使用未开放的随机端口来判定 TCP 反连"
-            },
-            {
-                id: "7-6",
-                key: Route.PayloadGenerater_New,
-                label: "Yso-Java Hack",
-                icon: <MenuYsoJavaHackIcon />,
-                hoverIcon: <MenuSolidYsoJavaHackIcon />,
-                describe: "配置序列化 Payload 或恶意类"
-            }
+            {id: "7-1", key: Route.ShellReceiver, label: "端口监听器", icon: <MenuPortListenerIcon />},
+            {id: "7-2", key: Route.ReverseServer_New, label: "反连服务器", icon: <MenuReverseConnectionServerIcon />},
+            {id: "7-3", key: Route.DNSLog, label: "DNSLog", icon: <MenuDNSLogIcon />},
+            {id: "7-4", key: Route.ICMPSizeLog, label: "ICMP-SizeLog", icon: <MenuICMPSizeLogIcon />},
+            // {id: "7-6", key: Route.PayloadGenerater_New, label: "Yso-Java Hack", icon: <MenuYsoJavaHackIcon />},
+            {id: "7-5", key: Route.TCPPortLog, label: "TCP-PortLog", icon: <MenuTCPPortLogIcon />}
         ]
     },
     {
@@ -662,23 +553,8 @@ export const DefaultRouteMenuData: MenuDataProps[] = [
         label: "数据处理",
         isNovice: true,
         subMenuData: [
-            {
-                id: "8-1",
-                key: Route.Codec,
-                label: "Codec",
-                icon: <MenuCodecIcon />,
-                hoverIcon: <MenuSolidCodecIcon />,
-                describe:
-                    "可对数据进行各种处理（包括加密、解密、反序列化、Json 处理等等），还可通过插件自定义数据处理方法"
-            },
-            {
-                id: "8-2",
-                key: Route.DataCompare,
-                label: "数据对比",
-                icon: <MenuDataComparisonIcon />,
-                hoverIcon: <MenuSolidDataComparisonIcon />,
-                describe: "将数据进行对比，快速识别不同处"
-            }
+            {id: "8-2", key: Route.DataCompare, label: "数据对比", icon: <MenuDataComparisonIcon />},
+            {id: "8-1", key: Route.Codec, label: "Codec", icon: <MenuCodecIcon />}
         ]
     },
     {
@@ -686,62 +562,12 @@ export const DefaultRouteMenuData: MenuDataProps[] = [
         label: "数据库",
         isNovice: true,
         subMenuData: [
-            {
-                id: "9-1",
-                key: Route.DB_Report,
-                label: "报告",
-                icon: <MenuReportIcon />,
-                hoverIcon: <MenuSolidReportIcon />
-            },
-            {
-                id: "9-2",
-                key: Route.DB_ExecResults,
-                label: "插件执行结果",
-                icon: <MenuPlugExecutionResultsIcon />,
-                hoverIcon: <MenuSolidPlugExecutionResultsIcon />
-            },
-            {
-                id: "9-3",
-                key: Route.DB_Ports,
-                label: "端口资产",
-                icon: <MenuPortAssetsIcon />,
-                hoverIcon: <MenuSolidPortAssetsIcon />
-            },
-            {
-                id: "9-4",
-                key: Route.DB_Risk,
-                label: "漏洞与风险",
-                icon: <MenuVulnerabilityRiskIcon />,
-                hoverIcon: <MenuSolidVulnerabilityRiskIcon />
-            },
-            {
-                id: "9-5",
-                key: Route.DB_Domain,
-                label: "域名资产",
-                icon: <MenuDomainAssetsIcon />,
-                hoverIcon: <MenuSolidDomainAssetsIcon />
-            },
-            {
-                id: "9-6",
-                key: Route.DB_HTTPHistory,
-                label: "HTTP History",
-                icon: <MenuHTTPHistoryIcon />,
-                hoverIcon: <MenuSolidHTTPHistoryIcon />
-            },
-            // {
-            //     id: "9-7",
-            //     key: Route.AttachEngineCombinedOutput,
-            //     label: "引擎 Console",
-            //     icon: <MenuDefaultPluginIcon />,
-            //     hoverIcon: <MenuSolidDefaultPluginIcon />
-            // },
-            {
-                id: "9-8",
-                key: Route.DB_Projects,
-                label: "项目管理(Beta*)",
-                icon: <MenuDefaultPluginIcon />,
-                hoverIcon: <MenuSolidDefaultPluginIcon />
-            }
+            {id: "9-1", key: Route.DB_Report, label: "报告(Beta*)", icon: <MenuReportIcon />},
+            {id: "9-3", key: Route.DB_Ports, label: "端口资产", icon: <MenuPortAssetsIcon />},
+            {id: "9-2", key: Route.DB_ExecResults, label: "插件执行结果", icon: <MenuPlugExecutionResultsIcon />},
+            {id: "9-4", key: Route.DB_Risk, label: "漏洞与风险", icon: <MenuVulnerabilityRiskIcon />},
+            {id: "9-5", key: Route.DB_Domain, label: "域名资产", icon: <MenuDomainAssetsIcon />},
+            {id: "9-6", key: Route.DB_HTTPHistory, label: "HTTP History", icon: <MenuHTTPHistoryIcon />}
         ]
     }
 ]
