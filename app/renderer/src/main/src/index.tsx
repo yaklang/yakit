f11f76bf48d5a809f37156a391c0c9cc56f22d90
nonight <378099757@qq.com>
2022-12-07 16:28:11 +0800

    add: 1、新增了button组件的类型种类和大小种类 2、新增并整合了popover组件 3、新增并整合了modal组件 4、新增并整合了menu组件，新增hint和浅底深字样式

--- app/renderer/src/main/src/index.tsx
+++ app/renderer/src/main/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import "./yakitUI.scss"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
