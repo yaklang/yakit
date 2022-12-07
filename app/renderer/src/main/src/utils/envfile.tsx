fe379585f3a71d6f83e2fadcf38bae6c01a3f3c5
TangHang123 <1425735414@qq.com>
2022-12-07 10:43:09 +0800

    增加newUI环境变量

--- app/renderer/src/main/src/utils/envfile.tsx
+++ app/renderer/src/main/src/utils/envfile.tsx
@@ -2,10 +2,21 @@ export enum ENTERPRISE_STATUS {
     IS_ENTERPRISE_STATUS = 1
 }
 
+/**
+ *  版本变量
+ *  @readonly
+ *  @enum {number} IS_NEW_UI 新UI
+ */
+export enum EDITION_STATUS {
+    IS_NEW_UI = 2
+}
+
 export const getJuageEnvFile = () => {
     switch (process.env?.REACT_APP_PLATFORM) {
         case "enterprise":
             return ENTERPRISE_STATUS.IS_ENTERPRISE_STATUS
+        case "newUI":
+            return EDITION_STATUS.IS_NEW_UI
         default:
             return 0
     }
