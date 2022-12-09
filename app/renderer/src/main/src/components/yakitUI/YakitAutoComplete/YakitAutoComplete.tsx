43f5d6ddca067c3e22ff86437a0cbb860a62f5ce
TangHang123 <1425735414@qq.com>
2022-12-09 11:40:42 +0800

    新增YakitAutoComplete

---
+++ app/renderer/src/main/src/components/yakitUI/YakitAutoComplete/YakitAutoComplete.tsx
@@ -0,0 +1,40 @@
+import {AutoComplete} from "antd"
+import React, {useState} from "react"
+import {YakitAutoCompleteProps} from "./YakitAutoCompleteType"
+import styles from "./YakitAutoComplete.module.scss"
+import classNames from "classnames"
+
+/**
+ * @description YakitAutoCompleteProps 的属性
+ * @augments AutoCompleteProps 继承antd的 AutoCompleteProps 默认属性
+ * @param {"small" | "middle" | "large" } size  默认middle
+ */
+export const YakitAutoComplete: React.FC<YakitAutoCompleteProps> = (props) => {
+    const {size, className, ...restProps} = props
+    const [show, setShow] = useState<boolean>(false)
+    return (
+        <div
+            className={classNames(styles["yakit-auto-complete-wrapper"], {
+                [styles["yakit-auto-complete-wrapper-large"]]: size === "large",
+                [styles["yakit-auto-complete-wrapper-small"]]: size === "small",
+                [styles["yakit-auto-complete-disabled"]]: !!props.disabled
+            })}
+        >
+            <AutoComplete
+                {...restProps}
+                size='middle'
+                dropdownClassName={classNames(
+                    styles["yakit-auto-complete-popup"],
+                    {
+                        [styles["yakit-auto-complete-popup-y"]]: show
+                    },
+                    props.dropdownClassName
+                )}
+                onDropdownVisibleChange={(open) => {
+                    setShow(open)
+                    if (props.onDropdownVisibleChange) props.onDropdownVisibleChange(open)
+                }}
+            />
+        </div>
+    )
+}
