0733511a2c8ce6aeea0a6c4acac23536e9d9d97d
TangHang123 <1425735414@qq.com>
2022-12-12 10:47:30 +0800

    YakitSwitch,主题色控制

--- app/renderer/src/main/src/protected/YakEnvironment.tsx
+++ app/renderer/src/main/src/protected/YakEnvironment.tsx
@@ -378,6 +378,7 @@ const YakEnvironment: React.FC<YakEnvironmentProp> = (props) => {
                                     </Button>
                                 </Button.Group>
                                 <YakitInput style={{width: 200}} />
+                                <YakitSwitch />
                             </Space>
                         </div>
                     </Form>
