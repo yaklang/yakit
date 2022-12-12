e6f040af8c027c075773acc073cd860d14ee9374
TangHang123 <1425735414@qq.com>
2022-12-12 11:43:02 +0800

    YakitRadioButtons,主题色控制

--- app/renderer/src/main/src/components/yakitUI/YakitRadioButtons/YakitRadioButtons.tsx
+++ app/renderer/src/main/src/components/yakitUI/YakitRadioButtons/YakitRadioButtons.tsx
@@ -3,32 +3,40 @@ import React from "react"
 import {YakitRadioButtonsProps} from "./YakitRadioButtonsType"
 import styles from "./YakitRadioButtons.module.scss"
 import classNames from "classnames"
+import {EDITION_STATUS, getJuageEnvFile} from "@/utils/envfile"
+
+const IsNewUI: boolean = EDITION_STATUS.IS_NEW_UI === getJuageEnvFile()
+
+/**
+ * 更新说明
+ * 1.增加环境变量加载主题色
+ */
 
 /**
  * @description: 按钮单选框Props
- * @deprecated optionType 弃用
+ * @param optionType 弃用
  * @param {"small" | "middle" | "large" | "maxLarge"} size  默认middle
  * @augments RadioGroupProps 继承antd的 RadioGroupProps 默认属性
- * @params {string} className RadioGroup  className 
+ * @params {string} className RadioGroup  className
  */
 export const YakitRadioButtons: React.FC<YakitRadioButtonsProps> = (props) => {
     const {className, size, ...restProps} = props
     return (
-        <div>
-            <Radio.Group
-                {...restProps}
-                size='middle'
-                optionType='button'
-                className={classNames(
-                    styles["yakit-radio-buttons-middle"],
-                    {
-                        [styles["yakit-radio-buttons-max-large"]]: size === "maxLarge",
-                        [styles["yakit-radio-buttons-large"]]: size === "large",
-                        [styles["yakit-radio-buttons-small"]]: size === "small"
-                    },
-                    className
-                )}
-            />
-        </div>
+        <Radio.Group
+            {...restProps}
+            size='middle'
+            optionType='button'
+            className={classNames(
+                styles["yakit-radio-buttons-middle"],
+                {
+                    [styles["yakit-radio-buttons-newUI"]]: IsNewUI,
+                    [styles["yakit-radio-buttons-oldUI"]]: !IsNewUI,
+                    [styles["yakit-radio-buttons-max-large"]]: size === "maxLarge",
+                    [styles["yakit-radio-buttons-large"]]: size === "large",
+                    [styles["yakit-radio-buttons-small"]]: size === "small"
+                },
+                className
+            )}
+        />
     )
 }
