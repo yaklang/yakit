f957cc20b9da82f4c7739b0820d4cb0588091855
TangHang123 <1425735414@qq.com>
2022-12-21 17:13:38 +0800

    组件增加颜色变量

--- app/renderer/src/main/src/components/yakitUI/YakitAutoComplete/YakitAutoComplete.tsx
+++ app/renderer/src/main/src/components/yakitUI/YakitAutoComplete/YakitAutoComplete.tsx
@@ -3,13 +3,11 @@ import React, {useState} from "react"
 import {YakitAutoCompleteProps} from "./YakitAutoCompleteType"
 import styles from "./YakitAutoComplete.module.scss"
 import classNames from "classnames"
-import {EDITION_STATUS, getJuageEnvFile} from "@/utils/envfile"
-
-const IsNewUI: boolean = EDITION_STATUS.IS_NEW_UI === getJuageEnvFile()
 
 /**
  * 更新说明
  * 1.增加环境变量加载主题色
+ * 2.增加颜色变量
  */
 
 /**
@@ -28,8 +26,6 @@ export const YakitAutoComplete: React.FC<YakitAutoCompleteProps> = (props) => {
     return (
         <div
             className={classNames(styles["yakit-auto-complete-wrapper"], {
-                [styles["yakit-auto-complete-wrapper-newUI"]]: IsNewUI,
-                [styles["yakit-auto-complete-wrapper-oldUI"]]: !IsNewUI,
                 [styles["yakit-auto-complete-wrapper-large"]]: size === "large",
                 [styles["yakit-auto-complete-wrapper-small"]]: size === "small",
                 [styles["yakit-auto-complete-disabled"]]: !!props.disabled
@@ -41,8 +37,6 @@ export const YakitAutoComplete: React.FC<YakitAutoCompleteProps> = (props) => {
                 dropdownClassName={classNames(
                     styles["yakit-auto-complete-popup"],
                     {
-                        [styles["yakit-auto-complete-wrapper-newUI"]]: IsNewUI,
-                        [styles["yakit-auto-complete-wrapper-oldUI"]]: !IsNewUI,
                         [styles["yakit-auto-complete-popup-y"]]: show
                     },
                     props.dropdownClassName
