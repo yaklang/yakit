73d7d1ca64a526e25f6357e187fab049b8750982
TangHang123 <1425735414@qq.com>
2022-12-30 10:43:30 +0800

    yakitInput,增加文本域

--- app/renderer/src/main/src/components/yakitUI/YakitInput/YakitInputType.d.ts
+++ app/renderer/src/main/src/components/yakitUI/YakitInput/YakitInputType.d.ts
@@ -1,10 +1,14 @@
 import {InputProps} from "antd"
 import {SizeType} from "antd/lib/config-provider/SizeContext"
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { SearchProps, TextAreaProps } from "antd/lib/input"
 =======
 import { SearchProps } from "antd/lib/input"
 >>>>>>> 1ceae5c8 (增加搜索组件，两种样式中的一种，另外一种未完成)
+=======
+import { SearchProps, TextAreaProps } from "antd/lib/input"
+>>>>>>> 324951bc (yakitInput,增加文本域)
 import type {YakitSizeType} from "../YakitInputNumber/YakitInputNumberType"
 
 /**
@@ -19,6 +23,9 @@ export interface YakitInputProps extends Omit<InputProps, "size"> {
 }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 324951bc (yakitInput,增加文本域)
 /**
  * @description YakitInputSearchProps 的属性
  * @augments InputProps 继承antd的Input SearchProps 默认属性
@@ -37,9 +44,12 @@ export interface YakitInputSearchProps extends Omit<SearchProps,"size">{
 export interface InternalTextAreaProps extends TextAreaProps{
     wrapperClassName?: string
 }
+<<<<<<< HEAD
 =======
 export interface YakitInputSearchProps extends Omit<SearchProps,"size">{
     size?: YakitSizeType
     wrapperClassName?: string
 }
 >>>>>>> 1ceae5c8 (增加搜索组件，两种样式中的一种，另外一种未完成)
+=======
+>>>>>>> 324951bc (yakitInput,增加文本域)
