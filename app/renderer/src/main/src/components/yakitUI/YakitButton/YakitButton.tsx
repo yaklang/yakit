645a7cc960c08d6139e1ba4e0c822275ebfe03db
nonight <378099757@qq.com>
2023-01-05 14:58:12 +0800

    yakit-button调整部分颜色变量和样式

--- app/renderer/src/main/src/components/yakitUI/YakitButton/YakitButton.tsx
+++ app/renderer/src/main/src/components/yakitUI/YakitButton/YakitButton.tsx
@@ -6,11 +6,12 @@ import styles from "./yakitButton.module.scss"
 
 /**
  * 更新说明
- * 1、新增yakit-button组件，新增按钮多种样式(参考原型稿 button 组件):
+ * 1、新增yakit-button组件
  *     1)主题色-primary(默认值)/outline1/outline2/text/secondary1/secondary2/success/warning/danger/serious
- *     2)大小-默认样式(不填)/big/small/max
+ *     2)大小-默认样式(不填)/large/small/max
  * 2、修改yakitButon，text有阴影问题和不居中问题
  * 3、修改disabled时，出现的一些伪类样式问题
+ * 4、新增id=yakit-button，方便外部直接书写样式
  */
 
 export interface YakitButtonProp extends Omit<ButtonProps, "size" | "type"> {
@@ -26,7 +27,7 @@ export interface YakitButtonProp extends Omit<ButtonProps, "size" | "type"> {
         | "danger"
         | "serious"
     themeClass?: string
-    size?: "big" | "small" | "max"
+    size?: "large" | "small" | "max"
 }
 
 /**
@@ -37,31 +38,31 @@ export interface YakitButtonProp extends Omit<ButtonProps, "size" | "type"> {
  * 注：1、需要用户自定义一个样式类，将如下全部或部分变量在类内进行定义 2、暂时需要给修改的变量值加上 !important
  *
  *
- *  --yakit-wave-shadow-color: 按钮动画边框阴影色
+ *  --button-wave-shadow-color: 按钮动画边框阴影色
  *
- *  --yakit-background: 按钮背景色
+ *  --button-background: 按钮背景色
  *
- *  --yakit-color: 按钮字体色
+ *  --button-color: 按钮字体色
  *
- *  --yakit-border-color: 按钮边框色
+ *  --button-border-color: 按钮边框色
  *
- *  --yakit-hover-background: 按钮 hover 背景色
+ *  --button-hover-background: 按钮 hover 背景色
  *
- *  --yakit-hover-color: 按钮 hover 字体色
+ *  --button-hover-color: 按钮 hover 字体色
  *
- *  --yakit-hover-border-color: 按钮 hover 边框色
+ *  --button-hover-border-color: 按钮 hover 边框色
  *
- *  --yakit-disable-background: 按钮 disable 背景色
+ *  --button-disable-background: 按钮 disable 背景色
  *
- *  --yakit-disable-color: 按钮 disable 字体色
+ *  --button-disable-color: 按钮 disable 字体色
  *
- *  --yakit-disable-border-color: 按钮 disable 边框色
+ *  --button-disable-border-color: 按钮 disable 边框色
  *
- *  --yakit-press-background: 按钮 press 背景色
+ *  --button-press-background: 按钮 press 背景色
  *
- *  --yakit-press-color: 按钮 press 字体色
+ *  --button-press-color: 按钮 press 字体色
  *
- *  --yakit-press-border-color: 按钮 press 边框色
+ *  --button-press-border-color: 按钮 press 边框色
  */
 export const YakitButton: React.FC<YakitButtonProp> = React.memo((props) => {
     const {size, type, themeClass, children, className, onMouseDown, onMouseUp, ...resePopover} = props
@@ -85,7 +86,7 @@ export const YakitButton: React.FC<YakitButtonProp> = React.memo((props) => {
 
     const sizeClass = useMemo(() => {
         if (!size) return "yakit-button-size"
-        if (size === "big") return "yakit-button-big-size"
+        if (size === "large") return "yakit-button-large-size"
         if (size === "small") return "yakit-button-small-size"
         if (size === "max") return "yakit-button-max-size"
         return "yakit-button-size"
@@ -97,6 +98,7 @@ export const YakitButton: React.FC<YakitButtonProp> = React.memo((props) => {
                 {...resePopover}
                 size='middle'
                 type='default'
+                id='yakit-button'
                 className={classnames(
                     styles[sizeClass],
                     styles["yakit-button"],
