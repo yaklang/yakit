ec6766ca133d08719d2ec10a57c72fece95d9488
z3 <z3r0ne@163.com>
2023-02-27 16:45:38 +0800

    interactive yak-runner

--- app/main/handlers/execYak.js
+++ app/main/handlers/execYak.js
@@ -192,4 +192,48 @@ module.exports = (win, getClient) => {
         let stream = getClient().AttachCombinedOutput(params);
         handlerHelper.registerHandler(win, stream, streamAttachCombinedOutputMap, token)
     })
+
+    const streamInteractiveRunYakCodeMap = new Map()
+    ipcMain.handle("cancel-InteractiveRunYakCode", handlerHelper.cancelHandler(streamInteractiveRunYakCodeMap))
+    ipcMain.handle("InteractiveRunYakCodeWrite", (e, token,data) => {
+        let stream = streamInteractiveRunYakCodeMap.get(token)
+        if (!!stream){
+            stream.write(data)
+        }
+    })
+    ipcMain.handle("InteractiveRunYakCode", (e, token) => {
+        let stream = getClient().CreateYaklangShell()
+        handlerHelper.registerHandler(win, stream, streamInteractiveRunYakCodeMap, token)
+
+        streamInteractiveRunYakCodeMap.set(token, stream)
+        stream.on("data", (data) => {
+            if (!win) {
+                return
+            }
+            if (data.Scope && data.Scope.length > 0){
+                win.webContents.send(`${token}-data-scope`, data.Scope)    
+            }
+            if (data.RawResult){
+                if (Boolean(data.RawResult?.IsMessage) && data.RawResult.Message.toString() === "signal-interactive-exec-end"){
+                    win.webContents.send(`${token}-exec-end`)
+                }else{
+                    win.webContents.send(`${token}-data`, data.RawResult)
+                }
+                
+            }
+        })
+        stream.on("error", (error) => {
+            if (!win) {
+                return
+            }
+            win.webContents.send(`${token}-error`, error && error.details)
+        })
+        stream.on("end", () => {
+            streamInteractiveRunYakCodeMap.delete(token)
+            if (!win) {
+                return
+            }
+            win.webContents.send(`${token}-end`)
+        })
+    })
 }
