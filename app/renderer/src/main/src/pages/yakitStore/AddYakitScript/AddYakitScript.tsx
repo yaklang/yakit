2f8510a5ec3f254a8a5554626c817823e894ae65
nonight <378099757@qq.com>
2023-03-02 15:27:08 +0800

    feature & fix: adjust some func (#650)
    * fix: 修复'漏洞与风险'和'域名资产'页里表格内容过长导致的宽度过大撑开的问题
    
    * fix: 修复http-history页面详情高度超出界限问题
    
    * feat:新建插件后关闭tab页并打开本地插件仓库同时刷新
    
    * fix: 修复http-history页面websocket详情无法滚动情况
    
    * fix: 修改插件界面的操作按钮显隐调整
    
    * 调整逻辑

--- app/renderer/src/main/src/pages/yakitStore/AddYakitScript/AddYakitScript.tsx
+++ app/renderer/src/main/src/pages/yakitStore/AddYakitScript/AddYakitScript.tsx
@@ -1,15 +1,11 @@
 import {YakScriptCreatorForm} from "@/pages/invoker/YakScriptCreator"
-import {Route} from "@/routes/routeSpec"
-import {Modal} from "antd"
-import React, {useEffect, useState} from "react"
+import React from "react"
 import "./AddYakitScript.scss"
-
-const {ipcRenderer} = window.require("electron")
 
 export const AddYakitScript: React.FC = (props) => {
     return (
         <div className='yak-form-content'>
-            <YakScriptCreatorForm onCreated={(s) => {}} />
+            <YakScriptCreatorForm onCreated={(s) => {}} isCreate={true} />
         </div>
     )
 }
