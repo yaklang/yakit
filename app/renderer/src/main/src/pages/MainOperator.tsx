fb6ffc500c87ca45a022ef8de781e6251f4802a5
TangHang123 <1425735414@qq.com>
2023-06-13 17:16:44 +0800

    fuzzer缓存代码释放

--- app/renderer/src/main/src/pages/MainOperator.tsx
+++ app/renderer/src/main/src/pages/MainOperator.tsx
@@ -930,25 +930,25 @@ const Main: React.FC<MainProp> = React.memo((props) => {
         fuzzerList.current.set(key, param)
         saveFuzzerList()
     }
-    // useEffect(() => {
-    //     ipcRenderer.on("fetch-fuzzer-setting-data", (e, res: any) => {
-    //         try {
-    //             updateFuzzerList(res.key, {...(fuzzerList.current.get(res.key) || {}), ...JSON.parse(res.param)})
-    //         } catch (error) {
-    //             failed("webFuzzer数据缓存失败：" + error)
-    //         }
-    //     })
-    //     // 开发环境不展示fuzzer缓存
-    //     ipcRenderer
-    //         .invoke("is-dev")
-    //         .then((flag) => {})
-    //         .finally(() => {
-    //             fetchFuzzerList()
-    //         })
-    //     return () => {
-    //         ipcRenderer.removeAllListeners("fetch-fuzzer-setting-data")
-    //     }
-    // }, [])
+    useEffect(() => {
+        ipcRenderer.on("fetch-fuzzer-setting-data", (e, res: any) => {
+            try {
+                updateFuzzerList(res.key, {...(fuzzerList.current.get(res.key) || {}), ...JSON.parse(res.param)})
+            } catch (error) {
+                failed("webFuzzer数据缓存失败：" + error)
+            }
+        })
+        // 开发环境不展示fuzzer缓存
+        ipcRenderer
+            .invoke("is-dev")
+            .then((flag) => {})
+            .finally(() => {
+                fetchFuzzerList()
+            })
+        return () => {
+            ipcRenderer.removeAllListeners("fetch-fuzzer-setting-data")
+        }
+    }, [])
 
     // 加载补全
     useEffect(() => {
