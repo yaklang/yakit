0ccb7a28f241af1a1a0ed9606bec1f65e50ed872
v1ll4n <v1ll4n@qq.com>
2023-07-04 15:20:32 +0800

    优化靶场管理器

--- app/renderer/src/main/src/pages/vulinbox/VulinboxManager.tsx
+++ app/renderer/src/main/src/pages/vulinbox/VulinboxManager.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import {AutoCard} from "@/components/AutoCard";
 import {EngineConsole} from "@/pages/engineConsole/EngineConsole";
 import {failed, info, success, yakitNotify} from "@/utils/notification";
-import {Form, Popconfirm, Space, Tag} from "antd";
+import {Form, Popconfirm, Progress, Space, Tag} from "antd";
 import {YakitButton} from "@/components/yakitUI/YakitButton/YakitButton";
 import useHoldingIPCRStream from "@/hook/useHoldingIPCRStream";
 import {randomString} from "@/utils/randomUtil";
@@ -11,6 +11,13 @@ import {InputInteger, InputItem, SwitchItem} from "@/utils/inputUtil";
 import {showYakitModal} from "@/components/yakitUI/YakitModal/YakitModalConfirm";
 import {PluginResultUI} from "@/pages/yakitStore/viewers/base";
 import {DefaultPluginResultUI} from "@/pages/invoker/ExecYakScript";
+import {ExecResult} from "@/pages/invoker/schema";
+import {StringToUint8Array, Uint8ArrayToString} from "@/utils/str";
+import {useGetState} from "ahooks";
+import styles from "@/pages/screenRecorder/ScreenRecorderPage.module.scss";
+import {ChromeFrameSvgIcon, ChromeSvgIcon} from "@/assets/newIcon";
+import {CheckOutlined} from "@ant-design/icons";
+import {openExternalWebsite} from "@/utils/openWebsite";
 
 export interface VulinboxManagerProp {
 
@@ -22,8 +29,31 @@ export const VulinboxManager: React.FC<VulinboxManagerProp> = (props) => {
     const [available, setAvailable] = useState(false);
     const [started, setStarted] = useState(false);
     const [token, setToken] = useState(randomString(60));
-    const [infoStat] = useHoldingIPCRStream("vulinbox-started", "StartVulinbox", token);
-    const [currentParams, setCurrentParams] = useState<StartVulinboxParams>();
+    const [currentParams, setCurrentParams] = useState<StartVulinboxParams>({
+        Host: "127.0.0.1",
+        Port: 8787,
+        NoHttps: true,
+        SafeMode: false,
+    });
+
+    useEffect(() => {
+        ipcRenderer.on(`${token}-data`, async (e, data: ExecResult) => {
+
+        })
+        ipcRenderer.on(`${token}-error`, (e, error) => {
+            failed(`[StartVulinbox] error:  ${error}`)
+        })
+        ipcRenderer.on(`${token}-end`, (e, data) => {
+            info("[StartVulinbox] finished")
+            setTimeout(() => setStarted(false), 300)
+        })
+        return () => {
+            ipcRenderer.invoke("cancel-StartVulinbox", token)
+            ipcRenderer.removeAllListeners(`${token}-data`)
+            ipcRenderer.removeAllListeners(`${token}-error`)
+            ipcRenderer.removeAllListeners(`${token}-end`)
+        }
+    }, [])
 
     useEffect(() => {
         ipcRenderer.invoke("IsVulinboxReady", {}).then(() => {
@@ -41,7 +71,15 @@ export const VulinboxManager: React.FC<VulinboxManagerProp> = (props) => {
     return <div style={{height: "100%", width: "100%", overflow: "hidden"}}>
         <AutoCard size={"small"} bordered={true} title={<Space>
             <div>Vulinbox 管理器</div>
-            {available ? <Tag color={"green"}>安装成功</Tag> : <Tag color={"red"}>未安装</Tag>}
+            {available ? <>
+                <Tag color={"green"}>安装成功</Tag>
+                {currentParams && <YakitButton type='outline2' onClick={() => {
+                    info("使用 Chrome 打开靶场")
+                    openExternalWebsite(`${currentParams?.NoHttps ? "http://" : "https//"}${currentParams?.Host}:${currentParams?.Port}`)
+                }}>
+                    <ChromeSvgIcon/>
+                </YakitButton>}
+            </> : <Tag color={"red"}>未安装</Tag>}
             {available && (
                 started ? <Popconfirm title={"确定要关闭靶场进程吗？"} onConfirm={() => {
                         ipcRenderer.invoke("cancel-StartVulinbox", token).then(() => {
@@ -56,7 +94,7 @@ export const VulinboxManager: React.FC<VulinboxManagerProp> = (props) => {
                             content: (
                                 <div style={{marginTop: 20, marginLeft: 20}}>
                                     <VulinboxStart onSubmit={param => {
-                                        ipcRenderer.invoke("StartVulinbox", {}, token).then(() => {
+                                        ipcRenderer.invoke("StartVulinbox", param, token).then(() => {
                                             setCurrentParams(param)
                                             info("启动靶场成功")
                                             setStarted(true)
@@ -81,6 +119,10 @@ export const VulinboxManager: React.FC<VulinboxManagerProp> = (props) => {
                     title: "安装靶场",
                     width: "50%",
                     height: 500,
+                    onOk: () => {
+                        m.destroy()
+                    },
+                    cancelButtonProps: {hidden: true},
                     content: (
                         <div style={{margin: 24}}>
                             <InstallVulinboxPrompt onFinished={() => {
@@ -145,23 +187,33 @@ export interface InstallVulinboxPromptProp {
 
 export const InstallVulinboxPrompt: React.FC<InstallVulinboxPromptProp> = (props) => {
     const [token, setToken] = useState(randomString(60));
-    const [infoState] = useHoldingIPCRStream("vulinbox-installed", "InstallVulinbox", token, () => {
-        setLoading(false)
-        let m = showYakitModal({
-            title: "安装完毕",
-            content: <div style={{margin: 24}}>
-                <p>安装完毕，可以在 Vulinbox 管理器中启动靶场</p>
-            </div>,
-            onOk: () => {
-                m.destroy()
-                props.onFinished()
-            },
-            onCancel: () => {
-                m.destroy()
-                props.onFinished()
+    const [data, setData, getData] = useGetState<string[]>([]);
+    const [percent, setPercent] = useState(0);
+
+    useEffect(() => {
+        ipcRenderer.on(`${token}-data`, async (e, data: ExecResult) => {
+            if (data.Progress > 0) {
+                setPercent(Math.ceil(data.Progress))
+                return
             }
+            if (!data.IsMessage) {
+                return
+            }
+            setData([...getData(), Uint8ArrayToString(data.Message)])
         })
-    });
+        ipcRenderer.on(`${token}-error`, (e, error) => {
+            failed(`[InstallVulinbox] error:  ${error}`)
+        })
+        ipcRenderer.on(`${token}-end`, (e, data) => {
+            info("[InstallVulinbox] finished")
+        })
+        return () => {
+            ipcRenderer.invoke("cancel-InstallVulinbox", token)
+            ipcRenderer.removeAllListeners(`${token}-data`)
+            ipcRenderer.removeAllListeners(`${token}-error`)
+            ipcRenderer.removeAllListeners(`${token}-end`)
+        }
+    }, [])
     const [loading, setLoading] = useState(false)
 
     useEffect(() => {
@@ -174,5 +226,19 @@ export const InstallVulinboxPrompt: React.FC<InstallVulinboxPromptProp> = (props
         }
     }, [])
 
-    return <DefaultPluginResultUI infoState={infoState} loading={loading}/>
+    return <Space direction={"vertical"}>
+        <div className={styles["download-progress"]}>
+            <Progress
+                strokeColor='#F28B44'
+                trailColor='#F0F2F5'
+                percent={percent}
+                format={(percent) => `已下载 ${percent}%`}
+            />
+        </div>
+        <div className={styles["download-progress-messages"]}>
+            {data.map((i) => {
+                return <p>{i}</p>
+            })}
+        </div>
+    </Space>
 };
