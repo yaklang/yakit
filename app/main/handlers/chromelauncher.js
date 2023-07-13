2435e316944b7525ce3b103d68434dae9a2cf24f
Youngster_yj <907985037@qq.com>
2023-07-13 17:08:28 +0800

    解决mac获取ChromePath卡顿
    fix: 解决卡顿
    
    
    -

--- app/main/handlers/chromelauncher.js
+++ app/main/handlers/chromelauncher.js
@@ -1,15 +1,16 @@
-const {ipcMain} = require("electron");
-const {launch, killAll, getChromePath} = require("chrome-launcher");
+const {ipcMain} = require("electron")
+const {launch, killAll, getChromePath} = require("chrome-launcher")
+const fs = require("fs")
 module.exports = (win, getClient) => {
-    let started = false;
+    let started = false
     ipcMain.handle("IsChromeLaunched", async () => {
         return started
     })
 
     ipcMain.handle("LaunchChromeWithParams", async (e, params) => {
-        const {port, host, chromePath} = params;
-        const portInt = parseInt(`${port}`);
-        const hostRaw = `${host}`;
+        const {port, host, chromePath} = params
+        const portInt = parseInt(`${port}`)
+        const hostRaw = `${host}`
         if (hostRaw === "undefined" || hostRaw.includes("/") || hostRaw.split(":").length > 1) {
             throw Error(`host: ${hostRaw} is invalid or illegal`)
         }
@@ -27,40 +28,71 @@ module.exports = (win, getClient) => {
                 `--ignore-certificate-errors`,
                 `--ignore-urlfetcher-cert-requests`,
                 `--disable-webrtc`,
-                `--force-webrtc-ip-handling-policy=default_public_interface_only`,
+                `--force-webrtc-ip-handling-policy=default_public_interface_only`
             ]
         }
         if (chromePath) {
             launchOpt["chromePath"] = chromePath
         }
-        return launch(launchOpt).then(chrome => {
-            chrome.process.on('exit', () => {
+        return launch(launchOpt).then((chrome) => {
+            chrome.process.on("exit", () => {
                 // 在这里执行您想要的操作，当所有chrome实例都关闭时
-                started=false
+                started = false
             })
             started = true
             return ""
         })
     })
 
-    let existedChromePath = "";
-    let executedGetChromePath = false;
-    ipcMain.handle("GetChromePath", async () => {
-        if (executedGetChromePath) {
-            return existedChromePath
+    function canAccess(file) {
+        if (!file) {
+            return false;
         }
         try {
-            existedChromePath = getChromePath()
-            return existedChromePath
+            fs.accessSync(file);
+            return true;
+        }
+        catch (e) {
+            return false;
+        }
+    }
+
+    function darwinFast() {
+        const priorityOptions = [
+            process.env.CHROME_PATH,
+            process.env.LIGHTHOUSE_CHROMIUM_PATH,
+            '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
+            '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+        ];
+        for (const chromePath of priorityOptions) {
+            if (chromePath && canAccess(chromePath))
+                return chromePath;
+        }
+        return null;
+    }
+
+    const judgePath = () => {
+        switch (process.platform) {
+            // mac存在卡顿问题 因此需单独处理
+            case "darwin":
+                return darwinFast()
+            case "win32":
+                return getChromePath()
+            case "linux":
+                return getChromePath()
+        }
+    }
+
+    ipcMain.handle("GetChromePath", async () => {
+        try {
+            return judgePath()
         } catch (e) {
             return null
-        } finally {
-            executedGetChromePath = true;
         }
     })
 
     ipcMain.handle("StopAllChrome", async (e) => {
-        started = false;
+        started = false
         return killAll()
     })
 }
