97ae37f6b51a88b967b3765de191e62de75e42a1
Youngster_yj <907985037@qq.com>
2023-07-14 17:12:48 +0800

    暂存

--- app/main/api/upload.js
+++ app/main/api/upload.js
@@ -2,17 +2,19 @@ const {httpApi} = require("../httpServer")
 const {ipcMain} = require("electron")
 const fs = require("fs")
 const FormData = require("form-data")
+const {Readable} = require("stream")
 
 module.exports = (win, getClient) => {
     ipcMain.handle("upload-img", async (event, params) => {
         const {path, type} = params
         // 创建数据流
         // console.log('time1',new Date().getHours(),new Date().getMinutes(),new Date().getSeconds());
-        const readerStream = fs.createReadStream(path)// 可以像使用同步接口一样使用它。
+        const readerStream = fs.createReadStream(path) // 可以像使用同步接口一样使用它。
+        console.log('3333',JSON.stringify(readerStream))
         const formData = new FormData()
         formData.append("file_name", readerStream)
         formData.append("type", type)
-        const res=httpApi(
+        const res = httpApi(
             "post",
             "upload/img",
             formData,
@@ -28,10 +30,10 @@ module.exports = (win, getClient) => {
     ipcMain.handle("upload-project", async (event, params) => {
         const {path} = params
         // 创建数据流
-        const readerStream = fs.createReadStream(path)// 可以像使用同步接口一样使用它。
+        const readerStream = fs.createReadStream(path) // 可以像使用同步接口一样使用它。
         const formData = new FormData()
         formData.append("projectFile", readerStream)
-        const res=httpApi(
+        const res = httpApi(
             "post",
             "import/project",
             formData,
@@ -42,36 +44,39 @@ module.exports = (win, getClient) => {
     })
 
     ipcMain.handle("yak-install-package", async (event, params) => {
-        const {path} = params
-        // 创建数据流
-        const readerStream = fs.createReadStream(path)// 可以像使用同步接口一样使用它。
-        readerStream.on('data', (chunk) => {
-            // 处理读取的分片数据
-            console.log('Received a chunk of data:');
-            console.log(chunk.toString());
-          });
-          
-          readerStream.on('end', () => {
-            // 读取完成的回调
-            console.log('File reading completed.');
-          });
-          
-          readerStream.on('error', (error) => {
-            // 读取过程中出错的回调
-            console.error('An error occurred while reading the file.');
-            console.error(error);
-          });
-        // const formData = new FormData()
-        // formData.append("installPackage", readerStream)
-        // const res=httpApi(
-        //     "post",
-        //     "yak/install/package",
-        //     formData,
-        //     {"Content-Type": `multipart/form-data; boundary=${formData.getBoundary()}`},
-        //     false,
-        //     30*60*1000
-        // )
-        // return res
+        const {path, size} = params
+        // 每块分片大小
+        const chunkSize = 30 * 1024 * 1024 // 每个分片的大小，这里设置为30MB
+        // 计算分片总数
+        const totalChunk = Math.ceil(size / chunkSize)
+
+        for (let chunkIndex = 0; chunkIndex < totalChunk; chunkIndex++) {
+            const start = chunkIndex * chunkSize
+            const end = Math.min(start + chunkSize, size)
+            // 创建当前分片的读取流
+            const chunkStream = fs.createReadStream(path,{start,end})
+            const formData = new FormData()
+            formData.append("file", chunkStream)
+            formData.append("index", chunkIndex + 1)
+            formData.append("totalChunks", totalChunk)
+            // formData.append("hash", totalChunk)
+            await httpApi(
+                "post",
+                "upload/install/package",
+                formData,
+                {"Content-Type": `multipart/form-data; boundary=${formData.getBoundary()}`},
+                false,
+                30 * 60 * 1000
+            )
+                .then((res) => {
+                    win.webContents.send("call-back-upload-yakit-ee", res)
+                    console.log("yak-install-package", res, chunkIndex + 1)
+                })
+                .catch((err) => {
+                    console.log("文件上传失败", err)
+                })
+        }
+        console.log("File upload completed.")
     })
 
     ipcMain.handle("get-folder-under-files", async (event, params) => {
