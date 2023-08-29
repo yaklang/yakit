a2891fd1e5082acb69b6b65fda0633730081dbfe
go0p <gp827782797@qq.com>
2023-08-29 17:41:50 +0800

    fix(wsm) improve the UI

--- app/renderer/src/main/src/pages/webShell/WebShellViewer.tsx
+++ app/renderer/src/main/src/pages/webShell/WebShellViewer.tsx
@@ -3,18 +3,24 @@ import {YakitSpin} from "@/components/yakitUI/YakitSpin/YakitSpin";
 import styles from "@/pages/cve/CVETable.module.scss";
 import fuzzerStyles from "@/pages/fuzzer/HttpQueryAdvancedConfig/HttpQueryAdvancedConfig.module.scss";
 import React, {useEffect, useState} from "react";
-import {useMemoizedFn} from "ahooks";
+import {useCreation, useMemoizedFn} from "ahooks";
 import {PaginationSchema} from "@/pages/invoker/schema";
 import {WebShellTable} from "@/pages/webShell/WebShellTable";
-import {defQueryCVERequest, QueryCVERequest} from "@/pages/cve/CVEViewer";
 import {YakitSwitch} from "@/components/yakitUI/YakitSwitch/YakitSwitch";
-import {YakitCheckableTagList} from "@/components/YakitCheckableTagList/YakitCheckableTagList";
+import {Divider, Form, Tooltip} from "antd";
+import {FromLayoutProps} from "@/pages/invoker/YakScriptCreator";
+import YakitCollapse from "@/components/yakitUI/YakitCollapse/YakitCollapse";
+import {YakitButton} from "@/components/yakitUI/YakitButton/YakitButton";
+import {HollowLightningBoltIcon, InformationCircleIcon, PlusSmIcon} from "@/assets/newIcon";
+import {YakitInputNumber} from "@/components/yakitUI/YakitInputNumber/YakitInputNumber";
+import {CustomCodecList} from "@/pages/webShell/CustomCodec";
 
 
 export interface WebShellManagerViewerProp {
 }
 
 const {ipcRenderer} = window.require("electron")
+const {YakitPanel} = YakitCollapse
 
 
 export const WebShellViewer: React.FC<WebShellManagerViewerProp> = (props) => {
@@ -79,30 +85,76 @@ const WebShellQuery: React.FC<WebShellQueryProp> = (props) => {
         props.onChange(params)
     }, [params])
 
+    const [activeKey, setActiveKey] = useState<string[]>() // Collapse打开的key
+    const onSwitchCollapse = useMemoizedFn((key) => {
+        setActiveKey(key)
+    })
+    const [customCodecList, setCustomCodecList] = useState<string[]>(["json","xx ","我"]) // Collapse打开的key
+
+
     return (
         <>
             <div className={fuzzerStyles["http-query-advanced-config"]}>
                 <div className={fuzzerStyles["advanced-config-heard"]}>
                     <span>高级设置</span>
-                    <YakitSwitch wrapperClassName={fuzzerStyles["btn-padding-right-0"]} checked={advancedQuery} onChange={setAdvancedQuery}/>
+                    <YakitSwitch wrapperClassName={fuzzerStyles["btn-padding-right-0"]} checked={advancedQuery}
+                                 onChange={setAdvancedQuery}/>
                 </div>
-                <div className={styles["cve-query-body"]}>
-                    <div className={styles["cve-query-text"]}>
-                        <span>编解码配置</span>
-                    </div>
-                    <div className={styles["cve-query-item"]}>
-                        <div>利用路径</div>
+                <Form
+                    colon={false}
+                    size='small'
+                    labelCol={{ span: 10 }}
+                    wrapperCol={{ span: 14 }}
+                    style={{ overflowY: "auto" }}
+                >
+                    <YakitCollapse
+                        activeKey={activeKey}
+                        onChange={(key) => onSwitchCollapse(key)}
+                        destroyInactivePanel={true}
+                        type='grey'
+                    >
+                        <YakitPanel
+                            header={
+                                <div className={fuzzerStyles["matchers-panel"]}>
+                                    WebShell 编解码器
+                                    <div className={fuzzerStyles["matchers-number"]}>{customCodecList?.length}</div>
+                                </div>
+                            }
+                            key='数据提取器'
+                            extra={
+                                <>
+                                    <Divider type='vertical' style={{ margin: 0 }} />
+                                    <YakitButton
+                                        type='text'
+                                        size='small'
+                                        onClick={(e) => {
+                                            e.stopPropagation()
+                                            if (activeKey?.findIndex((ele) => ele === "数据提取器") === -1) {
+                                                onSwitchCollapse([...activeKey, "数据提取器"])
+                                            }
+                                            // onAddMatchingAndExtractionCard("extractors")
+                                        }}
+                                        className={fuzzerStyles["btn-padding-right-0"]}
+                                    >
+                                        添加/调试
+                                        <HollowLightningBoltIcon />
+                                    </YakitButton>
+                                </>
+                            }
+                        >
+                            <CustomCodecList
+                                customCodecValue={ { customCodecList }}
+                                onAdd={() => {
+                                    console.log(1111)}}
+                                onRemove={()=>{
+                                    console.log(2222)}}
+                                onEdit={()=>{
+                                    console.log(3333)}}
+                            />
+                        </YakitPanel>
+                    </YakitCollapse>
 
-                    </div>
-                    <div className={styles["cve-query-item"]}>
-                        <div>利用难度</div>
-
-                    </div>
-                    <div className={styles["cve-query-item"]}>
-                        <div>漏洞级别</div>
-
-                    </div>
-                </div>
+                </Form>
 
             </div>
         </>
