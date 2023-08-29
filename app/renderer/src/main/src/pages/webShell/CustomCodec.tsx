9e63b2d1b2dba0fbaddd1b52e5f81631b354a9af
go0p <gp827782797@qq.com>
2023-08-29 23:16:14 +0800

    add(wsm) improve the UI

--- app/renderer/src/main/src/pages/webShell/CustomCodec.tsx
+++ app/renderer/src/main/src/pages/webShell/CustomCodec.tsx
@@ -1,10 +1,27 @@
-import styles from "@/pages/fuzzer/HttpQueryAdvancedConfig/HttpQueryAdvancedConfig.module.scss";
+import httpQueryStyles from "@/pages/fuzzer/HttpQueryAdvancedConfig/HttpQueryAdvancedConfig.module.scss";
+import matcherStyles from "@/pages/fuzzer/MatcherAndExtractionCard/MatcherAndExtraction.module.scss";
 import {YakitButton} from "@/components/yakitUI/YakitButton/YakitButton";
-import {HollowLightningBoltIcon, PlusIcon, TrashIcon} from "@/assets/newIcon";
-import React, { ReactNode, useState } from "react";
-import {Form, Tooltip} from "antd";
+import {HollowLightningBoltIcon, PlusIcon, QuestionMarkCircleIcon, RemoveIcon, TrashIcon} from "@/assets/newIcon";
+import React, {ReactNode, useContext, useEffect, useMemo, useState} from "react";
+import {Button, Form, Tooltip} from "antd";
 import classNames from "classnames";
 import {TerminalPopover} from "@/pages/fuzzer/HttpQueryAdvancedConfig/HttpQueryAdvancedConfig";
+import {YakitDrawer} from "@/components/yakitUI/YakitDrawer/YakitDrawer";
+import {
+    defMatcherAndExtractionCode,
+    MatcherAndExtraction,
+    MatcherAndExtractionCard
+} from "@/pages/fuzzer/MatcherAndExtractionCard/MatcherAndExtractionCard";
+import {MainOperatorContext} from "@/pages/layout/MainContext";
+import {YakitResizeBox} from "@/components/yakitUI/YakitResizeBox/YakitResizeBox";
+import {NewHTTPPacketEditor, YakEditor} from "@/utils/editors";
+import {StringToUint8Array, Uint8ArrayToString} from "@/utils/str";
+import mitmStyles from "@/pages/mitm/MITMRule/MITMRule.module.scss";
+import {openExternalWebsite} from "@/utils/openWebsite";
+import {RuleExportAndImportButton} from "@/pages/mitm/MITMRule/MITMRule";
+import {useMemoizedFn} from "ahooks";
+import {YakitSelect} from "@/components/yakitUI/YakitSelect/YakitSelect";
+import {WebShellDetail} from "@/pages/webShell/models";
 
 interface CustomCodecValueProps {
     customCodecList: string[]
@@ -24,11 +41,10 @@ export const CustomCodecList: React.FC<CustomCodecListProps> = React.memo((props
         <>
             <Form.Item name='customCodec' noStyle>
                 {customCodecList.map((item, index) => (
-                    <div className={styles["matchersList-item"]} key={`${item}-${index}`}>
-                        <div className={styles["matchersList-item-heard"]}>
-                            <span className={styles["item-id"]}>{item || `data_${index}`}</span>
-                            <span>[{customCodecList.find((e) => e)}]</span>
-                            <span className={styles["item-number"]}>{item}</span>
+                    <div className={httpQueryStyles["matchersList-item"]} key={`${item}-${index}`}>
+                        <div className={httpQueryStyles["matchersList-item-heard"]}>
+                            <span className={httpQueryStyles["item-number"]}>{index}</span>
+                            <span style={{"color": "#f69c5d"}}>{item}</span>
                         </div>
                         <CustomCodecListItemOperate
                             onRemove={() => onRemove(index)}
@@ -46,7 +62,7 @@ export const CustomCodecList: React.FC<CustomCodecListProps> = React.memo((props
                         type='outline2'
                         onClick={() => onAdd()}
                         icon={<PlusIcon/>}
-                        className={styles["plus-button-bolck"]}
+                        className={httpQueryStyles["plus-button-bolck"]}
                         block
                     >
                         添加
@@ -65,19 +81,19 @@ interface CustomCodecListItemOperateProps {
 
 const CustomCodecListItemOperate: React.FC<CustomCodecListItemOperateProps> = React.memo(
     (props) => {
-        const { onRemove, onEdit, popoverContent } = props
+        const {onRemove, onEdit, popoverContent} = props
         const [visiblePopover, setVisiblePopover] = useState<boolean>(false)
         return (
             <div
-                className={classNames(styles["matchersList-item-operate"], {
-                    [styles["matchersList-item-operate-hover"]]: visiblePopover
+                className={classNames(httpQueryStyles["matchersList-item-operate"], {
+                    [httpQueryStyles["matchersList-item-operate-hover"]]: visiblePopover
                 })}
             >
-                <TrashIcon className={styles["trash-icon"]} onClick={() => onRemove()} />
+                <TrashIcon className={httpQueryStyles["trash-icon"]} onClick={() => onRemove()}/>
 
                 <Tooltip title='调试'>
                     <HollowLightningBoltIcon
-                        className={styles["hollow-lightningBolt-icon"]}
+                        className={httpQueryStyles["hollow-lightningBolt-icon"]}
                         onClick={() => {
                             onEdit()
                         }}
@@ -92,3 +108,167 @@ const CustomCodecListItemOperate: React.FC<CustomCodecListItemOperateProps> = Re
         )
     }
 )
+
+interface CustomCodecEditorProps {
+
+    title: string
+    visibleDrawer: boolean
+    onClose: () => void
+}
+
+export const CustomCodecEditor: React.FC<CustomCodecEditorProps> = React.memo((props) => {
+    const {title, visibleDrawer, onClose} = props
+    const {tabMenuHeight} = useContext(MainOperatorContext)
+    const heightDrawer = useMemo(() => {
+        return tabMenuHeight - 40
+    }, [tabMenuHeight])
+    const onOkImport = useMemoizedFn(() => {
+        console.log("onOkImport")
+    })
+    const onSaveToDataBase = useMemoizedFn(() => {
+        console.log("onSaveToDataBase")
+    })
+    const onExecute = useMemoizedFn(() => {
+        console.log("onExecute")
+    })
+    return (
+        <YakitDrawer
+            placement='bottom'
+            closable={false}
+            onClose={() => onClose()}
+            visible={visibleDrawer}
+            mask={false}
+            style={{height: visibleDrawer ? heightDrawer : 0}}
+            className={classNames(mitmStyles["mitm-rule-drawer"])}
+            contentWrapperStyle={{boxShadow: "0px -2px 4px rgba(133, 137, 158, 0.2)"}}
+            title={<div className={mitmStyles["heard-title"]}>{title}</div>}
+            extra={
+                <div className={mitmStyles["heard-right-operation"]}>
+                    <RuleExportAndImportButton onOkImport={onOkImport}/>
+                    <YakitButton
+                        type='outline1'
+                        onClick={() => onExecute()}
+                        className={mitmStyles["button-question"]}
+                        size={"small"}
+                    >
+                        调试执行
+                    </YakitButton>
+                    <YakitButton
+                        type='primary'
+                        className={mitmStyles["button-save"]}
+                        onClick={() => onSaveToDataBase()}
+                    >
+                        保存
+                    </YakitButton>
+                    <Tooltip title='官方网站' placement='top' overlayClassName={mitmStyles["question-tooltip"]}>
+                        <YakitButton
+                            type='outline2'
+                            className={mitmStyles["button-question"]}
+                            onClick={() => openExternalWebsite("https://www.yaklang.com/")}
+                            icon={<QuestionMarkCircleIcon/>}
+                        >
+                        </YakitButton>
+                    </Tooltip>
+                    <div onClick={() => onClose()} className={mitmStyles["icon-remove"]}>
+                        <RemoveIcon/>
+                    </div>
+                </div>
+            }
+        >
+            <CustomEditor
+
+                enCoder={defEncoder}
+                deCoder={""}
+                // onClose={onClose}
+            />
+
+        </YakitDrawer>
+    )
+})
+
+interface CustomEditorProps {
+
+    enCoder: string
+    deCoder: string
+    onClose?: () => void
+}
+
+const defEncoder = `
+hijackHTTPRequest = func(isHttps, url, req, forward , drop) {
+    _, reqBody = poc.Split(req)
+    jsonStr := '{"id":"1","body":{"user":"lucky"}}'
+    encodedData = codec.EncodeBase64(reqBody)
+    encodedData = str.ReplaceAll(encodedData, "+", "<")
+    encodedData = str.ReplaceAll(encodedData, "/", ">")
+    jsonStr = str.ReplaceAll(jsonStr, "lucky", encodedData)
+    res = poc.ReplaceBody(req,[]byte(jsonStr),false)
+    dump(res)
+    forward(res)
+}
+`
+
+const CustomEditor: React.FC<CustomEditorProps> = React.memo((props) => {
+    const {enCoder, deCoder, onClose} = props
+    const [codeValue, setCodeValue] = useState<string>("")
+
+    const [refreshTrigger, setRefreshTrigger] = useState<boolean>(false)
+    useEffect(() => {
+        setCodeValue(enCoder || defEncoder)
+        setRefreshTrigger(!refreshTrigger)
+    }, [enCoder])
+    const [shellScript, setShellScript] = useState<string>("")
+
+    useEffect(() => {
+        // 如果 params.ShellScript 是空的，那么就设置它为 "jsp"
+        if (!shellScript) {
+            setShellScript("jsp");
+        }
+    }, []);
+    return (
+        <div className={matcherStyles["matching-extraction-resize-box"]} style={{ display: 'flex' }}>
+            <YakitResizeBox
+                isVer={true}
+                freeze={false}
+                firstNodeStyle={{height: "50%"}}
+                firstNode={
+                    <YakEditor
+                        noMiniMap={true} type={"yak"}
+                        value={codeValue} setValue={setCodeValue}
+                    />
+                }
+                secondNode={<YakEditor
+                    noMiniMap={true} type={"yak"}
+                    value={"codeValue"} setValue={setCodeValue}
+                />}
+                secondNodeStyle={{paddingTop: 5}}
+                style={{"width": "50%"}}
+            />
+
+            <Form
+                colon={false}
+                size='small'
+                labelCol={{span: 10}}
+                wrapperCol={{span: 14}}
+                style={{overflowY: "auto",width:"50%"}}
+            >
+                <Form.Item label={"脚本类型"}>
+                    <YakitSelect
+                        value={shellScript || "jsp"}
+                        onSelect={(val) => {
+                            setShellScript(val)
+                        }}
+                    >
+                        <YakitSelect.Option value='jsp'>JSP</YakitSelect.Option>
+                        <YakitSelect.Option value='jspx'>JSPX</YakitSelect.Option>
+                        <YakitSelect.Option value='php'>PHP</YakitSelect.Option>
+                        <YakitSelect.Option value='asp'>ASP</YakitSelect.Option>
+                        <YakitSelect.Option value='aspx'>ASPX</YakitSelect.Option>
+                    </YakitSelect>
+                </Form.Item>
+
+
+            </Form>
+
+        </div>
+    )
+})
--- app/renderer/src/main/src/pages/webShell/WebShellViewer.tsx
+++ app/renderer/src/main/src/pages/webShell/WebShellViewer.tsx
@@ -2,8 +2,8 @@ import {yakitFailed} from "@/utils/notification";
 import {YakitSpin} from "@/components/yakitUI/YakitSpin/YakitSpin";
 import styles from "@/pages/cve/CVETable.module.scss";
 import fuzzerStyles from "@/pages/fuzzer/HttpQueryAdvancedConfig/HttpQueryAdvancedConfig.module.scss";
-import React, {useEffect, useState} from "react";
-import {useCreation, useMemoizedFn} from "ahooks";
+import React, {useEffect, useRef, useState} from "react";
+import {useCreation, useInViewport, useMemoizedFn} from "ahooks";
 import {PaginationSchema} from "@/pages/invoker/schema";
 import {WebShellTable} from "@/pages/webShell/WebShellTable";
 import {YakitSwitch} from "@/components/yakitUI/YakitSwitch/YakitSwitch";
@@ -13,7 +13,7 @@ import YakitCollapse from "@/components/yakitUI/YakitCollapse/YakitCollapse";
 import {YakitButton} from "@/components/yakitUI/YakitButton/YakitButton";
 import {HollowLightningBoltIcon, InformationCircleIcon, PlusSmIcon} from "@/assets/newIcon";
 import {YakitInputNumber} from "@/components/yakitUI/YakitInputNumber/YakitInputNumber";
-import {CustomCodecList} from "@/pages/webShell/CustomCodec";
+import {CustomCodecEditor, CustomCodecList} from "@/pages/webShell/CustomCodec";
 
 
 export interface WebShellManagerViewerProp {
@@ -87,11 +87,20 @@ const WebShellQuery: React.FC<WebShellQueryProp> = (props) => {
 
     const [activeKey, setActiveKey] = useState<string[]>() // Collapse打开的key
     const onSwitchCollapse = useMemoizedFn((key) => {
+        console.log("onSwitchCollapse ", key)
         setActiveKey(key)
     })
-    const [customCodecList, setCustomCodecList] = useState<string[]>(["json","xx ","我"]) // Collapse打开的key
 
+    const [title, setTitle] = useState<string>("") // Collapse打开的key
 
+    const [customCodecList, setCustomCodecList] = useState<string[]>([])
+
+    const [visibleDrawer, setVisibleDrawer] = useState<boolean>(false)
+    const queryRef = useRef(null)
+    const [inViewport] = useInViewport(queryRef)
+    useEffect(() => {
+        if (!inViewport) setVisibleDrawer(false)
+    }, [inViewport])
     return (
         <>
             <div className={fuzzerStyles["http-query-advanced-config"]}>
@@ -103,9 +112,9 @@ const WebShellQuery: React.FC<WebShellQueryProp> = (props) => {
                 <Form
                     colon={false}
                     size='small'
-                    labelCol={{ span: 10 }}
-                    wrapperCol={{ span: 14 }}
-                    style={{ overflowY: "auto" }}
+                    labelCol={{span: 10}}
+                    wrapperCol={{span: 14}}
+                    style={{overflowY: "auto"}}
                 >
                     <YakitCollapse
                         activeKey={activeKey}
@@ -116,45 +125,159 @@ const WebShellQuery: React.FC<WebShellQueryProp> = (props) => {
                         <YakitPanel
                             header={
                                 <div className={fuzzerStyles["matchers-panel"]}>
-                                    WebShell 编解码器
+                                    WebShell 编码器
                                     <div className={fuzzerStyles["matchers-number"]}>{customCodecList?.length}</div>
                                 </div>
                             }
-                            key='数据提取器'
+                            key='WebShell 编码器'
                             extra={
                                 <>
-                                    <Divider type='vertical' style={{ margin: 0 }} />
+                                    <Divider type='vertical' style={{margin: 0}}/>
                                     <YakitButton
                                         type='text'
                                         size='small'
                                         onClick={(e) => {
                                             e.stopPropagation()
-                                            if (activeKey?.findIndex((ele) => ele === "数据提取器") === -1) {
-                                                onSwitchCollapse([...activeKey, "数据提取器"])
+                                            setTitle("WebShell 编码器")
+                                            if (activeKey?.findIndex((ele) => ele === "WebShell 编码器") === -1) {
+                                                onSwitchCollapse([...activeKey, "WebShell 编码器"])
                                             }
+                                            setVisibleDrawer(true)
                                             // onAddMatchingAndExtractionCard("extractors")
                                         }}
                                         className={fuzzerStyles["btn-padding-right-0"]}
                                     >
                                         添加/调试
-                                        <HollowLightningBoltIcon />
+                                        <HollowLightningBoltIcon/>
                                     </YakitButton>
                                 </>
                             }
                         >
                             <CustomCodecList
-                                customCodecValue={ { customCodecList }}
+                                customCodecValue={{customCodecList}}
                                 onAdd={() => {
-                                    console.log(1111)}}
-                                onRemove={()=>{
-                                    console.log(2222)}}
-                                onEdit={()=>{
-                                    console.log(3333)}}
+                                    console.log(1111)
+                                }}
+                                onRemove={() => {
+                                    console.log(2222)
+                                }}
+                                onEdit={() => {
+                                    console.log(3333)
+                                }}
                             />
+                        </YakitPanel>
+
+                        <YakitPanel
+                            header={
+                                <div className={fuzzerStyles["matchers-panel"]}>
+                                    WebShell 解码器
+                                    <div className={fuzzerStyles["matchers-number"]}>{customCodecList?.length}</div>
+                                </div>
+                            }
+                            key='WebShell 解码器'
+                            extra={
+                                <>
+                                    <Divider type='vertical' style={{margin: 0}}/>
+                                    <YakitButton
+                                        type='text'
+                                        size='small'
+                                        onClick={(e) => {
+                                            e.stopPropagation()
+                                            setTitle("WebShell 解码器")
+                                            if (activeKey?.findIndex((ele) => ele === "WebShell 解码器") === -1) {
+                                                onSwitchCollapse([...activeKey, "WebShell 解码器"])
+                                            }
+                                            setVisibleDrawer(true)
+                                        }}
+                                        className={fuzzerStyles["btn-padding-right-0"]}
+                                    >
+                                        添加/调试
+                                        <HollowLightningBoltIcon/>
+                                    </YakitButton>
+                                </>
+                            }
+                        >
+                            <CustomCodecList
+                                customCodecValue={{customCodecList}}
+                                onAdd={() => {
+                                    console.log(1111)
+                                }}
+                                onRemove={() => {
+                                    console.log(2222)
+                                }}
+                                onEdit={() => {
+                                    console.log(3333)
+                                }}
+                            />
+                        </YakitPanel>
+                        <YakitPanel
+                            header='WebShell 生成'
+                            key='WebShell 生成'
+                            extra={
+                                <YakitButton
+                                    type='text'
+                                    colors="danger"
+                                    className={fuzzerStyles["btn-padding-right-0"]}
+                                    onClick={(e) => {
+                                        e.stopPropagation()
+                                        const restValue = {
+                                            forceFuzz: true,
+                                            isHttps: false,
+                                            noFixContentLength: false,
+                                            actualHost: "",
+                                            timeout: 30
+                                        }
+                                        // onReset(restValue)
+                                    }}
+                                    size='small'
+                                >
+                                    重置
+                                </YakitButton>
+                            }
+                        >
+                            <Form.Item label='Fuzztag 辅助'>
+                                <YakitButton
+                                    size='small'
+                                    type='outline1'
+                                    onClick={() => {
+                                        console.log("on")
+                                    }}
+                                    icon={<PlusSmIcon/>}
+                                >
+                                    插入 yak.fuzz 语法
+                                </YakitButton>
+                            </Form.Item>
+                            <Form.Item
+                                label={
+                                    <span className={fuzzerStyles["advanced-config-form-label"]}>
+                                    渲染 Fuzz
+                                    <Tooltip title='关闭之后，所有的 Fuzz 标签将会失效' overlayStyle={{width: 150}}>
+                                        <InformationCircleIcon className={fuzzerStyles["info-icon"]}/>
+                                    </Tooltip>
+                                </span>
+                                }
+                                name='forceFuzz'
+                                valuePropName='checked'
+                            >
+                                <YakitSwitch/>
+                            </Form.Item>
+
+                            <Form.Item label='不修复长度' name='noFixContentLength' valuePropName='checked'>
+                                <YakitSwitch/>
+                            </Form.Item>
+
+                            <Form.Item label='超时时长' name='timeout'>
+                                <YakitInputNumber type='horizontal' size='small'/>
+                            </Form.Item>
                         </YakitPanel>
                     </YakitCollapse>
 
                 </Form>
+                <CustomCodecEditor
+                    title={title}
+                    visibleDrawer={visibleDrawer}
+                    onClose={() => setVisibleDrawer(false)}
+                />
 
             </div>
         </>
