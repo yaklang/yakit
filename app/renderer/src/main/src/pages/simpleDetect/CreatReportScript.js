d835d0fa66a17fe5ad14c3108d046f9c3ef85d12
Youngster_yj <907985037@qq.com>
2023-09-01 17:17:32 +0800

    fix: 修改文案

--- app/renderer/src/main/src/pages/simpleDetect/CreatReportScript.js
+++ app/renderer/src/main/src/pages/simpleDetect/CreatReportScript.js
@@ -498,7 +498,7 @@ for i, riskIns := range potentialRisks {
 }
 if len(potentialRisks) != 0 {
     
-    reportInstance.Markdown(sprintf("### 3.3.3 风险资产图"))
+    reportInstance.Markdown(sprintf("### 3.3.3 合规检测风险统计"))
     for _, gp := range cpp.ToGraphs(){
         aa = json.dumps(gp)
         reportInstance.Raw(aa)
