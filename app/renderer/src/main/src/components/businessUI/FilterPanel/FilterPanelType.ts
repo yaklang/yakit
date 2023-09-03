cc6b6198a12bab585e30a4f0f3c351bff039c673
nonight <378099757@qq.com>
2023-09-03 22:15:21 +0800

    插件商店-新UI开发母版

---
+++ app/renderer/src/main/src/components/businessUI/FilterPanel/FilterPanelType.ts
@@ -0,0 +1,34 @@
+export interface FilterPanelGroupItem {
+    /** 单项过滤条件key */
+    groupKey: string
+    /** 单项过滤条件展示名 */
+    groupName: string
+    /** 单项过滤条件-过滤内容列表 */
+    data: {
+        /** 选项展示内容 */
+        label: string
+        /** 选项值 */
+        value: string
+        /** 选项统计总数 */
+        count: number
+    }[]
+}
+
+export interface FilterPanelProps {
+    /** 外框架修饰类 */
+    wrapperClassName?: string
+    /** 加载状态 */
+    loading?: boolean
+    /** 是否可见 */
+    visible: boolean
+    /** 设置是否可见 */
+    setVisible: (show: boolean) => any
+    /** 选中数据 */
+    selecteds: Record<string, string[]>
+    /** 选中数据回调 */
+    onSelect: (value: Record<string, string[] | string>) => any
+    /** 数据展示列表 */
+    groupList: FilterPanelGroupItem[]
+    /** 数据为空时的提示信息 */
+    noDataHint?: string
+}
