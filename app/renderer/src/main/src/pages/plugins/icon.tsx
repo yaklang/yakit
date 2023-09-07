019f00a29fcc2627296799d73669c2a9a2d56a57
nonight <378099757@qq.com>
2023-09-07 10:58:12 +0800

    插件商店-新增/编辑插件页面

--- app/renderer/src/main/src/pages/plugins/icon.tsx
+++ app/renderer/src/main/src/pages/plugins/icon.tsx
@@ -23,3 +23,21 @@ const PluginsGridCheck = () => (
 export const PluginsGridCheckIcon = (props: Partial<IconProps>) => {
     return <Icon component={PluginsGridCheck} {...props} />
 }
+
+const PluginTestError = () => (
+    <svg width='20' height='20' viewBox='0 0 20 20' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <rect width='20' height='20' rx='10' fill='#F7544A' />
+        <path
+            fillRule='evenodd'
+            clipRule='evenodd'
+            d='M10 16C13.3137 16 16 13.3137 16 10C16 6.68629 13.3137 4 10 4C6.68629 4 4 6.68629 4 10C4 13.3137 6.68629 16 10 16ZM9.03033 7.96967C8.73744 7.67678 8.26256 7.67678 7.96967 7.96967C7.67678 8.26256 7.67678 8.73744 7.96967 9.03033L8.93934 10L7.96967 10.9697C7.67678 11.2626 7.67678 11.7374 7.96967 12.0303C8.26256 12.3232 8.73744 12.3232 9.03033 12.0303L10 11.0607L10.9697 12.0303C11.2626 12.3232 11.7374 12.3232 12.0303 12.0303C12.3232 11.7374 12.3232 11.2626 12.0303 10.9697L11.0607 10L12.0303 9.03033C12.3232 8.73744 12.3232 8.26256 12.0303 7.96967C11.7374 7.67678 11.2626 7.67678 10.9697 7.96967L10 8.93934L9.03033 7.96967Z'
+            fill='white'
+        />
+    </svg>
+)
+/**
+ * @description  icon/log 节点状态/错误 Error
+ */
+export const PluginTestErrorIcon = (props: Partial<IconProps>) => {
+    return <Icon component={PluginTestError} {...props} />
+}
