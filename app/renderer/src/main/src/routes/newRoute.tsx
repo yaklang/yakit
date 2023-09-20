9a270ad0e86a47af2cd0a4957c481b6fdc687fda
TangHang123 <1425735414@qq.com>
2023-09-20 16:15:35 +0800

    -

--- app/renderer/src/main/src/routes/newRoute.tsx
+++ app/renderer/src/main/src/routes/newRoute.tsx
@@ -119,8 +119,6 @@ import {ErrorBoundary} from "react-error-boundary"
 import {PageItemProps} from "@/pages/layout/mainOperatorContent/renderSubPage/RenderSubPageType"
 import { FuzzerParamItem, AdvancedConfigValueProps } from "@/pages/fuzzer/HttpQueryAdvancedConfig/HttpQueryAdvancedConfigType"
 import { HTTPResponseExtractor } from "@/pages/fuzzer/MatcherAndExtractionCard/MatcherAndExtractionCardType"
-import { PluginManage } from "@/pages/plugins/manage/PluginManage"
-import { PluginsOnline } from "@/pages/plugins/online/PluginsOnline"
 
 const HTTPHacker = React.lazy(() => import("../pages/hacker/httpHacker"))
 const CodecPage = React.lazy(() => import("../pages/codec/CodecPage"))
@@ -419,10 +417,7 @@ export const RouteToPage: (props: PageItemProps) => ReactNode = (props) => {
     const {routeKey, yakScriptId, params} = props
     switch (routeKey) {
         case YakitRoute.NewHome:
-            // return <PlugInAdminPage/>
-            // return <PluginManage/>
-            return <PluginsOnline/>
-            // return <NewHome />
+            return <NewHome />
         case YakitRoute.HTTPHacker:
             return (
                 <Suspense fallback={<PageLoading />}>
