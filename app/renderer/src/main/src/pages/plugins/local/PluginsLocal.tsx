f73a477d7a64b7eef32114c2363a0ba19bbcf37b
TangHang123 <1425735414@qq.com>
2023-09-20 16:20:21 +0800

    新建本地插件文件

---
+++ app/renderer/src/main/src/pages/plugins/local/PluginsLocal.tsx
@@ -0,0 +1,14 @@
+import React, {useState, useRef, useMemo, useEffect} from "react"
+import "../plugins.scss"
+import styles from "./PluginsLocal.module.scss";
+import { PluginsLocalProps } from "./PluginsLocalType"
+
+export const PluginsLocal: React.FC<PluginsLocalProps> = React.memo((props) => {
+
+    return (
+        <div>444</div>
+    )
+})
+
+
+
