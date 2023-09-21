f6fe845161f4cf02f0fb6b258735dbf8a71a8305
TangHang123 <1425735414@qq.com>
2023-09-21 11:36:53 +0800

    本地插件列表页面

--- app/renderer/src/main/src/pages/plugins/local/PluginsLocalType.d.ts
+++ app/renderer/src/main/src/pages/plugins/local/PluginsLocalType.d.ts
@@ -1,3 +1,14 @@
-export interface PluginsLocalProps{
-    
+export interface PluginsLocalProps {}
+
+export interface LocalExtraOperateProps {
+    /**是否是自己的插件 */
+    isOwn: boolean
+    /**删除插件 */
+    onRemovePlugin: () => void
+    /**导出插件 */
+    onExportPlugin: () => void
+    /**编辑 */
+    onEditPlugin: () => void
+    /**上传 */
+    onUploadPlugin: () => void
 }
