3f92d87e3478815778f3a94b795c13d8a66ff3d5
TangHang123 <1425735414@qq.com>
2023-09-21 17:46:28 +0800

    我的插件

--- app/renderer/src/main/src/pages/plugins/user/PluginUserType.d.ts
+++ app/renderer/src/main/src/pages/plugins/user/PluginUserType.d.ts
@@ -1,3 +1,10 @@
-export interface UserPluginProps{
-    
+import {API} from "@/services/swagger/resposeType"
+
+export interface PluginUserProps {}
+
+export interface PluginUserListProps {
+    pluginState: string[]
+    searchUser: PluginSearchParams
+    setIsShowDetails: (b: boolean) => void
+    setIsSelectNum: (b: boolean) => void
 }
