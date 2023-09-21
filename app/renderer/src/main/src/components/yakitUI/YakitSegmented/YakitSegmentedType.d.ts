ca94f873c05c81535348c78502b40e03996c6930
TangHang123 <1425735414@qq.com>
2023-09-21 17:46:28 +0800

    我的插件

--- app/renderer/src/main/src/components/yakitUI/YakitSegmented/YakitSegmentedType.d.ts
+++ app/renderer/src/main/src/components/yakitUI/YakitSegmented/YakitSegmentedType.d.ts
@@ -1,10 +1,12 @@
 import {SegmentedProps} from "antd"
-
+import {SizeType} from "antd/lib/config-provider/SizeContext"
 
 /**
  * @description: 分段器 Props
- * @param {string} wrapperClassName  
+ * @property {string} wrapperClassName
+ * @property {SizeType} size  尺寸
  */
 interface YakitSegmentedProps extends SegmentedProps {
     wrapClassName?: string
+    size?: SizeType
 }
