2bfbda9e73e258a95acef1fb7a6b642bdc681a1e
Youngster_yj <907985037@qq.com>
2023-11-09 15:37:07 +0800

    fix: 新添全端口

--- app/renderer/src/main/src/pages/portscan/PortScanPage.tsx
+++ app/renderer/src/main/src/pages/portscan/PortScanPage.tsx
@@ -648,6 +648,7 @@ export const ScanPortForm: React.FC<ScanPortFormProp> = (props) => {
                                     <Checkbox value={"topdb"}>常见数据库与 MQ</Checkbox>
                                     <Checkbox value={"topudp"}>常见 UDP 端口</Checkbox>
                                     <Checkbox value={"defect"}>常见弱口令端口</Checkbox>
+                                    <Checkbox value={"all"}>全端口</Checkbox>
                                 </Checkbox.Group>
                             </Form.Item>
 
