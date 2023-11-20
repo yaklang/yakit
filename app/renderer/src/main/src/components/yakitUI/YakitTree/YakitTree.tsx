c2ebf97eef5a1d1688be320a08022053eb105a06
song_xiao_lin <song_xiao_lin@126.com>
2023-11-20 16:46:53 +0800

    feat: YakitTree搜索内容调整

--- app/renderer/src/main/src/components/yakitUI/YakitTree/YakitTree.tsx
+++ app/renderer/src/main/src/components/yakitUI/YakitTree/YakitTree.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import {Tree} from "antd"
 import type {DataNode, TreeProps} from "antd/es/tree"
 import {OutlineDocumentIcon, OutlineMinusIcon, OutlinePlusIcon} from "@/assets/icon/outline"
@@ -11,6 +11,10 @@ import {RefreshIcon} from "@/assets/newIcon"
 
 export type TreeKey = string | number
 
+/**
+ * 目前基本上只封装了样式
+ */
+
 interface YakitTreeProps extends TreeProps {
     showIcon?: boolean // 是否展示treeNode节点前的icon 默认 -> 展示
     icon?: React.ReactNode // treeNode节点前的icon 默认 -> 文件图标
@@ -18,14 +22,15 @@ interface YakitTreeProps extends TreeProps {
     treeData: DataNode[] // 需要满足 DataNode类型的数组
     expandedAllKeys?: boolean // 是否展开所有节点 ----- expandedAllKeys 优先级高于expandedKeys
     expandedKeys?: TreeKey[] // 展开节点的key结合 若设置selectedKeys或checkedKeys需要自动展开节点的话，需要手动找到父节点的key去组装expandedKeys传进来才能实现自动展开功能。autoExpandParent主要是针对手动点击触发的自动展开
-    onExpandedKeys?: (expandedKeys: TreeKey[]) => void // 若传了expandedKeys -> onExpand必传（需改变传过来的expandedKeys）
+    onExpandedKeys?: (expandedKeys: TreeKey[]) => void // 若传了expandedKeys -> onExpandedKeys必传（需改变传过来的expandedKeys）
     selectedKeys?: TreeKey[]
     onSelectedKeys?: (selectedKeys: TreeKey[], selectedNodes: DataNode[]) => void
     checkedKeys?: TreeKey[]
     onCheckedKeys?: (checkedKeys: TreeKey[], checkedNodes: DataNode[]) => void
-    searchValue?: string
+    showSearch?: boolean // 是否显示搜索框 默认 -> 显示
+    defaultSearchValue?: string // 默认初始搜索内容
     onSearchValue?: (searchValue: string) => void
-    refreshTree?: () => void
+    refreshTree?: () => void // 刷新树
 }
 
 const YakitTree: React.FC<YakitTreeProps> = (props) => {
@@ -33,6 +38,7 @@ const YakitTree: React.FC<YakitTreeProps> = (props) => {
         showLine = true,
         showIcon = true,
         icon = <OutlineDocumentIcon className={styles["outlineDocument-icon"]} />,
+        showSearch = true,
         searchPlaceholder = "请输入关键词搜索",
         expandedAllKeys = false
     } = props
@@ -112,41 +118,52 @@ const YakitTree: React.FC<YakitTreeProps> = (props) => {
     }
 
     /**
-     * check - 选中树
+     * 搜索树
+     */
+    const [searchValue, setSearchValue] = useState("")
+    useEffect(() => {
+        setSearchValue(props.defaultSearchValue || "")
+    }, [props.defaultSearchValue])
+    const debounceSearch = useDebounceFn(
+        (value) => {
+            props.onSearchValue && props.onSearchValue(value)
+        },
+        {wait: 500, leading: true, trailing: true}
+    )
+    const onSearchChange = useMemoizedFn((e: {target: {value: string}}) => {
+        const value = e.target.value
+        setSearchValue(value)
+        debounceSearch.run(searchValue)
+        setAutoExpandParent(true)
+    })
+
+    /**
+     * check - 选中树 TODO：未处理复选框是否关联返回的勾选结果不一样
      */
     const onTreeCheck = (checkedKeys, info) => {
         setCheckedKeys(checkedKeys)
         props.onCheckedKeys && props.onCheckedKeys(checkedKeys, info.checkedNodes)
     }
 
-    /**
-     * 搜索树
-     */
-    const debounceSearch = useDebounceFn(
-        (value) => {
-            props.onSearchValue && props.onSearchValue(value)
-        },
-        {wait: 500}
-    )
-    const [searchValue, setSearchValue] = useState("")
-    const onSearchChange = useMemoizedFn((e: {target: {value: string}}) => {
-        const value = e.target.value
-        setSearchValue(value)
-        setAutoExpandParent(true)
-        debounceSearch.run(value)
-    })
-
     return (
         <div className={styles.yakitTree}>
             <div className={styles["tree-top-wrap"]}>
-                <YakitInput.Search
-                    style={{marginBottom: 15, width: "calc(100% - 40px)"}}
-                    placeholder={searchPlaceholder}
-                    allowClear
-                    onChange={onSearchChange}
-                />
+                {showSearch && (
+                    <YakitInput.Search
+                        style={{marginBottom: 15, width: "calc(100% - 40px)"}}
+                        placeholder={searchPlaceholder}
+                        allowClear
+                        onChange={onSearchChange}
+                        value={searchValue}
+                    />
+                )}
                 {props.refreshTree && (
-                    <YakitButton type='text2' icon={<RefreshIcon />} onClick={props.refreshTree} />
+                    <YakitButton
+                        type='text2'
+                        icon={<RefreshIcon />}
+                        onClick={props.refreshTree}
+                        style={{marginBottom: 15}}
+                    />
                 )}
             </div>
             {props.treeData.length ? (
