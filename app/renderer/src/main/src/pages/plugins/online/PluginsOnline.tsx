c709fdc7d52372d42a2325e3b8ef6c8e810551f5
luoluo <1425735414@qq.com>
2023-11-30 17:09:17 +0800

    插件商店heard文案yak增加提示

--- app/renderer/src/main/src/pages/plugins/online/PluginsOnline.tsx
+++ app/renderer/src/main/src/pages/plugins/online/PluginsOnline.tsx
@@ -942,7 +942,7 @@ const PluginsOnlineHeard: React.FC<PluginsOnlineHeardProps> = React.memo((props)
                     <div className={styles["plugin-online-heard-content-top-tip"]}>Hello everyone! 👋</div>
                     <div className={styles["plugin-online-heard-content-top-title"]}>Yakit 插件商店</div>
                     <div className={styles["plugin-online-heard-content-top-subTitle"]}>
-                        未封闭的牛头 YAK等待你来填满
+                        未封闭的牛头&nbsp;&nbsp;YAK&nbsp;等待你来填满
                     </div>
                     <YakitCombinationSearchCircle value={search} onChange={setSearch} onSearch={onSearch} />
                 </div>
