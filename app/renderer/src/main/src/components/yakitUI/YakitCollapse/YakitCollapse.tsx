126564927afc58d4cd52cf408df4fb3c83606936
luoluo <1425735414@qq.com>
2023-12-07 11:49:37 +0800

    YakitCollapse:修改部分样式

--- app/renderer/src/main/src/components/yakitUI/YakitCollapse/YakitCollapse.tsx
+++ app/renderer/src/main/src/components/yakitUI/YakitCollapse/YakitCollapse.tsx
@@ -2,7 +2,7 @@ import {Collapse} from "antd"
 import React, {useState} from "react"
 import styles from "./YakitCollapse.module.scss"
 import classNames from "classnames"
-import {YakitCollapseProps} from "./YakitCollapseType"
+import {YakitCollapseProps, YakitPanelProps} from "./YakitCollapseType"
 import {SolidChevrondownIcon, SolidChevronrightIcon} from "@/assets/icon/solid"
 
 const {Panel} = Collapse
@@ -14,14 +14,15 @@ const {Panel} = Collapse
  * @param {CSSProperties} wrapperStyle Collapse 装饰div的style
  */
 export const YakitCollapse: React.FC<YakitCollapseProps> = (props) => {
-    const {type = "default", wrapperClassName, wrapperStyle, ...restProps} = props
-    
+    const {type = "default",divider=true, wrapperClassName, wrapperStyle, ...restProps} = props
+
     return (
         <div
             className={classNames(
                 styles["yakit-collapse-default-wrapper"],
                 {
-                    [styles["yakit-collapse-grey-wrapper"]]: type === "grey"
+                    [styles["yakit-collapse-grey-wrapper"]]: type === "grey",
+                    [styles["yakit-collapse-grey-divider"]]: divider,
                 },
                 wrapperClassName
             )}
@@ -36,4 +37,14 @@ export const YakitCollapse: React.FC<YakitCollapseProps> = (props) => {
     )
 }
 
-export default Object.assign(YakitCollapse, {YakitPanel: Panel})
+/**
+ * @description: 折叠面板
+ * @augments  继承antd的 CollapseProps 默认属性
+ * @param {string} wrapperClassName Collapse 装饰div的className
+ * @param {CSSProperties} wrapperStyle Collapse 装饰div的style
+ */
+export const YakitPanel: React.FC<YakitPanelProps> = (props) => {
+    return <Panel {...props} className={styles["yakit-panel"]} />
+}
+
+export default Object.assign(YakitCollapse, {YakitPanel})
