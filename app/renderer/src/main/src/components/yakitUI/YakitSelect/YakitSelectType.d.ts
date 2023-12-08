0cc4652f18c9cc5d6906d5c7a1e91a1fa7636c3b
luoluo <1425735414@qq.com>
2023-12-08 17:54:34 +0800

    YakitSelect:新增缓存数据逻辑。支持单选和tag两种模式，多选暂未支持

--- app/renderer/src/main/src/components/yakitUI/YakitSelect/YakitSelectType.d.ts
+++ app/renderer/src/main/src/components/yakitUI/YakitSelect/YakitSelectType.d.ts
@@ -2,20 +2,40 @@ import {SelectProps} from "antd"
 import {OptionProps} from "rc-select/lib/Option"
 import {ReactNode} from "react"
 import type {YakitSizeType} from "../YakitInputNumber/YakitInputNumberType"
+import {CacheDataHistoryProps, YakitOptionTypeProps} from "../utils"
+import {BaseSelectRef} from "rc-select"
+import {BaseOptionType, DefaultOptionType} from "antd/lib/select"
+import {type} from "os"
 
 /**
  * @description: YakitSelectProps
  * @augments YakitSelectProps 继承antd的 SelectProps 默认属性
- * @param {string} wrapperClassName Switch装饰div的className
- * @param {CSSProperties} wrapperStyle Switch装饰div的style
+ * @param {string} wrapperClassName 装饰div的className
+ * @param {CSSProperties} wrapperStyle 装饰div的style
+ * @param {string} cacheHistoryDataKey 缓存数据
  */
 
 export interface YakitSelectProps<
     ValueType = any,
-    OptionType extends BaseOptionType | DefaultOptionType = DefaultOptionType
+    OptionType extends BaseOptionType | DefaultOptionType | YakitOptionTypeProps = DefaultOptionType
 > extends SelectProps {
     wrapperClassName?: string
     wrapperStyle?: CSSProperties
     size?: YakitSizeType
+    /**@name 缓存数据 */
+    cacheHistoryDataKey?: string
+    /**@name 缓存数据  list长度*/
+    cacheHistoryListLength?: number
+    defaultOptions?: OptionType
 }
 export interface YakitSelectOptionProps extends OptionProps {}
+
+export interface YakitSelectCacheDataHistoryProps extends Omit<CacheDataHistoryProps, "options", "defaultValue"> {
+    options?: OptionType
+    defaultValue: string[]
+}
+
+export interface YakitBaseSelectRef {
+    onSetRemoteValues: (s: string[]) => void
+    onGetRemoteValues: () => void
+}
