80294a0e3a3e9e166ceba295db183c6b594c0e67
luoluo <1425735414@qq.com>
2023-12-11 16:38:02 +0800

    优化一些命名

--- app/renderer/src/main/src/pages/plugins/local/PluginsLocal.tsx
+++ app/renderer/src/main/src/pages/plugins/local/PluginsLocal.tsx
@@ -40,9 +40,9 @@ import {Tooltip} from "antd"
 import {LoadingOutlined} from "@ant-design/icons"
 import {
     DeleteLocalPluginsByWhereRequestProps,
-    DeleteYakScriptRequestProps,
+    DeleteYakScriptRequestByIdsProps,
     apiDeleteLocalPluginsByWhere,
-    apiDeleteYakScript,
+    apiDeleteYakScriptByIds,
     apiFetchGroupStatisticsLocal,
     apiGetYakScriptByOnlineID,
     apiQueryYakScript,
@@ -528,10 +528,10 @@ export const PluginsLocal: React.FC<PluginsLocalProps> = React.memo((props) => {
                 await apiDeleteLocalPluginsByWhere(deleteAllParams)
             } else {
                 // 批量删除
-                let deleteBatchParams: DeleteYakScriptRequestProps = {
+                let deleteBatchParams: DeleteYakScriptRequestByIdsProps = {
                     Ids: (selectList || []).map((ele) => ele.Id)
                 }
-                await apiDeleteYakScript(deleteBatchParams)
+                await apiDeleteYakScriptByIds(deleteBatchParams)
             }
         } catch (error) {}
         setRemoveCheckVisible(false)
@@ -570,11 +570,11 @@ export const PluginsLocal: React.FC<PluginsLocalProps> = React.memo((props) => {
     })
     /**单个删除基础 */
     const onRemovePluginSingleBase = useMemoizedFn((data: YakScript) => {
-        let deleteParams: DeleteYakScriptRequestProps = {
+        let deleteParams: DeleteYakScriptRequestByIdsProps = {
             Ids: [data.Id]
         }
         return new Promise<void>((resolve, reject) => {
-            apiDeleteYakScript(deleteParams)
+            apiDeleteYakScriptByIds(deleteParams)
                 .then(() => {
                     const index = selectList.findIndex((ele) => ele.ScriptName === data.ScriptName)
                     if (index !== -1) {
