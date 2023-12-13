011ef91f0a31bca9c05c69cc08d1ff38a9ccb786
luoluo <1425735414@qq.com>
2023-12-13 11:33:16 +0800

    插件执行的日志和console

--- app/renderer/src/main/src/pages/plugins/operator/pluginExecuteResult/PluginExecuteResult.tsx
+++ app/renderer/src/main/src/pages/plugins/operator/pluginExecuteResult/PluginExecuteResult.tsx
@@ -2,7 +2,12 @@ import PluginTabs from "@/components/businessUI/PluginTabs/PluginTabs"
 import React, {useEffect, useMemo, useState} from "react"
 import {HorizontalScrollCard} from "../horizontalScrollCard/HorizontalScrollCard"
 import styles from "./PluginExecuteResult.module.scss"
-import {PluginExecuteResultProps, VulnerabilitiesRisksTableProps} from "./PluginExecuteResultType"
+import {
+    PluginExecuteLogProps,
+    PluginExecuteResultProps,
+    PluginExecuteResultTabContentProps,
+    VulnerabilitiesRisksTableProps
+} from "./PluginExecuteResultType"
 import {YakitButton} from "@/components/yakitUI/YakitButton/YakitButton"
 import {useMemoizedFn} from "ahooks"
 import emiter from "@/utils/eventBus/eventBus"
@@ -17,12 +22,16 @@ import {QueryGeneralResponse, genDefaultPagination} from "@/pages/invoker/schema
 import {showYakitModal} from "@/components/yakitUI/YakitModal/YakitModalConfirm"
 import {formatTimestamp} from "@/utils/timeUtil"
 import {CurrentHttpFlow} from "@/pages/yakitStore/viewers/base"
+import {Timeline} from "antd"
+import {LogLevelToCode} from "@/components/HTTPFlowTable/HTTPFlowTable"
+import {YakitLogFormatter} from "@/pages/invoker/YakitLogFormatter"
+import {EngineConsole} from "@/components/baseConsole/BaseConsole"
 
 const {TabPane} = PluginTabs
 const {ipcRenderer} = window.require("electron")
 
 export const PluginExecuteResult: React.FC<PluginExecuteResultProps> = React.memo((props) => {
-    const {infoState, runtimeId} = props
+    const {infoState, runtimeId, loading} = props
     return (
         <div className={styles["plugin-execute-result"]}>
             <div className={styles["plugin-execute-result-wrapper"]}>
@@ -35,14 +44,45 @@ export const PluginExecuteResult: React.FC<PluginExecuteResultProps> = React.mem
                 <TabPane tab='HTTP 流量' key='httpFlow' style={{borderBottom: "1px solid var(--yakit-border-color)"}}>
                     <CurrentHttpFlow runtimeId={runtimeId} />
                 </TabPane>
-                <TabPane tab='基础插件信息 / 日志' key='feature-0'>
-                    基础插件信息 / 日志
+                <TabPane tab='基础插件信息 / 日志' key='log'>
+                    <PluginExecuteLog loading={loading} messageList={infoState.messageState} />
                 </TabPane>
                 <TabPane tab='Console' key='console'>
-                    Console
+                    <EngineConsole isMini={true} />
                 </TabPane>
             </PluginTabs>
         </div>
+    )
+})
+const PluginExecuteLog: React.FC<PluginExecuteLogProps> = React.memo((props) => {
+    const {loading, messageList} = props
+    const timelineItemProps = useMemo(() => {
+        return (messageList || [])
+            .filter((i) => {
+                return !((i?.level || "").startsWith("json-feature") || (i?.level || "").startsWith("feature-"))
+            })
+            .splice(0, 25)
+    }, [messageList])
+    return (
+        <PluginExecuteResultTabContent title='任务额外日志与结果'>
+            <Timeline pending={loading} style={{marginTop: 10, marginBottom: 10}}>
+                {(timelineItemProps || [])
+                    .reverse()
+                    .filter((item) => item.level !== "json-risk")
+                    .map((e, index) => {
+                        return (
+                            <Timeline.Item key={index} color={LogLevelToCode(e.level)}>
+                                <YakitLogFormatter
+                                    data={e.data}
+                                    level={e.level}
+                                    timestamp={e.timestamp}
+                                    onlyTime={true}
+                                />
+                            </Timeline.Item>
+                        )
+                    })}
+            </Timeline>
+        </PluginExecuteResultTabContent>
     )
 })
 /**获取风险等级的展示tag类型 */
@@ -182,5 +222,17 @@ const VulnerabilitiesRisksTable: React.FC<VulnerabilitiesRisksTableProps> = Reac
             columns={columns}
             containerClassName={styles["table-container"]}
         />
+    )
+})
+/**插件执行的tab content 结构 */
+const PluginExecuteResultTabContent: React.FC<PluginExecuteResultTabContentProps> = React.memo((props) => {
+    return (
+        <div className={styles["plugin-execute-result-tab-content"]}>
+            <div className={styles["plugin-execute-result-tab-content-head"]}>
+                <div>{props.title}</div>
+                <div>{props.extra}</div>
+            </div>
+            <div className={styles["plugin-execute-result-tab-content-body"]}>{props.children}</div>
+        </div>
     )
 })
