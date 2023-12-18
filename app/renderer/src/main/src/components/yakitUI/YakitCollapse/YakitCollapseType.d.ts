b0970f7b75f5c8576c2eca4ece687665f01033e9
luoluo <1425735414@qq.com>
2023-12-18 16:28:41 +0800

    新版yakit-collapse

--- app/renderer/src/main/src/components/yakitUI/YakitCollapse/YakitCollapseType.d.ts
+++ app/renderer/src/main/src/components/yakitUI/YakitCollapse/YakitCollapseType.d.ts
@@ -1,13 +1,14 @@
-import { CollapseProps } from "antd";
+import { CollapsePanelProps, CollapseProps } from "antd";
 /**
  * @description: YakitSelectProps
  * @augments YakitCollapseProps 继承antd的 CollapseProps 默认属性
- * @param {'default'|'grey'} type Collapse背景色类型
- * @param {string} wrapperClassName Switch装饰div的className
- * @param {CSSProperties} wrapperStyle Switch装饰div的style
  */
 export interface YakitCollapseProps extends CollapseProps{
-    type?:'default'|'grey'
-    wrapperClassName?: string
-    wrapperStyle?: CSSProperties
+}
+/**
+ * @description: YakitPanelProps
+ * @augments YakitPanelProps 继承antd的 CollapsePanelProps 默认属性
+ */
+export interface YakitPanelProps extends CollapsePanelProps{
+
 }
