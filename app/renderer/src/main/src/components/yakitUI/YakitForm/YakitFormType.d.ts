7afeed3ae7db0369f8442623fea54cc9b0df9697
luoluo <1425735414@qq.com>
2023-12-19 11:52:22 +0800

    YakitDragger:修改报错属性

--- app/renderer/src/main/src/components/yakitUI/YakitForm/YakitFormType.d.ts
+++ app/renderer/src/main/src/components/yakitUI/YakitForm/YakitFormType.d.ts
@@ -3,20 +3,23 @@ import {DraggerProps} from "antd/lib/upload"
 import type {YakitSizeType} from "../YakitInputNumber/YakitInputNumberType"
 import {InternalTextAreaProps} from "../YakitInput/YakitInputType"
 
-export interface YakitFormDraggerProps extends Omit<DraggerProps,'beforeUpload'> {
+type YakitDragger = Omit<DraggerProps, "beforeUpload" | "onChange">
+export interface YakitFormDraggerProps extends YakitDragger {
     size?: YakitSizeType
     formItemClassName?: string
     formItemProps?: FormItemProps
 }
 /**拖拽/点击文件,回显文件路径组件props */
-export interface YakitDraggerProps extends DraggerProps {
+export interface YakitDraggerProps extends YakitDragger {
     size?: YakitSizeType
     InputProps?: InputProps
     /**@description selectType为file,该属性才有效*/
     setContent?: (s: string) => void
     help?: ReactDOM
     showDefHelp?: boolean
+    /**回显的文本值 */
     value?: string
+    /**回显的文本回调事件 */
     onChange?: (s: string) => void
     /**all:支出上传文件和文件夹,不支持accept; file:只支持文件; folder:只支持文件夹;a */
     selectType?: "file" | "folder" | "all"
