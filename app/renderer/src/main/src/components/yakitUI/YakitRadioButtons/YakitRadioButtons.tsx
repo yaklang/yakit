bfae7b813d87fffcf012ee830518e7e6fca41028
luoluo <1425735414@qq.com>
2023-12-20 16:02:09 +0800

    YakitRadioButtons一些修改

--- app/renderer/src/main/src/components/yakitUI/YakitRadioButtons/YakitRadioButtons.tsx
+++ app/renderer/src/main/src/components/yakitUI/YakitRadioButtons/YakitRadioButtons.tsx
@@ -14,7 +14,6 @@ import classNames from "classnames"
 
 /**
  * @description: 按钮单选框Props
- * @deprecated optionType 弃用
  * @param {"small" | "middle" | "large" | "maxLarge"} size  默认middle
  * @augments RadioGroupProps 继承antd的 RadioGroupProps 默认属性
  * @params {string} className RadioGroup  className
