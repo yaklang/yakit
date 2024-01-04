081cf8f9a423e02da8efbcc5de094251ab980136
luoluo <1425735414@qq.com>
2024-01-04 15:39:25 +0800

    react-scripts升级到5后，修改webpack配置及其他配置

--- app/renderer/src/main/config-overrides.js
+++ app/renderer/src/main/config-overrides.js
@@ -1,14 +1,23 @@
-const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin")
-const { override, fixBabelImports, addWebpackExternals, addWebpackAlias, addWebpackPlugin, overrideDevServer, addWebpackModuleRule, addBabelPresets, setWebpackTarget, addPostcssPlugins, addWebpackResolve, adjustStyleLoaders } = require('customize-cra');
+const {
+    override,
+    fixBabelImports,
+    addWebpackResolve,
+    addWebpackExternals,
+    addWebpackPlugin,
+    addWebpackAlias,
+    addWebpackModuleRule,
+    overrideDevServer,
+    setWebpackTarget,
+    addBabelPresets
+} = require('customize-cra')
+const path = require('path')
 const ProgressBarPlugin = require('progress-bar-webpack-plugin'); // 打包进度
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin")
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require("path")
-
+const HotModuleReplacementPlugin = require('webpack/lib/HotModuleReplacementPlugin');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
 const OUTPUT_PATH = path.resolve(__dirname, "..", "..", "pages", "main")
-
-
 
 module.exports = {
     webpack: override(
@@ -16,156 +25,74 @@ module.exports = {
             fallback: {
                 // 添加其它需要的核心模块
                 fs: false,
-                buffer: false,
-                assert: false,
-                timers: false,
-                crypto: false,
-                path: require.resolve("path-browserify"),
-                url: false,
-                stream: false,
-                util: false,
-                http: false,
-                zlib: false
-                // fs: require.resolve('fs'),
-                // buffer: require.resolve('buffer'),
-                // crypto: require.resolve("crypto-browserify"),
-                // assert: require.resolve("assert/"),
-                // timers: require.resolve("timers-browserify")
             },
-            // ignoreWarnings: [/Failed to parse source map/],
         }),
-        fixBabelImports('import', { //配置按需加载
+        fixBabelImports('import', {
             libraryName: 'antd',
             libraryDirectory: 'es',
-            style: 'css',
+            style: 'css'
         }),
-        addWebpackAlias({ //路径别名
-            '@': path.resolve(__dirname, 'src'),
+        addWebpackAlias({
+            '@': path.resolve(__dirname, 'src')
         }),
         // 打包进度条
         addWebpackPlugin(new ProgressBarPlugin()),
         addWebpackPlugin(new MonacoWebpackPlugin({
-            languages: ["json", "javascript", "go", "markdown", "html", "yaml", "java"]
+            languages: ["json", "javascript", "go", "markdown", "html", "yaml", "java"],
         })),
-        // addWebpackPlugin(new HtmlWebpackPlugin({
-        //     favicon: "./public/favicon.ico",
-        //     filename: "./public/index.html",
-        //     manifest: "./public/manifest.json"
-        // })),
-        // addPostcssPlugins([require("postcss-px2rem")({ remUnit: 37.5 })]),
-        // process.env.NODE_ENV === "development" ?
-        //     addWebpackPlugin(new UglifyJsPlugin({
-        //         uglifyOptions: {
-        //             // 删除警告
-        //             warnings: false,
-        //         },
-        //     })) : addWebpackPlugin(new UglifyJsPlugin({
-        //         // 开启打包缓存
-        //         cache: true,
-        //         // 开启多线程打包
-        //         parallel: true,
-        //         uglifyOptions: {
-        //             // 删除警告
-        //             warnings: false,
-        //             // 压缩
-        //             compress: {
-        //                 // 移除console
-        //                 drop_console: true,
-        //                 // 移除debugger
-        //                 drop_debugger: true,
-        //             },
-        //         },
-        //     })),
-        // addWebpackPlugin(new UglifyJsPlugin({
-        //     // // 开启打包缓存
-        //     // cache: true,
-        //     // // 开启多线程打包
-        //     // parallel: true,
-        //     uglifyOptions: {
-        //         // 删除警告
-        //         warnings: false,
-        //         // 压缩
-        //         // compress: {
-        //         //     // 移除console
-        //         //     drop_console: true,
-        //         //     // 移除debugger
-        //         //     drop_debugger: true,
-        //         // },
-        //     },
-        // })),
-        addWebpackExternals(
-            { "./cptable": "var cptable" },
-        ),
-        // setWebpackTarget('node'),
+        addWebpackPlugin(new HotModuleReplacementPlugin()),
+        addWebpackPlugin(new NodePolyfillPlugin()),
         addWebpackModuleRule({
             test: [/\.css$/, /\.scss$/], // 可以打包后缀为sass/scss/css的文件
             use: ["style-loader", "css-loader", "sass-loader"]
-        },
-            {
-                test: /\.js$/,
-                enforce: 'pre',
-                use: [
-                    {
-                        //needed to chain sourcemaps.  see: https://webpack.js.org/loaders/source-map-loader/
-                        loader: 'source-map-loader',
-                        options: {
-                            filterSourceMappingUrl: (url, resourcePath) => {
-                                if (/.*\/node_modules\/.*/.test(resourcePath)) {
-                                    return false
-                                }
-                                return true
-                            }
-
-                        }
-                    }],
-            }
+        }),
+        addWebpackExternals(
+            { "./cptable": "var cptable" },
+            // {
+            //     test: /\.js[x]?$/, // 设置所匹配要解析的文件
+            //     use: {
+            //         loader: 'babel-loader',  // 使用的loader
+            //         options: { // loader的配置
+            //             presets: [ // 设置预设
+            //                 ['@babel/preset-env', { // 高级语法解析预设
+            //                     // useBuiltIns 有三个值
+            //                     // false：不对polyfill做操作。
+            //                     // entry：根据配置的浏览器兼容, 引入浏览器不兼容的 polyfill。需要在入口处手动引入polyfill
+            //                     // usage：根据配置的浏览器兼容,及代码中使用到的高级语法按需求引入polyfill
+            //                     useBuiltIns: 'usage',
+            //                     corejs: 3 // core-js的使用版本
+            //                 }],
+            //                 '@babel/preset-react', // 解析
+            //             ]
+            //         },
+            //     },
+            //     exclude: /node_modules/, // 一般依赖已经经过转换，所以排除安装的依赖包，只转换我们自己的代码提高速度。
+            // },
         ),
         (config) => {
             config.output.path = OUTPUT_PATH
-            config.output.publicPath = "./"
-            // config.externals = [{ "./cptable": "var cptable" }]
-            // config.plugins.push(
-            //     new MonacoWebpackPlugin({
-            //         languages: ["json", "javascript", "go", "markdown", "html", "yaml", "java"]
-            //     })
-            // )
+            // config.output.publicPath = "./"
             //暴露webpack的配置 config ,evn
             // 去掉打包生产map 文件
-            // config.devtool = config.mode === 'development' ? 'cheap-module-source-map' : false;
+            config.devtool = config.mode === 'development' ? 'cheap-module-source-map' : false;
             // if (process.env.NODE_ENV === "production") 
-            config.devtool = false
             // if (process.env.NODE_ENV !== "development") config.plugins = [...config.plugins, ...myPlugin]
-            //1.修改、添加loader 配置 :
-            // 所有的loaders规则是在config.module.rules(数组)的第二项 
-            // 即：config.module.rules[2].oneof  (如果不是，具体可以打印 一下是第几项目)
-            // 修改 sass 配置 ，规则 loader 在第五项(具体看配置)
-            // const loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf)).oneOf;
-            // loaders[5].use.push({
-            //     loader: 'sass-resources-loader',
-            //     options: {
-            //         // resources: path.resolve(__dirname, 'src/asset/base.scss')//全局引入公共的scss 文件
-            //     }
-            // })
-            // console.log('config-webpack', config)
+            console.log('config-webpack', config, OUTPUT_PATH)
             return config
         }
     ),
     devServer: overrideDevServer(
-        config => ({
-            ...config,
-            hot: true,
-            devMiddleware: {
-                writeToDisk: true,
-                // host: process.env.HOST || "0.0.0.0",
-                // sockHost: process.env.WDS_SOCKET_HOST,
-                // sockPath: process.env.WDS_SOCKET_PATH,// default: '/sockjs-node'
-                // sockPort: process.env.WDS_SOCKET_PORT,
+        config => {
+            const newConfig = {
+                ...config,
+                hot: true,
+                devMiddleware: {
+                    writeToDisk: true,
+                },
             }
-            // writeToDisk: true,
-            // host: process.env.HOST || "0.0.0.0",
-            // sockHost: process.env.WDS_SOCKET_HOST,
-            // sockPath: process.env.WDS_SOCKET_PATH,// default: '/sockjs-node'
-            // sockPort: process.env.WDS_SOCKET_PORT,
-        })
-    )
-};
+            console.log('config-devServer', newConfig)
+            return newConfig
+        },
+        // watchAll()
+    ),
+}
