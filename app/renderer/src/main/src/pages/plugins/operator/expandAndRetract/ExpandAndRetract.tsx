b658e5b5d5ef95e7ccc07652486d39179fbd9e69
luoluo <1425735414@qq.com>
2024-01-19 16:19:12 +0800

    空间引擎头部逻辑

--- app/renderer/src/main/src/pages/plugins/operator/expandAndRetract/ExpandAndRetract.tsx
+++ app/renderer/src/main/src/pages/plugins/operator/expandAndRetract/ExpandAndRetract.tsx
@@ -1,16 +1,18 @@
 import React, {MouseEventHandler, ReactNode} from "react"
 import styles from "./ExpandAndRetract.module.scss"
 import {OutlineChevrondoubledownIcon, OutlineChevrondoubleupIcon} from "@/assets/icon/outline"
+import classNames from "classnames"
 
 interface ExpandAndRetractProps {
     onExpand: MouseEventHandler<HTMLDivElement>
     isExpand: boolean
     children?: ReactNode
+    className?: string
 }
 export const ExpandAndRetract: React.FC<ExpandAndRetractProps> = React.memo((props) => {
-    const {isExpand, onExpand, children} = props
+    const {isExpand, onExpand, children, className = ""} = props
     return (
-        <div className={styles["expand-and-retract-header"]} onClick={onExpand}>
+        <div className={classNames(styles["expand-and-retract-header"], className)} onClick={onExpand}>
             <div className={styles["expand-and-retract-header-icon-body"]}>
                 {isExpand ? (
                     <>
--- app/renderer/src/main/src/pages/spaceEngine/SpaceEngine.tsx
+++ app/renderer/src/main/src/pages/spaceEngine/SpaceEngine.tsx
@@ -1,9 +1,56 @@
-import React, {MouseEventHandler, ReactNode} from "react"
+import React, {MouseEventHandler, ReactNode, useEffect, useRef, useState} from "react"
 import styles from "./SpaceEngine.module.scss"
 import {OutlineChevrondoubledownIcon, OutlineChevrondoubleupIcon} from "@/assets/icon/outline"
+import {ExpandAndRetract} from "../plugins/operator/expandAndRetract/ExpandAndRetract"
+import {useInViewport, useMemoizedFn} from "ahooks"
+import {PageNodeItemProps, usePageInfo} from "@/store/pageInfo"
+import {shallow} from "zustand/shallow"
+import {YakitRoute, YakitRouteToPageInfo} from "@/routes/newRoute"
+import emiter from "@/utils/eventBus/eventBus"
 
-interface SpaceEngineProps {}
+interface SpaceEngineProps {
+    /**页面id */
+    pageId: string
+}
 export const SpaceEngine: React.FC<SpaceEngineProps> = React.memo((props) => {
-    // const {} = props
-    return <div>555</div>
+    const {pageId} = props
+    const {queryPagesDataById} = usePageInfo(
+        (s) => ({
+            queryPagesDataById: s.queryPagesDataById
+        }),
+        shallow
+    )
+    const initSpaceEnginePageInfo = useMemoizedFn(() => {
+        const currentItem: PageNodeItemProps | undefined = queryPagesDataById(YakitRoute.Space_Engine, pageId)
+        if (currentItem && currentItem.pageName) {
+            return currentItem.pageName
+        }
+        return YakitRouteToPageInfo[YakitRoute.Space_Engine].label
+    })
+    /**是否展开/收起 */
+    const [isExpand, setIsExpand] = useState<boolean>(false)
+    const [tabName, setTabName] = useState<string>(initSpaceEnginePageInfo())
+
+    const spaceEngineWrapperRef = useRef<HTMLDivElement>(null)
+    const [inViewport] = useInViewport(spaceEngineWrapperRef)
+
+    useEffect(() => {
+        if (inViewport) emiter.on("secondMenuTabDataChange", onSetTabName)
+        return () => {
+            emiter.off("secondMenuTabDataChange", onSetTabName)
+        }
+    }, [inViewport])
+    const onSetTabName = useMemoizedFn(() => {
+        setTabName(initSpaceEnginePageInfo())
+    })
+    const onExpand = useMemoizedFn((e) => {
+        setIsExpand(!isExpand)
+    })
+    return (
+        <div className={styles["space-engine-wrapper"]} ref={spaceEngineWrapperRef}>
+            <ExpandAndRetract className={styles["space-engine-heard"]} onExpand={onExpand} isExpand={isExpand}>
+                <span className={styles["space-engine-heard-tabName"]}>{tabName}</span>
+            </ExpandAndRetract>
+        </div>
+    )
 })
