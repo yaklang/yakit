6b5786c060606e016369b85d2fefb1cf7561a354
luoluo <1425735414@qq.com>
2024-01-19 11:06:40 +0800

    插件空间引擎页面改为固定页面

---
+++ app/renderer/src/main/src/pages/spaceEngine/SpaceEngine.tsx
@@ -0,0 +1,9 @@
+import React, {MouseEventHandler, ReactNode} from "react"
+import styles from "./SpaceEngine.module.scss"
+import {OutlineChevrondoubledownIcon, OutlineChevrondoubleupIcon} from "@/assets/icon/outline"
+
+interface SpaceEngineProps {}
+export const SpaceEngine: React.FC<SpaceEngineProps> = React.memo((props) => {
+    // const {} = props
+    return <div>555</div>
+})
