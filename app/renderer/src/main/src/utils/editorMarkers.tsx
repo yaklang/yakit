e1add6fdb8064094569d1ebdea6b34fc93dfc89b
wlz <lingze.wl@gmail.com>
2024-01-24 17:24:17 +0800

    feat(monaco): marker support Tag "Deprecated"

--- app/renderer/src/main/src/utils/editorMarkers.tsx
+++ app/renderer/src/main/src/utils/editorMarkers.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import monaco, {MarkerSeverity} from "monaco-editor";
+import monaco, {MarkerSeverity, MarkerTag, languages} from "monaco-editor";
 import {Uint8ArrayToString} from "@/utils/str";
 
 export interface IMonacoEditorMarker {
@@ -9,25 +9,40 @@ export interface IMonacoEditorMarker {
     startColumn: number
     endLineNumber: number
     endColumn: number
+    tags : MarkerTag[]
 }
 
 export interface YakStaticAnalyzeErrorResult {
     Message: Uint8Array
     RawMessage: Uint8Array
-    Severity: "error" | "warning" | string
+    Severity: "Error" | "Warning" | "Info" | "Hint" | string
     StartLineNumber: number
     StartColumn: number
     EndLineNumber: number
     EndColumn: number
+    Tag : "Unnecessary" | "Deprecated" | string
 }
+
+
+function getMarkerTags(name: string): MarkerTag []{
+    const tag = MarkerTag[name as keyof typeof MarkerTag];
+    return tag !== undefined ? [tag] : [];
+}
+
+function getMarkerSeverity(name: string): MarkerSeverity {
+    const severity = MarkerSeverity[name as keyof typeof MarkerSeverity];
+    return severity !== undefined ? severity : MarkerSeverity.Hint;
+}
+
 
 export const ConvertYakStaticAnalyzeErrorToMarker = (i: YakStaticAnalyzeErrorResult): IMonacoEditorMarker => {
     return {
         message: i.Message.length > 0 ? Uint8ArrayToString(i.Message) : Uint8ArrayToString(i.RawMessage),
-        severity: i.Severity === "error" ? MarkerSeverity.Error : MarkerSeverity.Warning,
+        severity: getMarkerSeverity(i.Severity),
         startLineNumber: parseInt(`${i.StartLineNumber}`),
         startColumn: parseInt(`${i.StartColumn}`),
         endLineNumber: parseInt(`${i.EndLineNumber}`),
         endColumn: parseInt(`${i.EndColumn}`),
+        tags: getMarkerTags(i.Tag),
     }
 }
