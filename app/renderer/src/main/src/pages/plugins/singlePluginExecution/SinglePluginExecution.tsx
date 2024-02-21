6cfb4156aee8b728541a723abaf65eb54abd7f1d
luoluo <1425735414@qq.com>
2024-02-21 11:07:07 +0800

    新建单个插件执行页面文件

---
+++ app/renderer/src/main/src/pages/plugins/singlePluginExecution/SinglePluginExecution.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+import {SinglePluginExecutionProps} from "./SinglePluginExecutionType"
+import styles from "SinglePluginExecution.module.scss"
+
+export const SinglePluginExecution: React.FC<SinglePluginExecutionProps> = React.memo((props) => {
+    return <div>66</div>
+})
---
+++ app/renderer/src/main/src/pages/plugins/singlePluginExecution/SinglePluginExecutionType.d.ts
@@ -0,0 +1,3 @@
+export interface SinglePluginExecutionProps{
+    yakScriptId: number
+}
