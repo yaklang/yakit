88e8246c2c7b2ad94503b685b0c4f958c7977457
song_xiao_lin <song_xiao_lin@126.com>
2024-03-18 16:51:40 +0800

    style: padding调整

--- app/renderer/src/main/src/pages/mitm/MITMServerHijacking/MITMHijackedContent.tsx
+++ app/renderer/src/main/src/pages/mitm/MITMServerHijacking/MITMHijackedContent.tsx
@@ -428,7 +428,7 @@ const MITMHijackedContent: React.FC<MITMHijackedContentProps> = React.memo((prop
     }, [alertMsg])
 
     return (
-        <div className={styles["mitm-hijacked-content"]} style={{paddingLeft: 13}}>
+        <div className={styles["mitm-hijacked-content"]} style={{paddingLeft: isFullScreen ? 13 : 5}}>
             <ReactResizeDetector
                 onResize={(w, h) => {
                     if (!w) {
