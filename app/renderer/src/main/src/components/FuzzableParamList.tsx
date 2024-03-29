785b4886808b98f018cceb2d08bac43720cb74fe
song_xiao_lin <63901989+song-xiao-lin@users.noreply.github.com>
2024-03-29 10:32:53 +0800

    pref: 数据包详情参数过长给省略号加提示 (#1543)

--- app/renderer/src/main/src/components/FuzzableParamList.tsx
+++ app/renderer/src/main/src/components/FuzzableParamList.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import {Button, Typography, Space, Table, Tag, Popconfirm} from "antd";
-import {FuzzableParams} from "./HTTPFlowTable/HTTPFlowTable";
-import {HTTPPacketFuzzable} from "./HTTPHistory";
+import React from "react"
+import {Button, Typography, Space, Table, Tag, Popconfirm} from "antd"
+import {FuzzableParams} from "./HTTPFlowTable/HTTPFlowTable"
+import {HTTPPacketFuzzable} from "./HTTPHistory"
 
 const {ipcRenderer} = window.require("electron")
-const {Text} = Typography;
+const {Text} = Typography
 
 export interface FuzzableParamListProp extends HTTPPacketFuzzable {
     data: FuzzableParams[]
@@ -12,43 +12,70 @@ export interface FuzzableParamListProp extends HTTPPacketFuzzable {
 }
 
 export const FuzzableParamList: React.FC<FuzzableParamListProp> = (props) => {
-    return <Table<FuzzableParams>
-        pagination={false}
-        dataSource={props.data}
-        rowKey={row => row.ParamName}
-        columns={[
-            {title: "参数名", render: (i: FuzzableParams) => <Tag>{i.ParamName}</Tag>},
-            {title: "参数位置", render: (i: FuzzableParams) => <Tag>{i.Position}</Tag>},
-            {
-                title: "参数原值", render: (i: FuzzableParams) => <Tag><Text style={{maxWidth: 500}} ellipsis={{
-                    tooltip: true,
-                }} copyable={true}>
-                    {i.OriginValue ? new Buffer(i.OriginValue).toString() : ""}
-                </Text></Tag>
-            },
-            {title: "IsHTTPS", render: (i: FuzzableParams) => <Tag>{i.IsHTTPS}</Tag>},
-            {
-                title: "操作", render: (i: FuzzableParams) => <Space>
-                    <Popconfirm title={"测试该参数将会暂时进入 Web Fuzzer"}
+    return (
+        <Table<FuzzableParams>
+            pagination={false}
+            dataSource={props.data}
+            rowKey={(row) => row.ParamName}
+            columns={[
+                {
+                    title: "参数名",
+                    render: (i: FuzzableParams) => (
+                        <Tag>
+                            <Text
+                                style={{maxWidth: 500}}
+                                ellipsis={{
+                                    tooltip: true
+                                }}
+                            >
+                                {i.ParamName}
+                            </Text>
+                        </Tag>
+                    )
+                },
+                {title: "参数位置", render: (i: FuzzableParams) => <Tag>{i.Position}</Tag>},
+                {
+                    title: "参数原值",
+                    render: (i: FuzzableParams) => (
+                        <Tag>
+                            <Text
+                                style={{maxWidth: 500}}
+                                ellipsis={{
+                                    tooltip: true
+                                }}
+                                copyable={true}
+                            >
+                                {i.OriginValue ? new Buffer(i.OriginValue).toString() : ""}
+                            </Text>
+                        </Tag>
+                    )
+                },
+                {title: "IsHTTPS", render: (i: FuzzableParams) => <Tag>{i.IsHTTPS}</Tag>},
+                {
+                    title: "操作",
+                    render: (i: FuzzableParams) => (
+                        <Space>
+                            <Popconfirm
+                                title={"测试该参数将会暂时进入 Web Fuzzer"}
                                 onConfirm={(e) => {
                                     ipcRenderer.invoke("send-to-tab", {
                                         type: "fuzzer",
-                                        data:{
+                                        data: {
                                             isHttps: i.IsHTTPS,
                                             request: new Buffer(i.AutoTemplate).toString("utf8")
                                         }
                                     })
                                     if (props.sendToWebFuzzer) props.sendToWebFuzzer()
                                 }}
-                    >
-                        <Button
-                            type={"primary"} size={"small"}
-                        >模糊测试该参数</Button>
-                    </Popconfirm>
-                </Space>
-            },
-        ]}
-    >
-
-    </Table>
-};
+                            >
+                                <Button type={"primary"} size={"small"}>
+                                    模糊测试该参数
+                                </Button>
+                            </Popconfirm>
+                        </Space>
+                    )
+                }
+            ]}
+        ></Table>
+    )
+}
--- app/renderer/src/main/src/components/HTTPFlowExtractedDataTable.tsx
+++ app/renderer/src/main/src/components/HTTPFlowExtractedDataTable.tsx
@@ -1,17 +1,17 @@
-import React, {useEffect, useState} from "react";
-import {Button, Pagination, Table} from "antd";
-import {Paging} from "@/utils/yakQueryHTTPFlow";
-import {genDefaultPagination, QueryGeneralRequest, QueryGeneralResponse} from "@/pages/invoker/schema";
-import {useMemoizedFn} from "ahooks";
-import {CopyableField} from "@/utils/inputUtil";
+import React, {useEffect, useState} from "react"
+import {Button, Pagination, Typography, Table} from "antd"
+import {Paging} from "@/utils/yakQueryHTTPFlow"
+import {genDefaultPagination, QueryGeneralRequest, QueryGeneralResponse} from "@/pages/invoker/schema"
+import {useMemoizedFn} from "ahooks"
+import {CopyableField} from "@/utils/inputUtil"
 import styles from "./hTTPFlowDetail.module.scss"
-
+const {Text} = Typography
 export interface HTTPFlowExtractedDataTableProp {
     title: React.ReactNode
     httpFlowHash: string
 }
 
-const {ipcRenderer} = window.require("electron");
+const {ipcRenderer} = window.require("electron")
 
 interface HTTPFlowExtractedData {
     Id: number
@@ -24,76 +24,98 @@ interface HTTPFlowExtractedData {
 }
 
 export const HTTPFlowExtractedDataTable: React.FC<HTTPFlowExtractedDataTableProp> = (props) => {
-    const [pagination, setPagination] = useState<Paging>(genDefaultPagination());
-    const [loading, setLoading] = useState(false);
-    const [data, setData] = useState<HTTPFlowExtractedData[]>([]);
-    const [params, setParams] = useState<QueryGeneralRequest & { HTTPFlowHash: string }>({
-        HTTPFlowHash: props.httpFlowHash, Pagination: genDefaultPagination()
+    const [pagination, setPagination] = useState<Paging>(genDefaultPagination())
+    const [loading, setLoading] = useState(false)
+    const [data, setData] = useState<HTTPFlowExtractedData[]>([])
+    const [params, setParams] = useState<QueryGeneralRequest & {HTTPFlowHash: string}>({
+        HTTPFlowHash: props.httpFlowHash,
+        Pagination: genDefaultPagination()
     })
     const [total, setTotal] = useState(0)
 
     const update = useMemoizedFn((page?: number, limit?: number, HTTPFlowHash?: string) => {
         const paginationProps = {
             Page: page || 1,
-            Limit: limit || pagination.Limit,
-        };
+            Limit: limit || pagination.Limit
+        }
         setLoading(true)
-        ipcRenderer.invoke("QueryMITMRuleExtractedData", {
-            ...params, Pagination: paginationProps, HTTPFlowHash:HTTPFlowHash?HTTPFlowHash:params.HTTPFlowHash
-        }).then((r: QueryGeneralResponse<HTTPFlowExtractedData>) => {
-            setData(r.Data);
-            setPagination(r.Pagination)
-            setTotal(r.Total)
-        }).finally(() => setTimeout(() => setLoading(false), 300))
+        ipcRenderer
+            .invoke("QueryMITMRuleExtractedData", {
+                ...params,
+                Pagination: paginationProps,
+                HTTPFlowHash: HTTPFlowHash ? HTTPFlowHash : params.HTTPFlowHash
+            })
+            .then((r: QueryGeneralResponse<HTTPFlowExtractedData>) => {
+                setData(r.Data)
+                setPagination(r.Pagination)
+                setTotal(r.Total)
+            })
+            .finally(() => setTimeout(() => setLoading(false), 300))
     })
 
     useEffect(() => {
         if (!props.httpFlowHash) {
             return
         }
-        update(1, 10,props.httpFlowHash)
+        update(1, 10, props.httpFlowHash)
     }, [props.httpFlowHash])
 
-    return <div className={styles["httpFlow-data-table"]}>
-        <Table<HTTPFlowExtractedData>
-            bordered={true}
-            title={() => <>
-                {props.title}
-            </>}
-            dataSource={data}
-            key={"Id"}
-            columns={[
-                {
-                    title: "规则名",
-                    render: (i: HTTPFlowExtractedData) => <div className='content-ellipsis' style={{width:100,overflow:"hidden"}}>{i.RuleName}</div>,
-                    width: 100
-                },
-                {
-                    title: "规则数据",
-                    render: (i: HTTPFlowExtractedData) => <div className='content-ellipsis' style={{width:200,overflow:"hidden"}}>{i.Data.slice(0,50)}</div>,
-                    width: 210
-                },
-            ]}
-            scroll={{x: "auto"}}
-            loading={loading}
-            size={"small"} style={{margin: 0, padding: 0}}
-            pagination={{
-                pageSize: pagination.Limit,
-                showSizeChanger: true,
-                total,
-                pageSizeOptions: ["5", "10", "20"],
-                onChange: (page: number, limit?: number) => {
-                    // dispatch({type: "updateParams", payload: {page, limit}})
-                    update(page, limit)
-                },
-                onShowSizeChange: (old, limit) => {
-                    // dispatch({type: "updateParams", payload: {page: 1, limit}})
-                    update(1, limit)
-                }
-            }}
-
-        >
-
-        </Table>
-    </div>
-};
+    return (
+        <div className={styles["httpFlow-data-table"]}>
+            <Table<HTTPFlowExtractedData>
+                bordered={true}
+                title={() => <>{props.title}</>}
+                dataSource={data}
+                key={"Id"}
+                columns={[
+                    {
+                        title: "规则名",
+                        render: (i: HTTPFlowExtractedData) => (
+                            <Text
+                                style={{maxWidth: 100}}
+                                ellipsis={{
+                                    tooltip: true
+                                }}
+                            >
+                                {i.RuleName}
+                            </Text>
+                        ),
+                        width: 100
+                    },
+                    {
+                        title: "规则数据",
+                        render: (i: HTTPFlowExtractedData) => (
+                            <Text
+                                style={{maxWidth: 200}}
+                                ellipsis={{
+                                    tooltip: true
+                                }}
+                            >
+                                {i.Data}
+                            </Text>
+                        ),
+                        width: 210
+                    }
+                ]}
+                scroll={{x: "auto"}}
+                loading={loading}
+                size={"small"}
+                style={{margin: 0, padding: 0}}
+                pagination={{
+                    pageSize: pagination.Limit,
+                    showSizeChanger: true,
+                    total,
+                    pageSizeOptions: ["5", "10", "20"],
+                    onChange: (page: number, limit?: number) => {
+                        // dispatch({type: "updateParams", payload: {page, limit}})
+                        update(page, limit)
+                    },
+                    onShowSizeChange: (old, limit) => {
+                        // dispatch({type: "updateParams", payload: {page: 1, limit}})
+                        update(1, limit)
+                    }
+                }}
+            ></Table>
+        </div>
+    )
+}
