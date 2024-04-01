d2c40216a09e68918d8efbe4a1bda14d4ee59a6c
luoluo <1425735414@qq.com>
2024-04-01 17:01:49 +0800

    add file:废弃的一级和二级的菜单文件

---
+++ app/renderer/src/main/src/routes/deprecatedMenu.ts
@@ -0,0 +1,39 @@
+/**
+ * @description 社区版
+ * 废弃的一级菜单
+ * */
+export const CommunityDeprecatedFirstMenu: string[] = ["子域名收集"]
+
+/**
+ * @description 社区版
+ * 废弃的二级菜单
+ * */
+export const CommunityDeprecatedSecondMenu: string[] = [
+    "api提取",
+    "空间引擎集成版本",
+    "域名、IP提取",
+    "域名批量转IP并查CDN",
+    "IP反查域名",
+    "批量备案查询",
+    "空间引擎集成版本",
+    "网站信息获取",
+    "主域名提取",
+    "杀软匹配tasklist /svc"
+]
+
+/**
+ * @description 企业版
+ * 废弃的一级菜单
+ * */
+export const EnterpriseDeprecatedFirstMenu: string[] = []
+
+/**
+ * @description 企业版
+ * 废弃的二级菜单
+ * */
+export const EnterpriseDeprecatedSecondMenu: string[] = [
+    "项目管理(Beta*)",
+    "插件执行结果",
+    "api提取",
+    "空间引擎集成版本"
+]
