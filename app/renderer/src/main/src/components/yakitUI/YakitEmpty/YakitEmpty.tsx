72935f115e215adf8a81da26db56b28b0dd71a4a
nonight <378099757@qq.com>
2024-05-27 15:56:17 +0800

    给yakitempty和plugintabs增加包装类字段

--- app/renderer/src/main/src/components/yakitUI/YakitEmpty/YakitEmpty.tsx
+++ app/renderer/src/main/src/components/yakitUI/YakitEmpty/YakitEmpty.tsx
@@ -1,29 +1,28 @@
-import { Empty } from "antd"
+import {Empty} from "antd"
 import React from "react"
-import { YakitEmptyProps } from "./YakitEmptyType"
+import {YakitEmptyProps} from "./YakitEmptyType"
+import classNames from "classnames"
 import styles from "./YakitEmpty.module.scss"
-import Icon from "@ant-design/icons"
-import { CustomIconComponentProps } from "@ant-design/icons/lib/components/Icon"
-import EmptyPng from "./empty.png";
+import EmptyPng from "./empty.png"
 
 /**
  * @description:YakitEmpty
  * @augments YakitEmptyProps 继承antd的 Empty 默认属性
  */
 export const YakitEmpty: React.FC<YakitEmptyProps> = (props) => {
-    const { title = "暂无数据",...restProps } = props
+    const {title = "暂无数据", titleClassName, ...restProps} = props
     return (
         <Empty
-            image={<img src={EmptyPng} alt="" />}
+            image={<img src={EmptyPng} alt='' />}
             imageStyle={
                 props.imageStyle
                     ? props.imageStyle
                     : {
-                        height: 160,
-                        width: 160,
-                        margin: 'auto',
-                        marginBottom: 24
-                    }
+                          height: 160,
+                          width: 160,
+                          margin: "auto",
+                          marginBottom: 24
+                      }
             }
             {...restProps}
             description={
@@ -31,7 +30,7 @@ export const YakitEmpty: React.FC<YakitEmptyProps> = (props) => {
                     props.descriptionReactNode
                 ) : (
                     <div className={styles["yakit-empty"]}>
-                        <div className={styles["yakit-empty-title"]}>{title}</div>
+                        <div className={classNames(styles["yakit-empty-title"], titleClassName)}>{title}</div>
                         <div className={styles["yakit-empty-description"]}>{props.description}</div>
                     </div>
                 )
--- app/renderer/src/main/src/components/yakitUI/YakitEmpty/YakitEmptyType.d.ts
+++ app/renderer/src/main/src/components/yakitUI/YakitEmpty/YakitEmptyType.d.ts
@@ -1,6 +1,9 @@
 import {ReactNode} from "react"
 import {EmptyProps} from "antd"
 export interface YakitEmptyProps extends EmptyProps {
-    title?: string|null|ReactNode
+    title?: string | null | ReactNode
     descriptionReactNode?: ReactNode
+
+    /** title内容的修饰类 */
+    titleClassName?: string
 }
