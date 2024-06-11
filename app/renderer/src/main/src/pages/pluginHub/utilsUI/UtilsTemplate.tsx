1afb4d7a8cc224e6c7e08a1eda96c62103756876
nonight <378099757@qq.com>
2024-06-12 00:52:37 +0800

    feat: 新增二次确认框组件

---
+++ app/renderer/src/main/src/pages/pluginHub/utilsUI/UtilsTemplate.tsx
@@ -0,0 +1,54 @@
+import React, {memo, useState} from "react"
+import {useMemoizedFn} from "ahooks"
+import {YakitCheckbox} from "@/components/yakitUI/YakitCheckbox/YakitCheckbox"
+import {YakitHint} from "@/components/yakitUI/YakitHint/YakitHint"
+import {setRemoteValue} from "@/utils/kv"
+
+// import classNames from "classnames"
+import styles from "./UtilsTemplate.module.scss"
+
+interface RecycleOptFooterExtraProps {
+    visible: boolean
+    title: string
+    content: string
+    cacheKey: string
+    onCallback: (isOk: boolean, cache: boolean) => any
+}
+/** @name 带不再提示选项的二次确认弹框*/
+export const NoPromptHint: React.FC<RecycleOptFooterExtraProps> = memo((props) => {
+    const {visible, title, content, cacheKey, onCallback} = props
+
+    const [checked, setChecked] = useState<boolean>(false)
+    const handleCallback = useMemoizedFn((isOK: boolean) => {
+        if (isOK) {
+            if (cacheKey && checked) {
+                setRemoteValue(cacheKey, `${checked}`)
+            }
+            onCallback(true, checked)
+        } else {
+            onCallback(false, false)
+        }
+    })
+
+    const handleOK = useMemoizedFn(() => {
+        handleCallback(true)
+    })
+    const handleCancel = useMemoizedFn(() => {
+        handleCallback(false)
+    })
+
+    return (
+        <YakitHint
+            visible={visible}
+            title={title || ""}
+            content={content || ""}
+            onOk={handleOK}
+            onCancel={handleCancel}
+            footerExtra={
+                <YakitCheckbox checked={checked} onChange={(e) => setChecked(e.target.checked)}>
+                    下次不再提醒
+                </YakitCheckbox>
+            }
+        />
+    )
+})
