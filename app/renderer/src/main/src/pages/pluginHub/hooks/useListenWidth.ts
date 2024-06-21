1e007c14499cfc54bc984651522974110b393fc2
nonight <378099757@qq.com>
2024-06-21 18:05:07 +0800

    调整样式闪动问题

--- app/renderer/src/main/src/pages/pluginHub/hooks/useListenWidth.ts
+++ app/renderer/src/main/src/pages/pluginHub/hooks/useListenWidth.ts
@@ -1,4 +1,4 @@
-import {MutableRefObject, useEffect, useState} from "react"
+import {MutableRefObject, useEffect, useRef, useState} from "react"
 import {useCreation, useDebounceFn, useMemoizedFn} from "ahooks"
 
 type TargetValue<T> = T | undefined | null
@@ -7,9 +7,13 @@ type BasicTarget<T extends TargetType = Element> = string | TargetValue<T> | Mut
 
 function useListenWidth(target: BasicTarget): number {
     const [width, setWidth] = useState<number>(0)
+    const oldWidth = useRef<number>(0)
 
     const onSetWrapperWidth = useDebounceFn(
         useMemoizedFn((width: number) => {
+            if (width === 0) return
+            if (oldWidth.current === width) return
+            oldWidth.current = width
             setWidth(width)
         }),
         {wait: 100}
