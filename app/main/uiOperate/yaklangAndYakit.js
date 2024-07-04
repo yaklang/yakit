c123c15de2e7663e9277a99f75dd1be49671f758
luoluo <1425735414@qq.com>
2024-07-04 16:28:12 +0800

    feat:asyncFetchCheckYaklangSource:区分arm64和amd64

--- app/main/uiOperate/yaklangAndYakit.js
+++ app/main/uiOperate/yaklangAndYakit.js
@@ -93,13 +93,21 @@ module.exports = (win, getClient) => {
             let url = ''
             switch (process.platform) {
                 case "darwin":
-                    url = `https://aliyun-oss.yaklang.com/yak/${version}/yak_darwin_amd64.sha256.txt`
+                    if (process.arch === "arm64") {
+                        url = `https://aliyun-oss.yaklang.com/yak/${version}/yak_darwin_arm64.sha256.txt`
+                    } else {
+                        url = `https://aliyun-oss.yaklang.com/yak/${version}/yak_darwin_amd64.sha256.txt`
+                    }
                     break
                 case "win32":
                     url = `https://aliyun-oss.yaklang.com/yak/${version}/yak_windows_amd64.exe.sha256.txt`
                     break
                 case "linux":
-                    url = `https://aliyun-oss.yaklang.com/yak/${version}/yak_linux_amd64.sha256.txt`
+                    if (process.arch === "arm64") {
+                        url = `https://aliyun-oss.yaklang.com/yak/${version}/yak_linux_arm64.sha256.txt`
+                    } else {
+                        url = `https://aliyun-oss.yaklang.com/yak/${version}/yak_linux_amd64.sha256.txt`
+                    }
                     break
             }
             if (url === '') {
