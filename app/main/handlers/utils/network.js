e85d0529a55c8aa40e19fa0bef5fc8882b058e0c
luoluo <1425735414@qq.com>
2024-07-04 17:47:14 +0800

    feat:getCheckTextUrl增加获取可用域的逻辑，使用可用域

--- app/main/handlers/utils/network.js
+++ app/main/handlers/utils/network.js
@@ -84,24 +84,25 @@ async function getAvailableOSSDomain() {
 }
 
 /**获取校验url */
-const getCheckTextUrl=async()=>{
-    let url=''
+const getCheckTextUrl = async () => {
+    const domain = await getAvailableOSSDomain();
+    let url = ''
     switch (process.platform) {
         case "darwin":
             if (process.arch === "arm64") {
-                url = `https://aliyun-oss.yaklang.com/yak/${version}/yak_darwin_arm64.sha256.txt`
+                url = `https://${domain}/yak/${version}/yak_darwin_arm64.sha256.txt`
             } else {
-                url = `https://aliyun-oss.yaklang.com/yak/${version}/yak_darwin_amd64.sha256.txt`
+                url = `https://${domain}/yak/${version}/yak_darwin_amd64.sha256.txt`
             }
             break
         case "win32":
-            url = `https://aliyun-oss.yaklang.com/yak/${version}/yak_windows_amd64.exe.sha256.txt`
+            url = `https://${domain}/yak/${version}/yak_windows_amd64.exe.sha256.txt`
             break
         case "linux":
             if (process.arch === "arm64") {
-                url = `https://aliyun-oss.yaklang.com/yak/${version}/yak_linux_arm64.sha256.txt`
+                url = `https://${domain}.com/yak/${version}/yak_linux_arm64.sha256.txt`
             } else {
-                url = `https://aliyun-oss.yaklang.com/yak/${version}/yak_linux_amd64.sha256.txt`
+                url = `https://${domain}.com/yak/${version}/yak_linux_amd64.sha256.txt`
             }
             break
         default:
