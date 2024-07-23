e234db955a4a4e9973afb3ba6c84c531edf61779
song_xiao_lin <song_xiao_lin@126.com>
2024-07-23 12:27:23 +0800

    del

--- app/renderer/src/main/src/pages/mitm/MITMYakScriptLoader.tsx
+++ app/renderer/src/main/src/pages/mitm/MITMYakScriptLoader.tsx
@@ -19,7 +19,6 @@ export const MITMYakScriptLoader = React.memo((p: MITMYakScriptLoaderProps) => {
     const {hooks, script, onSubmitYakScriptId, onRemoveHook, defaultPlugins, setDefaultPlugins, status} = p
     const [i, setI] = useState(script)
     useEffect(() => {
-        console.log(11, script);
         setI(script)
     }, [script])
     const onCheckboxClicked = useMemoizedFn(() => {
