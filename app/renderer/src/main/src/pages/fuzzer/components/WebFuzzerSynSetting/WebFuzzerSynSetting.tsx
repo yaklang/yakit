2dd45586cebd9828397c09b35ad34e8448342649
luoluo <1425735414@qq.com>
2024-08-23 16:04:47 +0800

    feat:同步配置功能

--- app/renderer/src/main/src/pages/fuzzer/components/WebFuzzerSynSetting/WebFuzzerSynSetting.tsx
+++ app/renderer/src/main/src/pages/fuzzer/components/WebFuzzerSynSetting/WebFuzzerSynSetting.tsx
@@ -1,13 +1,18 @@
-import {WebFuzzerSynSettingProps} from "./WebFuzzerSynSettingType"
+import {WebFuzzerSynSettingFormValueProps, WebFuzzerSynSettingProps} from "./WebFuzzerSynSettingType"
 import styles from "./WebFuzzerSynSetting.module.scss"
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import {Checkbox, Form} from "antd"
 import {YakitCheckbox} from "@/components/yakitUI/YakitCheckbox/YakitCheckbox"
 import {YakitRadioButtons} from "@/components/yakitUI/YakitRadioButtons/YakitRadioButtons"
 import {YakitSelect} from "@/components/yakitUI/YakitSelect/YakitSelect"
 import {YakitButton} from "@/components/yakitUI/YakitButton/YakitButton"
-import {on} from "events"
-import {useMemoizedFn} from "ahooks"
+import {useCreation, useMemoizedFn} from "ahooks"
+import {PageNodeItemProps, PageProps, usePageInfo} from "@/store/pageInfo"
+import {shallow} from "zustand/shallow"
+import {YakitRoute} from "@/enums/yakitRoute"
+import {yakitNotify} from "@/utils/notification"
+import {AdvancedConfigValueProps} from "../../HttpQueryAdvancedConfig/HttpQueryAdvancedConfigType"
+import {toolDelInvalidKV} from "@/utils/tool"
 
 const rangeList = [
     {
@@ -29,24 +34,118 @@ const types = [
         label: "规则"
     }
 ]
+const getConfigValue = (value: AdvancedConfigValueProps) => {
+    const config = {
+        ...value,
+        matchers: undefined,
+        extractors: undefined,
+        params: undefined,
+        methodGet: undefined,
+        methodPost: undefined,
+        cookie: undefined,
+        headers: undefined
+    }
+    return toolDelInvalidKV(config)
+}
+const getRuleValue = (value: AdvancedConfigValueProps) => {
+    const rule = {
+        matchers: value.matchers,
+        extractors: value.extractors,
+        params: value.params,
+        methodGet: value.methodGet,
+        methodPost: value.methodPost,
+        cookie: value.cookie,
+        headers: value.headers
+    }
+    return toolDelInvalidKV(rule)
+}
 const WebFuzzerSynSetting: React.FC<WebFuzzerSynSettingProps> = React.memo((props) => {
+    const {pages, setPagesData, queryPagesDataById} = usePageInfo(
+        (s) => ({
+            pages: s.pages,
+            setPagesData: s.setPagesData,
+            queryPagesDataById: s.queryPagesDataById
+        }),
+        shallow
+    )
     const {pageId, onClose} = props
     const [loading, setLoading] = useState<boolean>(false)
+    const [pageList, setPageList] = useState<PageNodeItemProps[]>([])
     const [form] = Form.useForm()
     const range = Form.useWatch("range", form)
+
+    useEffect(() => {
+        const wfPage: PageProps | undefined = pages.get(YakitRoute.HTTPFuzzer)
+        if (wfPage) {
+            const newPageList = wfPage.pageList.filter((ele) => !ele.pageId.endsWith("group")) // 排除组的数据
+            setPageList(newPageList)
+        }
+    }, [pages])
+
     const onCancel = useMemoizedFn(() => {
         onClose()
     })
     const onSyn = useMemoizedFn(() => {
-        
-        form.validateFields().then((value) => {
+        form.validateFields().then((value: WebFuzzerSynSettingFormValueProps) => {
+            const wfPageList: PageProps | undefined = pages.get(YakitRoute.HTTPFuzzer)
+            const currentPage: PageNodeItemProps | undefined = queryPagesDataById(YakitRoute.HTTPFuzzer, pageId)
+            if (!wfPageList) {
+                yakitNotify("error", "WF页面数据为空")
+                return
+            }
+            if (!(currentPage && currentPage.pageParamsInfo && currentPage.pageParamsInfo.webFuzzerPageInfo)) {
+                yakitNotify("error", "同步数据页面未找到")
+                return
+            }
             setLoading(true)
+            /**处理同步内容 */
+            const advancedConfigValue = currentPage.pageParamsInfo?.webFuzzerPageInfo?.advancedConfigValue
+            let synValue = advancedConfigValue
+            if (value.type.length === 1 && value.type.includes("config")) {
+                synValue = getConfigValue(advancedConfigValue)
+            }
+            if (value.type.length === 1 && value.type.includes("rule")) {
+                synValue = getRuleValue(advancedConfigValue)
+            }
+            /**处理同步范围 */
+            let newWFPageList = wfPageList.pageList.map((item) => {
+                if (
+                    item.pageParamsInfo.webFuzzerPageInfo &&
+                    item.pageParamsInfo.webFuzzerPageInfo.advancedConfigValue
+                ) {
+                    const advancedConfigValue = item.pageParamsInfo.webFuzzerPageInfo.advancedConfigValue
+                    if (value.range === "all") {
+                        item.pageParamsInfo.webFuzzerPageInfo.advancedConfigValue = {
+                            ...advancedConfigValue,
+                            ...synValue
+                        }
+                    }
+                    if (value.range === "batch" && value.ids.includes(item.pageId)) {
+                        item.pageParamsInfo.webFuzzerPageInfo.advancedConfigValue = {
+                            ...advancedConfigValue,
+                            ...synValue
+                        }
+                    }
+                }
+                return item
+            })
+            const pageNodeInfo: PageProps = {
+                ...wfPageList,
+                pageList: newWFPageList
+            }
+            setPagesData(YakitRoute.HTTPFuzzer, pageNodeInfo)
             setTimeout(() => {
                 setLoading(false)
-                // onClose()
+                onClose()
             }, 200)
         })
     })
+    const pageListOption = useCreation(() => {
+        return pageList.map((ele) => ({
+            label: ele.pageName,
+            value: ele.pageId
+        }))
+    }, [pageList])
     return (
         <div className={styles["wf-syn-setting"]}>
             <Form
@@ -80,8 +179,22 @@ const WebFuzzerSynSetting: React.FC<WebFuzzerSynSettingProps> = React.memo((prop
                     name='range'
                     extra={
                         range === "batch" && (
-                            <Form.Item name='ids' style={{marginTop: 8}}>
-                                <YakitSelect mode='tags' />
+                            <Form.Item
+                                name='ids'
+                                style={{marginTop: 8}}
+                                rules={[{required: true, message: `请选择需要同步的页面`}]}
+                            >
+                                <YakitSelect
+                                    allowClear
+                                    mode='multiple'
+                                    options={pageListOption}
+                                    filterOption={(inputValue, option) => {
+                                        if (option?.label && typeof option?.label === "string") {
+                                            return option?.label?.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+                                        }
+                                        return false
+                                    }}
+                                />
                             </Form.Item>
                         )
                     }
--- app/renderer/src/main/src/pages/fuzzer/components/WebFuzzerSynSetting/WebFuzzerSynSettingType.d.ts
+++ app/renderer/src/main/src/pages/fuzzer/components/WebFuzzerSynSetting/WebFuzzerSynSettingType.d.ts
@@ -2,3 +2,9 @@ export interface WebFuzzerSynSettingProps {
     pageId: string
     onClose: () => void
 }
+
+export interface WebFuzzerSynSettingFormValueProps {
+    type: string[]
+    range: string
+    ids: string[]
+}
