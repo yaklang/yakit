f5a74ae8c86c0ad1d4cf23deb2f4f070d8e41117
luoluo <1425735414@qq.com>
2024-08-27 11:32:46 +0800

    feat:PluginTabs增加type为line的样式

--- app/renderer/src/main/src/components/businessUI/PluginTabs/PluginTabs.tsx
+++ app/renderer/src/main/src/components/businessUI/PluginTabs/PluginTabs.tsx
@@ -9,16 +9,22 @@ const {TabPane} = Tabs
 interface PluginTabsProps extends Omit<TabsProps, "size" | "type"> {
     /** @deprecated 组件无法设置该属性,默认定值为 default */
     size?: "default"
-    /** @deprecated 组件无法设置该属性,默认定值为 card */
-    type?: "card"
+    type?: "card" | "line"
     wrapperClassName?: string
 }
 
 const PluginTabs: React.FC<PluginTabsProps> = (props) => {
-    const {children, size = "default", type = "card", wrapperClassName, ...rest} = props
-
+    const {children, size = "default", type = "card", wrapperClassName = "", ...rest} = props
     return (
-        <div className={classNames(styles["plugin-tabs"], wrapperClassName)}>
+        <div
+            className={classNames(
+                {
+                    [styles["plugin-tabs"]]: type === "card",
+                    [styles["plugin-tabs-line"]]: type === "line" // 目前只适用于type为card下的二级tab情况，其他情况未测试
+                },
+                wrapperClassName
+            )}
+        >
             <Tabs {...rest} type='card'>
                 {children}
             </Tabs>
