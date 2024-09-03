658fd9bc594732724e510017099d106848893eab
song_xiao_lin <song_xiao_lin@126.com>
2024-09-03 17:54:19 +0800

    feat: mitm免配置浏览器额外参数设置

--- app/main/handlers/chromelauncher.js
+++ app/main/handlers/chromelauncher.js
@@ -1,8 +1,8 @@
-const {ipcMain} = require("electron")
-const {launch, killAll, getChromePath} = require("chrome-launcher")
+const { ipcMain } = require("electron")
+const { launch, killAll, getChromePath } = require("chrome-launcher")
 const fs = require("fs")
 const path = require("path")
-const {YakitProjectPath} =require("../filePath")
+const { YakitProjectPath } = require("../filePath")
 const myUserDataDir = path.join(YakitProjectPath, "chrome-profile")
 
 const disableExtensionsExceptStr = (host, port, username, password) => `
@@ -129,7 +129,7 @@ module.exports = (win, getClient) => {
     })
 
     ipcMain.handle("LaunchChromeWithParams", async (e, params) => {
-        const {port, host, chromePath, userDataDir, username, password, disableCACertPage} = params
+        const { port, host, chromePath, userDataDir, username, password, disableCACertPage, chromeFlags } = params
         const portInt = parseInt(`${port}`)
         const hostRaw = `${host}`
         if (hostRaw === "undefined" || hostRaw.includes("/") || hostRaw.split(":").length > 1) {
@@ -143,48 +143,14 @@ module.exports = (win, getClient) => {
         let launchOpt = {
             startingUrl: disableCACertPage === false ? "http://mitm" : "chrome://newtab", // 确保在启动时打开 chrome://newtab 页面。
             chromeFlags: [
-                `--no-system-proxy-config-service`, // 禁用系统代理配置服务。
-                `--proxy-bypass-list=<-loopback>`, // 为代理设置回避列表，不代理回环地址。
                 `--proxy-server=http://${hostRaw}:${portInt}`, // 设置具体的代理服务器地址和端口。
-
-                `--ignore-certificate-errors`, // 忽略 SSL 证书错误。
-                `--test-type`, // 表示这是一个测试实例。
-                // `--no-sandbox`, // https://stackoverflow.com/questions/64788142/nodejs-error-connect-econnrefused-127-0-0-1port-chrome-remote-interface
-                `--ignore-urlfetcher-cert-requests`, // 忽略 URL fetcher 的证书请求。
-                `--disable-webrtc`, // 禁用 WebRTC。
-                `--disable-component-extensions-with-background-pages`, // 禁用带有背景页的组件扩展。
-                `--disable-extensions`, // 禁用所有扩展。
-                `--disable-notifications`, // 禁用通知。
-                `--force-webrtc-ip-handling-policy=default_public_interface_only`, // 强制 WebRTC IP 处理策略仅使用默认的公共接口。
-                `--disable-ipc-flooding-protection`, // 禁用 IPC 洪水攻击保护。
-                `--disable-xss-auditor`, // 禁用 XSS 审查器。
-                `--disable-bundled-ppapi-flash`, // 禁用捆绑的 PPAPI Flash 版本。
-                `--disable-plugins-discovery`, // 禁止插件发现。
-                `--disable-default-apps`, // 禁用默认应用。
-                `--disable-prerender-local-predictor`, // 禁用本地预加载页面的预测功能。
-                `--disable-sync`, // 禁用同步功能。
-                `--disable-breakpad`, // 禁用 Breakpad 崩溃报告。
-                `--disable-crash-reporter`, // 禁用崩溃报告器。
-                `--disk-cache-size=0`, // 设置磁盘缓存大小为 0。
-                `--disable-settings-window`, // 禁用设置窗口。
-                `--disable-speech-api`, // 禁用语音API。
-                `--disable-file-system`, // 禁用文件系统API。
-                `--disable-presentation-api`, // 禁用演示API。
-                `--disable-permissions-api`, // 禁用权限API。
-                `--disable-new-zip-unpacker`, // 禁用新 ZIP 解压功能。
-                `--disable-media-session-api`, // 禁用媒体会话API。
-                `--no-experiments`, // 禁止实验。
-                `--no-events`, // 不发送事件。
-                `--no-first-run`, // 启动时跳过首次运行向导。
-                `--no-default-browser-check`, // 启动时不检查默认浏览器。
-                `--no-pings`, // 禁用 ping 跟踪。
-                `--no-service-autorun`, // 不自动运行服务。
-                `--media-cache-size=0`, // 设置媒体缓存大小为0。
-                `--use-fake-device-for-media-stream`, // 使用虚拟设备来捕获媒体流。
-                `--dbus-stub`, // 使用 DBus 存根。
-                `--disable-background-networking`, // 禁用后台网络活动。
-                `--disable-component-update`, // 不要更新 chrome://components/ 中列出的浏览器“组件”
-                `--disable-features=ChromeWhatsNewUI,HttpsUpgrades,OptimizationHints` // 禁用特定的功能。
+                ...chromeFlags.filter(item => !item.disabled).map(item => {
+                    if (item.variableValues) {
+                        return item.parameterName + "=" + item.variableValues
+                    } else {
+                        return item.parameterName
+                    }
+                })
             ]
         }
         if (userDataDir) {
@@ -196,20 +162,13 @@ module.exports = (win, getClient) => {
         // 用户名/密码 参数重构
         if (username.length > 0 && password.length > 0) {
             try {
-                // 屏蔽项
-                // `--proxy-server=http://${hostRaw}:${portInt}`,  // 设置具体的代理服务器地址和端口。
-                // `--disable-extensions`,  // 禁用所有扩展。
-                launchOpt["chromeFlags"] = launchOpt.chromeFlags.filter(
-                    (item) => !(item.startsWith("--proxy-server=http://") || item === "--disable-extensions")
-                )
-
                 // 要写入的内容
                 const exceptContent = disableExtensionsExceptStr(host, port, username, password)
                 const manifestContent = manifestStr
 
                 // 创建文件夹 { recursive: true } 选项确保如果文件夹的上级目录也不存在时，一同创建。
                 if (!fs.existsSync(commonFilePath)) {
-                    fs.mkdirSync(commonFilePath, {recursive: true})
+                    fs.mkdirSync(commonFilePath, { recursive: true })
                 }
 
                 // 使用 fs.writeFileSync创建文件 写入内容到临时文件
