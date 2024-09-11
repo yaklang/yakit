43c9150610a0cf38dac6caaed9b962e56d381918
luoluo <1425735414@qq.com>
2024-09-11 14:47:04 +0800

    add file:新建记事本管理页面文件和记事本编辑页面文件

---
+++ app/renderer/src/main/src/pages/notepadManage/notepadManage/NotepadManage.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import {NotepadManageProps} from "./NotepadManageType"
+import styles from "./NotepadManage.module.scss"
+
+const NotepadManage: React.FC<NotepadManageProps> = React.memo((props) => {
+    return <div></div>
+})
+
+export default NotepadManage
