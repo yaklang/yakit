1f00f169e87a375746f970c46e90d8e7dfb40c21
nonight <378099757@qq.com>
2024-09-11 17:15:51 +0800

    feat: 新增 YakitImageTextarea 组件(未完成)

--- app/renderer/src/main/src/pages/pluginEditor/yakitImageTextarea/YakitImageTextareaType.d.ts
+++ app/renderer/src/main/src/pages/pluginEditor/yakitImageTextarea/YakitImageTextareaType.d.ts
@@ -1,2 +1,30 @@
-export interface YakitImageTextareaProps {}
+import {ForwardedRef} from "react"
 
+export interface YakitImageTextareaProps {
+    ref?: ForwardedRef<YakitImageTextareaRefProps>
+}
+
+export interface YakitImageTextareaRefProps {
+    /** 设置引用内容 */
+    setQuotationInfo?: (info: {name: string; content: string}) => void
+}
+
+/** 编辑框内容 */
+export interface YakitImageTextareaInfoProps {
+    type: "text" | "image"
+    content: string
+    order: string
+}
+
+/** 新建元素类型 */
+export type CreateNewElementType = "div" | "text" | "image"
+/** 粘贴|上传内容-图片 */
+export interface TextareaForImage {
+    base64: string
+    width: number
+    height: number
+}
+
+/** ---------- 元素操作方法相关定义 Start ---------- */
+export type SetImageElementFunc = (dom: HTMLDivElement | null, wrapper: HTMLDivElement | null) => void
+/** ---------- 元素操作方法相关定义 End ---------- */
