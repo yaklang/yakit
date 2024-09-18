d685ef76a2dca0540f0932233e6882255ccc589c
Youngster_yj <907985037@qq.com>
2024-09-18 15:28:19 +0800

    feat: 新增代码扫描页面

--- app/renderer/src/main/src/pages/layout/mainOperatorContent/MainOperatorContent.tsx
+++ app/renderer/src/main/src/pages/layout/mainOperatorContent/MainOperatorContent.tsx
@@ -823,6 +823,7 @@ export const MainOperatorContent: React.FC<MainOperatorContentProps> = React.mem
             if (type === "**config-network") openMenuPage({route: YakitRoute.Beta_ConfigNetwork})
             if (type === "**beta-debug-traffic-analize") openMenuPage({route: YakitRoute.Beta_DebugTrafficAnalize})
             if (type === "**webshell-manager") openMenuPage({route: YakitRoute.Beta_WebShellManager})
+            if (type === "**yak-runner-code-scan") openMenuPage({route: YakitRoute.YakRunner_Code_Scan})
             if (type === "**webshell-opt") addWebShellOpt(data)
 
             if (type === "open-plugin-store") {
