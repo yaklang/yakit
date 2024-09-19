850992804278ba5646493235e78da77fdaa29194
Youngster_程 <55881562+youngster-yj@users.noreply.github.com>
2024-09-19 17:17:27 +0800

    fix: 解除部分屏蔽项 (#2001)

--- app/renderer/src/main/src/routes/deprecatedMenu.ts
+++ app/renderer/src/main/src/routes/deprecatedMenu.ts
@@ -6,24 +6,12 @@ export const CommunityDeprecatedFirstMenu: string[] = []
 
 /**
  * @description 社区版
- * 废弃的二级菜单
+ * 废弃的二级菜单(桔子要求删除部分)
  * */
 export const CommunityDeprecatedSecondMenu: string[] = [
     "api提取",
     "空间引擎集成版本",
-    "域名、IP提取",
-    "域名批量转IP并查CDN",
-    "IP反查域名",
-    "批量备案查询",
-    "空间引擎集成版本",
-    "网站信息获取",
-    "主域名提取",
-    "杀软匹配tasklist /svc",
     "网站树",
-    "crt子域名收集",
-    "纯暴力子域名收集",
-    "SEO综合查询",
-    "被动子域名收集",
     "插件商店",
     "我的插件",
     "本地插件"
