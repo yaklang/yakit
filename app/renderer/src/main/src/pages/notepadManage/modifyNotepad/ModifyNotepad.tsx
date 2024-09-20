8d5fc2b514d48b934c0ac441f8d79caad2aecf3d
luoluo <1425735414@qq.com>
2024-09-20 14:09:28 +0800

    feat:md编辑器基本功能框架

--- app/renderer/src/main/src/pages/notepadManage/modifyNotepad/ModifyNotepad.tsx
+++ app/renderer/src/main/src/pages/notepadManage/modifyNotepad/ModifyNotepad.tsx
@@ -1,32 +1,22 @@
 import React from "react"
-import {NotepadManageProps} from "./ModifyNotepadType"
-// import {Editor, rootCtx} from "@milkdown/kit/core"
-// import {nord} from "@milkdown/theme-nord"
-// import {Milkdown, MilkdownProvider, useEditor} from "@milkdown/react"
-// import {commonmark} from "@milkdown/kit/preset/commonmark"
-// import styles from "./ModifyNotepad.module.scss"
+import {MilkdownEditor} from "./MilkdownEditor/MilkdownEditor"
+import {MilkdownProvider} from "@milkdown/react"
+import {ProsemirrorAdapterProvider} from "@prosemirror-adapter/react"
 
+import "@milkdown/theme-nord/style.css"
+
+interface NotepadManageProps {
+    pageId: string
+}
 const ModifyNotepad: React.FC<NotepadManageProps> = React.memo((props) => {
+    const onChange = () => {}
     return (
-        <>
-            {/* <MilkdownProvider>
+        <MilkdownProvider>
+            <ProsemirrorAdapterProvider>
                 <MilkdownEditor />
-            </MilkdownProvider> */}
-        </>
+            </ProsemirrorAdapterProvider>
+        </MilkdownProvider>
     )
 })
 
 export default ModifyNotepad
-
-// const MilkdownEditor: React.FC = () => {
-//     const {get} = useEditor((root) =>
-//         Editor.make()
-//             .config(nord)
-//             .config((ctx) => {
-//                 ctx.set(rootCtx, root)
-//             })
-//             .use(commonmark)
-//     )
-
-//     return <Milkdown />
-// }
