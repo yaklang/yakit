415dc136ab6304767494d950eb155ff00cf58e10
luoluo <1425735414@qq.com>
2024-09-25 17:48:22 +0800

    feat:记事本编辑页面+目录

--- app/renderer/src/main/src/components/MilkdownEditor/MilkdownEditorType.d.ts
+++ app/renderer/src/main/src/components/MilkdownEditor/MilkdownEditorType.d.ts
@@ -1,2 +1,8 @@
-export interface CustomMilkdownProps {}
+import {Editor} from "@milkdown/kit/core"
+
+export interface EditorMilkdownProps extends Editor{} 
+export interface CustomMilkdownProps {
+    editor?: MilkdownEditor
+    setEditor?: MilkdownEditor
+}
 export interface MilkdownEditorProps extends CustomMilkdownProps {}
