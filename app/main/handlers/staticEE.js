397bee025673bca62fc8501d3e5c22fb66d45d99
Youngster_yj <907985037@qq.com>
2024-09-27 14:04:53 +0800

    feat: 动态配置图片打包补充

--- app/main/handlers/staticEE.js
+++ app/main/handlers/staticEE.js
@@ -1,21 +1,22 @@
 const {ipcMain} = require("electron")
 const fs = require("fs")
 const customPath = require("path")
+const {loadExtraFilePath} = require("../filePath")
 module.exports = {
     register: (win, getClient) => {
         const sorcePath = {
-            loginImg: "../../../bins/staticEE/yakit.jpg",
-            miniProject:"../../../bins/staticEE/yakitFontEE.jpg",
-            project:"../../../bins/staticEE/yakitEE.jpg",
+            loginImg: "staticEE/yakit.jpg",
+            miniProject: "staticEE/yakitFontEE.jpg",
+            project: "staticEE/yakitEE.jpg"
         }
 
         const asyncGetStaticImgEEByType = (data) => {
             return new Promise((resolve, reject) => {
                 try {
                     const {type} = data
-                    const path = customPath.join(__dirname, sorcePath[type])
+                    let path = loadExtraFilePath(customPath.join("bins", sorcePath[type]))
                     const img = fs.readFileSync(path)
-                    const base64 = `data:image/jpeg;base64,${img.toString('base64')}`
+                    const base64 = `data:image/jpeg;base64,${img.toString("base64")}`
                     resolve(base64)
                 } catch (error) {
                     reject(error)
