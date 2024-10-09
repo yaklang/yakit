63174a7fa3b6dbf1b96de775973f8c55496b5d44
luoluo <1425735414@qq.com>
2024-10-09 17:12:30 +0800

    doc:增加解决冲突丢失的属性；增加解决冲突丢失的break

--- app/renderer/src/main/src/routes/newRoute.tsx
+++ app/renderer/src/main/src/routes/newRoute.tsx
@@ -111,6 +111,7 @@ import {
     BrutePageInfoProps,
     CodeScanPageInfoProps,
     HTTPHackerPageInfoProps,
+    ModifyNotepadPageInfoProps,
     PluginBatchExecutorPageInfoProps,
     PocPageInfoProps,
     RiskPageInfoProps,
@@ -386,6 +387,8 @@ export interface ComponentParams {
     auditCodePageInfo?: AuditCodePageInfoProps
     /**代码扫描页面 */
     codeScanPageInfo?: CodeScanPageInfoProps
+    /**记事本编辑页面 */
+    modifyNotepadPageInfo?: ModifyNotepadPageInfoProps
 }
 
 function withRouteToPage(WrappedComponent) {
