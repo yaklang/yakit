499159df298c99d1091388fa8df4b9c30d762b58
nonight <378099757@qq.com>
2024-10-15 00:02:04 +0800

    feat: 1、http请求方法(httpApi)，新增参数cancelInterrupt，用于取消上一次请求的主动关闭操作 2、新增360旋转动画样式类，放入公共类文件中 3、插件编辑页面-同步和提交按钮逻辑整合到弹框内，新增补充资料步骤 4、调整PluginImageTextarea组件 5、本地列表上传、插件详情上传功能新增补充资料，并调整上传后的更新逻辑 6、本地插件批量上传接口新增字段(补充资料)并调整对应参数处理逻辑

--- app/renderer/src/main/src/pages/pluginEditor/pluginImageTextarea/PluginImageTextareaType.d.ts
+++ app/renderer/src/main/src/pages/pluginEditor/pluginImageTextarea/PluginImageTextareaType.d.ts
@@ -1,25 +1,27 @@
 import {ForwardedRef} from "react"
 
+/** 上传图片基础信息 */
+export interface TextareaForImage {
+    url: string
+    width: number
+    height: number
+}
+
+export interface ImageTextareaData {
+    value: string
+    imgs: TextareaForImage[]
+}
+
 export interface PluginImageTextareaProps {
     ref?: ForwardedRef<PluginImageTextareaRefProps>
     /** 使用场景: 评论|补充资料 */
     type?: "comment" | "supplement"
-    onSubmit: (data: {value: string; imgs: TextareaForImage[]}) => any
+    onSubmit?: (data: ImageTextareaData) => any
 }
 
 export interface PluginImageTextareaRefProps {
     /** 设置引用内容 */
     setQuotationInfo?: (info: {name: string; content: string}) => void
     /** 获取编辑框所有内容 */
-    getData: () => {
-        value: string
-        imgs: TextareaForImage[]
-    }
-}
-
-/** 粘贴|上传内容-图片 */
-export interface TextareaForImage {
-    url: string
-    width: number
-    height: number
+    getData: () => ImageTextareaData | null
 }
