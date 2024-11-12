4b9ac50620dca32301bbb59a365f054ea9de93a4
luoluo <1425735414@qq.com>
2024-11-12 12:04:18 +0800

    feat:支持选中list元素转为paragraph;doc:修改一些变量存放位置

--- app/renderer/src/main/src/components/MilkdownEditor/Block/Block.tsx
+++ app/renderer/src/main/src/components/MilkdownEditor/Block/Block.tsx
@@ -1,6 +1,6 @@
 import {BlockProvider} from "@milkdown/kit/plugin/block"
 import {useInstance} from "@milkdown/react"
-import {useCallback, useEffect, useRef, useState} from "react"
+import {ReactNode, useCallback, useEffect, useRef, useState} from "react"
 import {usePluginViewContext} from "@prosemirror-adapter/react"
 import {
     blockquoteSchema,
@@ -15,19 +15,9 @@ import {
 import {TextSelection} from "@milkdown/kit/prose/state"
 import styles from "./Block.module.scss"
 import {YakitButton} from "@/components/yakitUI/YakitButton/YakitButton"
-import {OutlineLightbulbIcon, OutlinePaperclipIcon, OutlinePlusIcon} from "@/assets/icon/outline"
+import {OutlinePlusIcon} from "@/assets/icon/outline"
 import {YakitPopover} from "@/components/yakitUI/YakitPopover/YakitPopover"
-import {
-    IconHeading1,
-    IconHeading2,
-    IconHeading3,
-    IconListOrdered,
-    IconList,
-    IconCheckSquare,
-    IconCurlyBraces,
-    IconQuote,
-    IconFlipVertical
-} from "../icon/icon"
+
 import {useMemoizedFn} from "ahooks"
 import {Ctx} from "@milkdown/kit/ctx"
 import {alterCustomSchema} from "../utils/alertPlugin"
@@ -36,85 +26,20 @@ import {fileCommand} from "../utils/uploadPlugin"
 import {callCommand} from "@milkdown/kit/utils"
 import {insertImageBlockCommand} from "../utils/imageBlock"
 import {Tooltip} from "antd"
+import {defaultBlockList} from "../constants"
+import {cloneDeep} from "lodash"
+import {BlockListProps} from "../MilkdownEditorType"
 
 const {ipcRenderer} = window.require("electron")
 
 const imgTypes = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".tif", ".webp", ".svg"]
-
-const addList = [
-    {
-        id: 1,
-        icon: <IconHeading1 />,
-        label: "一级标题",
-        description: "一级标题: #空格"
-    },
-    {
-        id: 2,
-        icon: <IconHeading2 />,
-        label: "二级标题",
-        description: "二级标题: ##空格"
-    },
-    {
-        id: 3,
-        icon: <IconHeading3 />,
-        label: "三级标题",
-        description: "二级标题: ###空格"
-    },
-    {
-        id: 4,
-        icon: <IconListOrdered />,
-        label: "有序列表",
-        description: "有序列表: 1.空格"
-    },
-    {
-        id: 5,
-        icon: <IconList />,
-        label: "无序列表",
-        description: "无序列表: -空格或*空格"
-    },
-    {
-        id: 6,
-        icon: <IconCheckSquare />,
-        label: "任务",
-        description: "任务"
-    },
-    {
-        id: 7,
-        icon: <IconCurlyBraces />,
-        label: "代码块",
-        description: "代码块:```空格"
-    },
-    {
-        id: 8,
-        icon: <IconQuote />,
-        label: "引用",
-        description: "引用: >空格"
-    },
-    {
-        id: 9,
-        icon: <OutlineLightbulbIcon />,
-        label: "高亮",
-        description: "高亮: :::success空格"
-    },
-    {
-        id: 10,
-        icon: <OutlinePaperclipIcon />,
-        label: "上传文件",
-        description: "上传文件"
-    },
-    {
-        id: 11,
-        icon: <IconFlipVertical />,
-        label: "分割线",
-        description: "引用: ***"
-    }
-]
 
 export const BlockView = (block) => {
     const ref = useRef<HTMLDivElement>(null)
     const tooltipProvider = useRef<BlockProvider>()
 
     const [visibleAdd, setVisibleAdd] = useState(false)
+    const [blockList, setBlockList] = useState<BlockListProps[]>(cloneDeep(defaultBlockList))
 
     const {view, prevState} = usePluginViewContext()
 
@@ -281,7 +206,7 @@ export const BlockView = (block) => {
                 placement='bottomLeft'
                 content={
                     <div className={styles["tooltip-popover-content"]}>
-                        {addList.map((ele) => (
+                        {blockList.map((ele) => (
                             <Tooltip key={ele.id} title={ele.description}>
                                 <YakitButton
                                     key={ele.id}
