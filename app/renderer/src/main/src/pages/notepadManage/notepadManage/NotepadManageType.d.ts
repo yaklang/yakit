e07a0b9339ac894b1456dfa5659153a0bfa93451
luoluo <1425735414@qq.com>
2024-11-19 17:12:26 +0800

    perf:关于文件的一些代码优化

--- app/renderer/src/main/src/pages/notepadManage/notepadManage/NotepadManageType.d.ts
+++ app/renderer/src/main/src/pages/notepadManage/notepadManage/NotepadManageType.d.ts
@@ -1,3 +1,12 @@
-export interface NotepadManageProps{
-    
+import {YakitHintProps} from "@/components/yakitUI/YakitHint/YakitHintType"
+import {ReactNode} from "react"
+
+export interface NotepadManageProps {}
+
+export interface DownNotepadModalProps {
+    visible: boolean
+    setVisible: (b: boolean) => void
+    url: string
+    path: string
+    onCancelDownload: () => void
 }
