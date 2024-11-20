4cba2fbd454999ee0fb161bcf9bda9ce3e7cf0ce
luoluo <1425735414@qq.com>
2024-11-20 14:33:59 +0800

    feat:记事本自定义文件组件完

--- app/main/api/uploadOSS.js
+++ app/main/api/uploadOSS.js
@@ -1,5 +1,5 @@
 /**
- * TODO: OOS上传,后期需要整合
+ * TODO: OSS上传,后期需要整合
  * LINK - ./upload.js#split-upload
  */
 const { service, } = require("../httpServer")
@@ -12,15 +12,15 @@ const axios = require("axios")
 const PATH = require("path")
 
 module.exports = (win, getClient) => {
-    const streamOOSplitSUpload = new Map()
+    const streamOSSplitSUpload = new Map()
     // 上传次数缓存
-    let oosPostPackageHistory = {}
-    // 分片上传 oos版本
-    ipcMain.handle("oos-split-upload", (event, params) => {
+    let ossPostPackageHistory = {}
+    // 分片上传 oss版本
+    ipcMain.handle("oss-split-upload", (event, params) => {
         return new Promise(async (resolve, reject) => {
             try {
                 // path为文件路径 token为切片进度回调 url为接口
-                const { url, path, token } = params
+                const { url, path, token, } = params
                 // 获取文件名
                 const fileName = customPath.basename(path)
                 // 文件大小（以字节为单位）
@@ -32,7 +32,7 @@ module.exports = (win, getClient) => {
                     return
                 }
                 // 每块分片大小
-                const chunkSize = 10 * 1024 * 1024 // 每个分片的大小，这里设置为 10 MB
+                const chunkSize = 20 * 1024 * 1024 // 每个分片的大小，这里设置为 20 MB
                 // 计算分片总数
                 const totalChunks = Math.ceil(size / chunkSize)
                 // 计算整个文件Hash
@@ -40,7 +40,7 @@ module.exports = (win, getClient) => {
                 const fileHashTime = `${fileHash}-${Date.now()}`
                 // 创建一个新的CancelToken
                 const cancelTokenSource = axios.CancelToken.source()
-                streamOOSplitSUpload.set(token, cancelTokenSource)
+                streamOSSplitSUpload.set(token, cancelTokenSource)
                 for (let chunkIndex = 0; chunkIndex < totalChunks; chunkIndex++) {
                     try {
                         const hash = await hashChunk({ path, size, chunkSize, chunkIndex })
@@ -49,7 +49,7 @@ module.exports = (win, getClient) => {
                         const end = Math.min((chunkIndex + 1) * chunkSize, size)
                         // 创建当前分片的读取流
                         const chunkStream = fs.createReadStream(path, { start, end })
-                        await oosUploadBigFile({
+                        await ossUploadBigFile({
                             url,
                             chunkStream,
                             chunkIndex,
@@ -61,34 +61,38 @@ module.exports = (win, getClient) => {
                             cancelToken: cancelTokenSource.token
                         })
                     } catch (error) {
-                        win.webContents.send(`oos-split-upload-${token}-error`, error)
+                        win.webContents.send(`oss-split-upload-${token}-error`, error)
                         reject(error)
+                        break
                     }
                 }
-                win.webContents.send(`oos-split-upload-${token}-end`)
-                oosPostPackageHistory = {}
+                win.webContents.send(`oss-split-upload-${token}-end`)
+                ossPostPackageHistory = {}
+                cancelTokenSource && cancelTokenSource.cancel()
+                streamOSSplitSUpload.delete(token)
                 resolve()
             } catch (error) {
                 reject(error)
             }
         })
     })
-    // 取消分片上传 oos版本
-    ipcMain.handle("cancel-oos-split-upload", (event, token) => {
+    // 取消分片上传 oss版本
+    ipcMain.handle("cancel-oss-split-upload", (event, token) => {
         return new Promise(async (resolve, reject) => {
             try {
-                const stream = streamOOSplitSUpload.get(token)
+                const stream = streamOSSplitSUpload.get(token)
                 stream && stream.cancel()
+                streamOSSplitSUpload.delete(token)
                 resolve()
             } catch (error) {
                 reject(error)
             }
         })
     })
-    // 上传到oos
-    const oosUploadBigFile = ({ url, chunkStream, chunkIndex, totalChunks, fileName, hash, fileHash, token, cancelToken }) => {
+    // 上传到oss
+    const ossUploadBigFile = ({ url, chunkStream, chunkIndex, totalChunks, fileName, hash, fileHash, token, cancelToken }) => {
         return new Promise((resolve, reject) => {
-            oosPostPackageHistory[hash] ? (oosPostPackageHistory[hash] += 1) : (oosPostPackageHistory[hash] = 1)
+            ossPostPackageHistory[hash] ? (ossPostPackageHistory[hash] += 1) : (ossPostPackageHistory[hash] = 1)
             const percent = (chunkIndex + 1) / totalChunks
             const formData = new FormData()
             formData.append("file", chunkStream)
@@ -107,15 +111,15 @@ module.exports = (win, getClient) => {
                 .then(async (res) => {
                     if (res.code === 200) {
                         const progress = Math.floor(percent * 100)
-                        win.webContents.send(`oos-split-upload-${token}-data`, {
+                        win.webContents.send(`oss-split-upload-${token}-data`, {
                             res,
                             progress
                         })
                     }
-                    if (res.code !== 200 && oosPostPackageHistory[hash] <= 3) {
+                    if (res.code !== 200 && ossPostPackageHistory[hash] <= 3) {
                         // 传输失败 重传3次
-                        await oosUploadBigFile({ url, chunkStream, chunkIndex, totalChunks, fileName, hash, fileHash, token })
-                    } else if (oosPostPackageHistory[hash] > 3) {
+                        await ossUploadBigFile({ url, chunkStream, chunkIndex, totalChunks, fileName, hash, fileHash, token })
+                    } else if (ossPostPackageHistory[hash] > 3) {
                         reject(res.data.reason)
                     }
                     resolve()
@@ -139,6 +143,7 @@ module.exports = (win, getClient) => {
             } catch (error) {
                 reject(error)
             }
+
         })
     })
 
