e1201a420c9627350cff717d07756eaf1ff169c1
song_xiao_lin <song_xiao_lin@126.com>
2024-12-04 17:29:40 +0800

    feat: 1、图标替换&删除部分Yak&Yakit文字 2、屏蔽更新通知小铃铛 3、项目名居中 4、ci企业版Enterprise->CSPG 5、屏蔽License验证 6、屏蔽用户协议 7、屏蔽部分更新提示

--- app/renderer/src/main/src/components/layout/update/InstallEngine.tsx
+++ app/renderer/src/main/src/components/layout/update/InstallEngine.tsx
@@ -716,7 +716,7 @@ export const QuestionModal: React.FC<AgrAndQSModalProps> = React.memo((props) =>
                                         </div>
                                     )}
                                 </div>
-                                <span>Yak 核心引擎下载链接</span>
+                                <span>核心引擎下载链接</span>
                             </div>
                         ) : (
                             <div
@@ -727,7 +727,7 @@ export const QuestionModal: React.FC<AgrAndQSModalProps> = React.memo((props) =>
                                 onMouseOut={() => setDisabled(true)}
                                 onMouseDown={() => setIsTop(2)}
                             >
-                                <span className={styles["header-title"]}>Yak 核心引擎下载链接</span>
+                                <span className={styles["header-title"]}>核心引擎下载链接</span>
                                 <div className={styles["close-wrapper"]} onClick={() => setVisible(false)}>
                                     <WinUIOpCloseSvgIcon className={styles["icon-style"]} />
                                 </div>
