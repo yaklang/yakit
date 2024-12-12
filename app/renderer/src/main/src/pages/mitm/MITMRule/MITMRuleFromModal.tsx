a87a94312379aab63e5f95edadace65709ebeb49
song_xiao_lin <song_xiao_lin@126.com>
2024-12-12 12:07:06 +0800

    feat: 加描述

--- app/renderer/src/main/src/pages/mitm/MITMRule/MITMRuleFromModal.tsx
+++ app/renderer/src/main/src/pages/mitm/MITMRule/MITMRuleFromModal.tsx
@@ -211,7 +211,7 @@ export const MITMRuleFromModal: React.FC<MITMRuleFromModalProps> = (props) => {
                             <Divider dashed style={{marginTop: 0}} />
                         </Col>
                     </Row>
-                    <Form.Item label='生效url' name='EffectiveURL'>
+                    <Form.Item label='生效url' name='EffectiveURL' help='配置后规则只对该url生效，支持填写正则'>
                         <YakitInput />
                     </Form.Item>
                     <Form.Item label='命中颜色' name='Color'>
