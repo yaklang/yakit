a6d5ff7e8816df8d26145e4544297f145230750e
song_xiao_lin <song_xiao_lin@126.com>
2024-12-18 17:38:45 +0800

    feat: history自定义列

--- app/renderer/src/main/src/enums/history.ts
+++ app/renderer/src/main/src/enums/history.ts
@@ -1,4 +1,6 @@
 export enum RemoteHistoryGV {
     /** @name 是否已了解chat-cs功能 */
-    BackgroundRefresh = "background-refresh"
+    BackgroundRefresh = "background-refresh",
+    /** @name 排除展示列key值 */
+    HistroyExcludeColumnsKey = "history-exclude-columns-key"
 }
