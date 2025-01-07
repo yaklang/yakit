8d1fb61c9c0022e8333289abfd3d33f8e8e66f48
luoluo <1425735414@qq.com>
2025-01-07 16:07:45 +0800

    fix:修复选中部分标题转为正文后，会有遗留的未选中的标题数据

--- app/renderer/src/main/src/components/MilkdownEditor/utils/headingPlugin.ts
+++ app/renderer/src/main/src/components/MilkdownEditor/utils/headingPlugin.ts
@@ -20,19 +20,17 @@ export const headingToParagraphCommand = $command(`headingToParagraphCommand`, (
         return false // 没有选中任何内容
     }
     const parentNode = $from.node($from.depth)
-    if (parentNode.type.name === state.schema.nodes.heading.name) {
+    if (parentNode && parentNode.type.name === state.schema.nodes.heading.name) {
         let textNode = state.schema.text(parentNode.textContent)
-        // 创建新的节点，并用它替换列表项
         const node = state.schema.nodes.paragraph.create(
             null,
             textNode // 使用列表项的内容填充
         )
-
+        const start = $from.start() // 标题节点的起始位置
+        const end = $from.end() // 标题节点的结束位置
         // 替换整个标题节点
-        const tr = state.tr.replaceRangeWith(from, to, node)
-
-        if (dispatch) dispatch(tr)
-
+        const transaction = state.tr.replaceRangeWith(start, end, node)
+        if (dispatch) dispatch(transaction)
         return true
     }
     return false
