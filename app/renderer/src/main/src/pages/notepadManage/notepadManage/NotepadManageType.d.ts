e44dccb128c20307912eae59c269dc19fcfea9c4
luoluo <1425735414@qq.com>
2025-01-10 15:57:18 +0800

    delete-doc:删除没有使用的定义

--- app/renderer/src/main/src/pages/notepadManage/notepadManage/NotepadManageType.d.ts
+++ app/renderer/src/main/src/pages/notepadManage/notepadManage/NotepadManageType.d.ts
@@ -2,11 +2,3 @@ import {YakitHintProps} from "@/components/yakitUI/YakitHint/YakitHintType"
 import {ReactNode} from "react"
 
 export interface NotepadManageProps {}
-
-export interface DownNotepadModalProps {
-    visible: boolean
-    setVisible: (b: boolean) => void
-    url: string
-    path: string
-    onCancelDownload: () => void
-}
