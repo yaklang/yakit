d75cae876d242eba51f9ac397b2ad82cc10ee027
song_xiao_lin <song_xiao_lin@126.com>
2025-01-21 15:26:08 +0800

    feat: 项目管理页面名字图标替换

--- app/renderer/src/main/src/utils/envfile.tsx
+++ app/renderer/src/main/src/utils/envfile.tsx
@@ -19,7 +19,7 @@ export enum PRODUCT_RELEASE_EDITION {
 export const getReleaseEditionName = () => {
     switch (GetReleaseEdition()) {
         case PRODUCT_RELEASE_EDITION.EnpriTrace:
-            return "EnpriTrace"
+            return "攻防武器平台"
         case PRODUCT_RELEASE_EDITION.EnpriTraceAgent:
             return "EnpriTraceAgent"
         case PRODUCT_RELEASE_EDITION.BreachTrace:
