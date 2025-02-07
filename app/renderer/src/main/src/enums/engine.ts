1c19476cc85aa6e50eb353a38ef0d6fcc53f0f02
nonight <378099757@qq.com>
2025-02-07 20:56:35 +0800

    移除主界面内的内置引擎版本检测弹框

--- app/renderer/src/main/src/enums/engine.ts
+++ app/renderer/src/main/src/enums/engine.ts
@@ -2,6 +2,7 @@ export enum EngineRemoteGV {
     /**
      * @name 是否启动引擎版本检查
      * 引擎插件：判断当前版本是否比软件适配版本高，如果低于适配版本则弹出引擎版本检查弹窗
+     * @deprecated 该功能已废弃, 后续别的地方使用请注意是否有老数据影响，下个版本删除
      */
     RemoteCheckEngineVersion = "remote-check-engine-version"
 }
