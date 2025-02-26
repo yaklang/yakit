89351e3aefb9b2dedd78b0d986742b4c3ddffc54
luoluo <1425735414@qq.com>
2025-02-26 14:07:23 +0800

    feat:新增CustomAlter组件，在组件基础上增加历史对比

---
+++ app/renderer/src/main/src/components/MilkdownEditor/CustomAlter/CustomAlter.tsx
@@ -0,0 +1,28 @@
+import {useNodeViewContext} from "@prosemirror-adapter/react"
+import {useCreation} from "ahooks"
+import classNames from "classnames"
+import styles from "./CustomAlter.module.scss"
+import {YChangeProps} from "../YChange/YChangeType"
+import {YChange} from "../YChange/YChange"
+
+export const CustomAlter: React.FC = () => {
+    const {node, contentRef} = useNodeViewContext()
+    const {attrs} = node
+
+    const ychange: YChangeProps = useCreation(() => attrs.ychange, [attrs])
+
+    return (
+        <div
+            className={classNames(styles["alter-custom-block"], {
+                [styles["alter-custom-diff-history-block"]]: ychange
+            })}
+            style={{color: ychange ? ychange.color?.dark : ""}}
+        >
+            <div
+                className={classNames(styles["alter-custom"], styles[`alter-custom-${attrs.type}`])}
+                ref={contentRef}
+            />
+            <YChange {...ychange} />
+        </div>
+    )
+}
