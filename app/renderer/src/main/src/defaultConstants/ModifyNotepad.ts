e53debaadab8135fb684248f2fe92e2c3679423f
luoluo <1425735414@qq.com>
2025-03-10 16:23:14 +0800

    feat:增加本地笔记本列表

--- app/renderer/src/main/src/defaultConstants/ModifyNotepad.ts
+++ app/renderer/src/main/src/defaultConstants/ModifyNotepad.ts
@@ -1,6 +1,13 @@
 import {ModifyNotepadPageInfoProps} from "@/store/pageInfo"
+import {NoteFilter} from "@/pages/notepadManage/notepadManage/utils"
 
 export const defaultModifyNotepadPageInfo: ModifyNotepadPageInfoProps = {
     notepadHash: "",
     title: ""
+}
+
+export const defaultNoteFilter: NoteFilter = {
+    Id: [],
+    Title: [],
+    ContentKeyword: []
 }
