60286cf928accf0c7b914cca3f835cdbce5e1c36
Youngster_yj <907985037@qq.com>
2025-03-13 11:55:56 +0800

    feat: OSS验证
    feat: set

--- packageScript/script/buildPreScript.sh
+++ packageScript/script/buildPreScript.sh
@@ -13,10 +13,20 @@ case "$1" in
     sast)
         chmod +x ./packageScript/script/installSastRender.sh
         chmod +x ./packageScript/script/electron-sast-builder.sh
+        # 获取渲染端最新版本号
+        renderVersion=$(curl -fsL "http://yaklang.oss-accelerate.aliyuncs.com/sast/render/version.txt") || {
+            echo "Failed to obtain render version" >&2
+            exit 1
+        }
         ;;
     yakit)
         chmod +x ./packageScript/script/installRender.sh
         chmod +x ./packageScript/script/electron-builder.sh
+        # 获取渲染端最新版本号
+        renderVersion=$(curl -fsL "http://yaklang.oss-accelerate.aliyuncs.com/yak/render/version.txt") || {
+            echo "Failed to obtain render version" >&2
+            exit 1
+        }
         ;;
     *)
         echo "Invalid option: $1" >&2
@@ -24,10 +34,6 @@ case "$1" in
         ;;
 esac
 
-# 获取渲染端最新版本号
-renderVersion=$(curl -fsL "http://yaklang.oss-accelerate.aliyuncs.com/yak/render/version.txt") || {
-    echo "Failed to obtain render version" >&2
-    exit 1
-}
+
 # 设置渲染端最新版本号, 用于后续下载
 echo "RENDER_VERSION=${renderVersion}" >>$GITHUB_ENV
