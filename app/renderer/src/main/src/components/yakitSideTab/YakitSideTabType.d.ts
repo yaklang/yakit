752efc3b0f3984025178ff78d878f9ca93567dfb
luoluo <1425735414@qq.com>
2025-03-13 17:25:00 +0800

    feat:新增YakitSideTab组件，样式按插件仓库中的tab

---
+++ app/renderer/src/main/src/components/yakitSideTab/YakitSideTabType.d.ts
@@ -0,0 +1,17 @@
+import {ReactNode} from "react"
+export interface YakitSideTabProps {
+    yakitTabs: YakitTabsProps[]
+    /**TODO 点击展示的tab状态 缓存的key */
+    cacheKey?: string
+    activeKey: string
+    onActiveKey: (s: string) => void
+
+    setShow?: (s: boolean) => void
+    show?: boolean
+}
+
+export interface YakitTabsProps {
+    icon: ReactNode
+    label: ReactNode
+    value: string
+}
