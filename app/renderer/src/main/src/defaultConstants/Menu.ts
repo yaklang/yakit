5cd6896a9feb821695502cc3ef48b99bc9b7d005
song_xiao_lin <song_xiao_lin@126.com>
2025-03-31 16:47:50 +0800

    feat: web中间件漏洞突破工具菜单固定

---
+++ app/renderer/src/main/src/defaultConstants/Menu.ts
@@ -0,0 +1,9 @@
+export const defMenuDataString = [
+    {
+        label: "网络资产信息探测工具",
+        menuName: "网络资产信息探测工具",
+        pluginId: 0,
+        pluginName: "",
+        children: [{route: "poc", label: "专项漏洞检测", menuName: "专项漏洞检测", pluginId: 0, pluginName: ""}]
+    }
+]
