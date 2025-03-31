f4cda3e165d346404d970d183db84533be7e5271
song_xiao_lin <song_xiao_lin@126.com>
2025-03-31 16:38:04 +0800

    feat: 网络资产信息探测工具菜单固定

---
+++ app/renderer/src/main/src/defaultConstants/Menu.ts
@@ -0,0 +1,12 @@
+export const defMenuDataString = [
+    {
+        label: "网络资产信息探测工具",
+        menuName: "网络资产信息探测工具",
+        pluginId: 0,
+        pluginName: "",
+        children: [
+            {route: "scan-port", label: "端口/指纹扫描", menuName: "端口/指纹扫描", pluginId: 0, pluginName: ""},
+            {route: "space-engine", label: "空间引擎", menuName: "空间引擎", pluginId: 0, pluginName: ""}
+        ]
+    }
+]
