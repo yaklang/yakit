731f45c1740463df468690b4ac9de2f02f2f3dba
song_xiao_lin <song_xiao_lin@126.com>
2025-03-31 17:34:12 +0800

    feat: 网站管理菜单固定

---
+++ app/renderer/src/main/src/defaultConstants/Menu.ts
@@ -0,0 +1,18 @@
+export const defMenuDataString = [
+  {
+    "label": "Webshell远程隐蔽控制工具",
+    "menuName": "Webshell远程隐蔽控制工具",
+    "pluginId": 0,
+    "pluginName": "",
+    "children": [
+      {
+        "route": "beta-webshell-manager",
+        "label": "网站管理",
+        "menuName": "网站管理",
+        "pluginId": 0,
+        "pluginName": ""
+      }
+    ]
+  }
+]
+
