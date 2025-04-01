8be1ea9ee3b281c3dd7497e32df4c4ea79edd63f
song_xiao_lin <song_xiao_lin@126.com>
2025-04-01 10:43:25 +0800

    feat: 名字改为web中间件漏洞突破工具

--- packageScript/electron-builder.config.js
+++ packageScript/electron-builder.config.js
@@ -13,9 +13,9 @@ const platform = process.env.PLATFORM
 switch (platform) {
     case "ee":
         appInfoOption = {
-            appId: "io.yaklang.enpritrace",
-            extraMetadata: {name: "enpritrace"},
-            productName: "EnpriTrace",
+            appId: "io.yaklang.web中间件漏洞突破工具",
+            extraMetadata: {name: "web中间件漏洞突破工具"},
+            productName: "web中间件漏洞突破工具",
             copyright: "Copyright © 2021 v1ll4n"
         }
         macIcon = "app/assets/yakiteelogo.icns"
