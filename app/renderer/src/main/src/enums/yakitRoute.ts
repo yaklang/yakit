79496c55cdb645ab918e1099885f04bf0663f65c
song_xiao_lin <song_xiao_lin@126.com>
2025-05-09 11:21:15 +0800

    zancun

--- app/renderer/src/main/src/enums/yakitRoute.ts
+++ app/renderer/src/main/src/enums/yakitRoute.ts
@@ -98,5 +98,7 @@ export enum YakitRoute {
     System_Config = "system-config",
 
     // 快捷键设置
-    ShortcutKey = "shortcut-key"
+    ShortcutKey = "shortcut-key",
+    // 指纹库
+    FingerprintManage = "fingerprint-manage"
 }
