3df77a55e476afe5be8858581bf5c88a01c451d6
song_xiao_lin <song_xiao_lin@126.com>
2025-05-12 10:46:36 +0800

    feat: 指纹管理部分接口对接

--- app/renderer/src/main/src/pages/fingerprintManage/api.ts
+++ app/renderer/src/main/src/pages/fingerprintManage/api.ts
@@ -1,38 +1,54 @@
-import {APIFunc} from "@/apiUtils/type"
+import {APIFunc, APINoRequestFunc} from "@/apiUtils/type"
 import {yakitNotify} from "@/utils/notification"
+import {DbOperateMessage} from "../layout/mainOperatorContent/utils"
+import {Paging} from "@/utils/yakQueryHTTPFlow"
 const {ipcRenderer} = window.require("electron")
 
-/** @name 获取本地指纹库组列表数据 */
-export const grpcFetchLocalFingerprintGroupList: APIFunc<any, any> = (request, hiddenError) => {
+export interface FingerprintGroup {
+    GroupName: string
+    Count: number
+}
+interface FingerprintGroups {
+    Data: FingerprintGroup[]
+}
+/** @name 获取本地指纹组列表数据 */
+export const grpcFetchLocalFingerprintGroupList: APINoRequestFunc<FingerprintGroups> = () => {
     return new Promise(async (resolve, reject) => {
         ipcRenderer
-            .invoke("QuerySyntaxFlowRuleGroup", request)
+            .invoke("GetAllFingerprintGroup")
             .then(resolve)
             .catch((e) => {
-                if (!hiddenError) yakitNotify("error", "查询本地指纹组失败:" + e)
+                yakitNotify("error", "查询本地指纹组失败：" + e)
                 reject(e)
             })
     })
 }
 
 /** @name 创建本地指纹组 */
-export const grpcCreateLocalFingerprintGroup: APIFunc<any, any> = (request, hiddenError) => {
+export const grpcCreateLocalFingerprintGroup: APIFunc<FingerprintGroup, DbOperateMessage> = (request) => {
     return new Promise(async (resolve, reject) => {
         ipcRenderer
-            .invoke("CreateSyntaxFlowRuleGroup", request)
+            .invoke("CreateFingerprintGroup", request)
             .then(resolve)
             .catch((e) => {
-                if (!hiddenError) yakitNotify("error", "创建本地指纹组失败:" + e)
+                yakitNotify("error", "创建本地指纹组失败：" + e)
                 reject(e)
             })
     })
 }
 
+interface RenameFingerprintGroupRequest {
+    GroupName: string
+    NewGroupName: string
+}
 /** @name 更新本地指纹组 */
-export const grpcUpdateLocalFingerprintGroup: APIFunc<any, any> = (request, hiddenError) => {
+export const grpcUpdateLocalFingerprintGroup: APIFunc<RenameFingerprintGroupRequest, DbOperateMessage> = (
+    request,
+    hiddenError
+) => {
     return new Promise(async (resolve, reject) => {
         ipcRenderer
-            .invoke("UpdateSyntaxFlowRuleGroup", request)
+            .invoke("RenameFingerprintGroup", request)
             .then(resolve)
             .catch((e) => {
                 if (!hiddenError) yakitNotify("error", "更新本地指纹组失败:" + e)
@@ -41,14 +57,152 @@ export const grpcUpdateLocalFingerprintGroup: APIFunc<any, any> = (request, hidd
     })
 }
 
+interface DeleteFingerprintGroupRequest {
+    GroupNames: string[]
+}
 /** @name 删除本地指纹组 */
-export const grpcDeleteLocalFingerprintGroup: APIFunc<any, any> = (request, hiddenError) => {
+export const grpcDeleteLocalFingerprintGroup: APIFunc<DeleteFingerprintGroupRequest, DbOperateMessage> = (
+    request,
+    hiddenError
+) => {
     return new Promise(async (resolve, reject) => {
         ipcRenderer
-            .invoke("DeleteSyntaxFlowRuleGroup", request)
+            .invoke("DeleteFingerprintGroup", request)
             .then(resolve)
             .catch((e) => {
                 if (!hiddenError) yakitNotify("error", "删除本地指纹组失败:" + e)
+                reject(e)
+            })
+    })
+}
+
+export interface FingerprintFilter {
+    Vendor?: string[]
+    Product?: string[]
+    IncludeId?: number[]
+    GroupName?: string[]
+}
+export interface QueryFingerprintRequest {
+    Filter: FingerprintFilter
+    Pagination: Paging
+}
+interface CPE {}
+export interface FingerprintRule {
+    Id: number
+    RuleName: string
+    CPE: CPE
+    WebPath: string
+    ExtInfo: string
+    MatchExpression: string
+    GroupName: string[]
+}
+export interface QueryFingerprintResponse {
+    Data: FingerprintRule[]
+    Pagination: Paging
+    Total: number
+}
+/** @name 获取本地指纹列表数据 */
+export const grpcFetchLocalFingerprintList: APIFunc<QueryFingerprintRequest, QueryFingerprintResponse> = (request) => {
+    return new Promise(async (resolve, reject) => {
+        ipcRenderer
+            .invoke("QueryFingerprint", request)
+            .then(resolve)
+            .catch((e) => {
+                yakitNotify("error", "查询本地指纹列表数据失败：" + e)
+                reject(e)
+            })
+    })
+}
+
+interface DeleteFingerprintRequest {
+    Filter: FingerprintFilter
+    Pagination?: Paging
+}
+/** @name 删除本地指纹列表数据 */
+export const grpcDeleteFingerprint: APIFunc<DeleteFingerprintRequest, DbOperateMessage> = (request) => {
+    return new Promise(async (resolve, reject) => {
+        ipcRenderer
+            .invoke("DeleteFingerprint", request)
+            .then(resolve)
+            .catch((e) => {
+                yakitNotify("error", "删除本地指纹列表数据失败：" + e)
+                reject(e)
+            })
+    })
+}
+
+interface UpdateFingerprintRequest {
+    Id: number
+    Rule: FingerprintRule
+}
+/** @name 更新本地指纹 */
+export const grpcUpdateFingerprint: APIFunc<UpdateFingerprintRequest, DbOperateMessage> = (request) => {
+    return new Promise(async (resolve, reject) => {
+        ipcRenderer
+            .invoke("UpdateFingerprint", request)
+            .then(resolve)
+            .catch((e) => {
+                yakitNotify("error", "更新本地指纹列表数据失败：" + e)
+                reject(e)
+            })
+    })
+}
+
+interface CreateFingerprintRequest {
+    Rule: {
+        RuleName: string
+        MatchExpression: string
+        GroupName: string[]
+    }
+}
+/** @name 创建本地指纹 */
+export const grpcCreateFingerprint: APIFunc<CreateFingerprintRequest, DbOperateMessage> = (request) => {
+    return new Promise(async (resolve, reject) => {
+        ipcRenderer
+            .invoke("CreateFingerprint", request)
+            .then(resolve)
+            .catch((e) => {
+                yakitNotify("error", "创建本地指纹列表数据失败：" + e)
+                reject(e)
+            })
+    })
+}
+
+interface QueryFingerprintSameGroupRequest {
+    Filter: FingerprintFilter
+}
+interface QueryFingerprintSameGroupResponse {
+    Group: FingerprintGroup[]
+}
+/** @name 查询指纹集合的所属组交集 */
+export const grpcFetchFingerprintForSameGroup: APIFunc<
+    QueryFingerprintSameGroupRequest,
+    QueryFingerprintSameGroupResponse
+> = (request) => {
+    return new Promise(async (resolve, reject) => {
+        ipcRenderer
+            .invoke("QuerySyntaxFlowSameGroup", request)
+            .then(resolve)
+            .catch((e) => {
+                yakitNotify("error", "查询指纹所属于组交集失败：" + e)
+                reject(e)
+            })
+    })
+}
+
+export interface UpdateFingerprintAndGroupRequest {
+    Filter: FingerprintFilter
+    AddGroups: string[]
+    RemoveGroups: string[]
+}
+/** @name 更新指纹里的本地组 */
+export const grpcUpdateFingerprintToGroup: APIFunc<UpdateFingerprintAndGroupRequest, unknown> = (request) => {
+    return new Promise(async (resolve, reject) => {
+        ipcRenderer
+            .invoke("UpdateSyntaxFlowRuleAndGroup", request)
+            .then(resolve)
+            .catch((e) => {
+                yakitNotify("error", "更新组失败：" + e)
                 reject(e)
             })
     })
