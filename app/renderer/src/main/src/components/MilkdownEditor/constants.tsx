014d7a6a7523dff78c6d74710ea694921629036a
luoluo <1425735414@qq.com>
2025-05-21 16:50:17 +0800

    perf:规范milkdown菜单

--- app/renderer/src/main/src/components/MilkdownEditor/constants.tsx
+++ app/renderer/src/main/src/components/MilkdownEditor/constants.tsx
@@ -11,230 +11,181 @@ import {
     IconFlipVertical,
     IconType
 } from "./icon/icon"
-import {BlockListProps, TooltipListProps} from "./MilkdownEditorType"
+import {ReactNode} from "react"
 
-export const defaultBlockList: BlockListProps[] = [
-    {
-        id: 1,
+export interface MilkdownBaseUtilProps {
+    key: string
+    icon: ReactNode
+    label: string
+    description: string
+}
+export interface BlockListProps {
+    key: MilkdownMenuType
+    icon: ReactNode
+    label: string
+    description: string
+}
+export type TooltipListProps = BlockListProps | {key: string; label: string}
+
+export const MilkdownMenu = {
+    text: {
+        key: "text",
+        icon: <IconType />,
+        label: "文本",
+        description: "文本"
+    },
+    heading1: {
+        key: "heading1",
         icon: <IconHeading1 />,
         label: "一级标题",
         description: "一级标题: #空格"
     },
-    {
-        id: 2,
+    heading2: {
+        key: "heading2",
         icon: <IconHeading2 />,
         label: "二级标题",
         description: "二级标题: ##空格"
     },
-    {
-        id: 3,
+    heading3: {
+        key: "heading3",
         icon: <IconHeading3 />,
         label: "三级标题",
         description: "三级标题: ###空格"
     },
-    {
-        id: 4,
+    orderedList: {
+        key: "orderedList",
         icon: <IconListOrdered />,
         label: "有序列表",
         description: "有序列表: 1.空格"
     },
-    {
-        id: 5,
+    unorderedList: {
+        key: "unorderedList",
         icon: <IconList />,
         label: "无序列表",
         description: "无序列表: -空格或*空格"
     },
-    {
-        id: 6,
+    task: {
+        key: "task",
         icon: <IconCheckSquare />,
         label: "任务",
         description: "任务"
     },
-    {
-        id: 7,
+    codeBlock: {
+        key: "codeBlock",
         icon: <IconCurlyBraces />,
         label: "代码块",
         description: "代码块:```空格"
     },
-    {
-        id: 8,
+    quote: {
+        key: "quote",
         icon: <IconQuote />,
         label: "引用",
         description: "引用: >空格"
     },
-    {
-        id: 9,
+    highLight: {
+        key: "highLight",
         icon: <OutlineLightbulbIcon />,
         label: "高亮",
         description: "高亮: :::success空格"
     },
-    {
-        id: 10,
+    file: {
+        key: "file",
         icon: <OutlinePaperclipIcon />,
         label: "上传文件",
         description: "上传文件"
     },
-    {
-        id: 11,
+    divider: {
+        key: "divider",
         icon: <IconFlipVertical />,
         label: "分割线",
         description: "分割线: ***"
     }
-]
+}
 
-export const defaultTooltipList: TooltipListProps[] = [
-    {
-        id: 1,
-        icon: <IconType />,
-        label: "正文",
-        description: "正文"
-    },
-    {
-        id: 2,
-        icon: <IconHeading1 />,
-        label: "一级标题",
-        description: "一级标题: #空格"
-    },
-    {
-        id: 3,
-        icon: <IconHeading2 />,
-        label: "二级标题",
-        description: "二级标题: ##空格"
-    },
-    {
-        id: 4,
-        icon: <IconHeading3 />,
-        label: "三级标题",
-        description: "三级标题: ###空格"
-    },
-    {
-        id: 5,
-        label: "divider"
-    },
-    {
-        id: 6,
-        icon: <IconListOrdered />,
-        label: "有序列表",
-        description: "有序列表: 1.空格"
-    },
-    {
-        id: 7,
-        icon: <IconList />,
-        label: "无序列表",
-        description: "无序列表: -空格或*空格"
-    },
-    {
-        id: 8,
-        icon: <IconCheckSquare />,
-        label: "任务",
-        description: "任务"
-    },
-    {
-        id: 9,
-        label: "divider"
-    },
-    {
-        id: 10,
-        icon: <IconCurlyBraces />,
-        label: "代码块",
-        description: "代码块:```空格"
-    },
-    {
-        id: 11,
-        icon: <IconQuote />,
-        label: "引用",
-        description: "引用: >空格"
-    }
-]
-//#region slash
-/**基础菜单 */
-const baseSlashList = [
-    {
-        id: "text",
-        label: "文本",
-        icon: <IconType />
-    },
-    {
-        id: "heading1",
-        label: "一级标题",
-        icon: <IconHeading1 />
-    },
-    {
-        id: "heading2",
-        label: "二级标题",
-        icon: <IconHeading2 />
-    },
-    {
-        id: "heading3",
-        label: "三级标题",
-        icon: <IconHeading3 />
-    },
-    {
-        id: "orderedList",
-        label: "有序列表",
-        icon: <IconListOrdered />
-    },
-    {
-        id: "unorderedList",
-        label: "无序列表",
-        icon: <IconList />
-    },
-    {
-        id: "codeBlock",
-        label: "代码块",
-        icon: <IconCurlyBraces />
-    },
-    {
-        id: "quote",
-        label: "引用",
-        icon: <IconQuote />
-    },
-    {
-        id: "divider",
-        label: "分割线",
-        icon: <IconFlipVertical />
-    }
-] as const
-/**常用菜单 */
-const commonList = [
-    {
-        id: "task",
-        label: "任务",
-        icon: <IconCheckSquare />
-    },
-    {
-        id: "highLight",
-        label: "高亮",
-        icon: <OutlineLightbulbIcon />
-    },
-    {
-        id: "file",
-        label: "文件",
-        icon: <OutlinePaperclipIcon />
-    }
-] as const
+const MilkdownMenuKey = Object.keys(MilkdownMenu)
 /**基础菜单中的key集合 */
-type BaseSlashType = (typeof baseSlashList)[number]["id"]
-/**常用菜单中的key集合 */
-type CommonSlashType = (typeof commonList)[number]["id"]
-export type SlashType = BaseSlashType | CommonSlashType
+export type MilkdownMenuType = keyof typeof MilkdownMenu
 
-const slashList = [...baseSlashList, ...commonList]
-export const SlashKeyEnum = Object.fromEntries(
-    slashList.map((item) => [item.id.charAt(0).toUpperCase() + item.id.slice(1), item.id])
+export const MilkdownMenuKeyEnum = Object.fromEntries(
+    MilkdownMenuKey.map((item) => [item.charAt(0).toUpperCase() + item.slice(1), item])
 ) as {
-    [K in SlashType as Capitalize<K>]: K
+    [K in MilkdownMenuType as Capitalize<K>]: K
 }
+/**
+ * 根据传的key生成对应的工具菜单，按key的顺序
+ * @param {MilkdownMenuType[]} keys
+ * */
+export const createMilkdownMenuListByKey = (keys: MilkdownMenuType[]) => {
+    return keys.map((ele) => ({
+        ...MilkdownMenu[ele],
+        key: ele
+    }))
+}
+//#region slash
 
-/** slash 线上菜单 */
-export const onlineSlashList = {
-    基础: baseSlashList,
-    常用: commonList
-}
+/**基础菜单 Key值 */
+export const baseSlashKey: MilkdownMenuType[] = [
+    MilkdownMenuKeyEnum.Text,
+    MilkdownMenuKeyEnum.Heading1,
+    MilkdownMenuKeyEnum.Heading2,
+    MilkdownMenuKeyEnum.Heading3,
+    MilkdownMenuKeyEnum.OrderedList,
+    MilkdownMenuKeyEnum.UnorderedList,
+    MilkdownMenuKeyEnum.CodeBlock,
+    MilkdownMenuKeyEnum.Quote,
+    MilkdownMenuKeyEnum.Divider
+]
+/**显示常用菜单 Key值 */
+export const onlineCommonSlashKey: MilkdownMenuType[] = [
+    MilkdownMenuKeyEnum.Task,
+    MilkdownMenuKeyEnum.HighLight,
+    MilkdownMenuKeyEnum.File
+]
 
-/** slash 本地菜单 */
-export const localSlashList = {
-    基础: baseSlashList,
-    常用: commonList.filter((ele) => !["file"].includes(ele.id))
-}
+/**本地常用菜单 Key值 */
+export const localCommonSlashKey: MilkdownMenuType[] = [MilkdownMenuKeyEnum.Task, MilkdownMenuKeyEnum.HighLight]
+
+//#endregion
+
+//#region Block
+export const onlineBlockKey: MilkdownMenuType[] = [
+    MilkdownMenuKeyEnum.Heading1,
+    MilkdownMenuKeyEnum.Heading2,
+    MilkdownMenuKeyEnum.Heading3,
+    MilkdownMenuKeyEnum.OrderedList,
+    MilkdownMenuKeyEnum.UnorderedList,
+    MilkdownMenuKeyEnum.Task,
+    MilkdownMenuKeyEnum.CodeBlock,
+    MilkdownMenuKeyEnum.Quote,
+    MilkdownMenuKeyEnum.HighLight,
+    MilkdownMenuKeyEnum.File,
+    MilkdownMenuKeyEnum.Divider
+]
+export const localBlockKey: MilkdownMenuType[] = [
+    MilkdownMenuKeyEnum.Heading1,
+    MilkdownMenuKeyEnum.Heading2,
+    MilkdownMenuKeyEnum.Heading3,
+    MilkdownMenuKeyEnum.OrderedList,
+    MilkdownMenuKeyEnum.UnorderedList,
+    MilkdownMenuKeyEnum.Task,
+    MilkdownMenuKeyEnum.CodeBlock,
+    MilkdownMenuKeyEnum.Quote,
+    MilkdownMenuKeyEnum.HighLight,
+    MilkdownMenuKeyEnum.Divider
+]
+//#endregion
+//#region Tooltip
+export const tooltipKey: (MilkdownMenuType | "key-divider")[] = [
+    MilkdownMenuKeyEnum.Text,
+    MilkdownMenuKeyEnum.Heading1,
+    MilkdownMenuKeyEnum.Heading2,
+    MilkdownMenuKeyEnum.Heading3,
+    "key-divider",
+    MilkdownMenuKeyEnum.OrderedList,
+    MilkdownMenuKeyEnum.UnorderedList,
+    MilkdownMenuKeyEnum.Task,
+    "key-divider",
+    MilkdownMenuKeyEnum.CodeBlock,
+    MilkdownMenuKeyEnum.Quote
+]
 //#endregion
