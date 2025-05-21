8a1f8bbbe1858f03a268f627d27997c638012730
luoluo <1425735414@qq.com>
2025-05-21 10:41:12 +0800

    feat:slash菜单增加功能

--- app/renderer/src/main/src/components/MilkdownEditor/Block/Block.tsx
+++ app/renderer/src/main/src/components/MilkdownEditor/Block/Block.tsx
@@ -2,16 +2,7 @@ import {BlockProvider} from "@milkdown/kit/plugin/block"
 import {useInstance} from "@milkdown/react"
 import {useCallback, useEffect, useRef, useState} from "react"
 import {usePluginViewContext} from "@prosemirror-adapter/react"
-import {
-    blockquoteSchema,
-    bulletListSchema,
-    codeBlockSchema,
-    headingSchema,
-    hrSchema,
-    listItemSchema,
-    orderedListSchema,
-    paragraphSchema
-} from "@milkdown/kit/preset/commonmark"
+import {paragraphSchema} from "@milkdown/kit/preset/commonmark"
 import {TextSelection} from "@milkdown/kit/prose/state"
 import styles from "./Block.module.scss"
 import {YakitButton} from "@/components/yakitUI/YakitButton/YakitButton"
@@ -20,27 +11,28 @@ import {YakitPopover} from "@/components/yakitUI/YakitPopover/YakitPopover"
 
 import {useMemoizedFn} from "ahooks"
 import {Ctx} from "@milkdown/kit/ctx"
-import {alterCustomSchema} from "../utils/alertPlugin"
-import {clearContentAndAddBlockType, clearContentAndSetBlockType, clearContentAndWrapInBlockType} from "../utils/utils"
-import {fileCommand} from "../utils/uploadPlugin"
-import {callCommand} from "@milkdown/kit/utils"
-import {insertImageBlockCommand} from "../utils/imageBlock"
+import {
+    createBlankHeading1,
+    createBlankHeading2,
+    createBlankHeading3,
+    createBlankOrderedList,
+    createBlankUnorderedList,
+    createBlankTask,
+    createBlankCodeBlock,
+    createBlankQuote,
+    createBlankHighLight,
+    uploadFileInMilkdown,
+    createDivider
+} from "../utils/utils"
 import {Tooltip} from "antd"
 import {defaultBlockList} from "../constants"
 import {cloneDeep} from "lodash"
 import {BlockListProps} from "../MilkdownEditorType"
-import {HttpUploadImgBaseRequest, httpUploadImgPath} from "@/apiUtils/http"
-import {yakitNotify} from "@/utils/notification"
-import {getLocalFileLinkInfo} from "../CustomFile/utils"
-import {ImgMaxSize} from "@/pages/pluginEditor/pluginImageTextarea/PluginImageTextarea"
+import {HttpUploadImgBaseRequest} from "@/apiUtils/http"
 import {useStore} from "@/store"
 import {InitEditorHooksLocalProps} from "../utils/initEditor"
 
 const {ipcRenderer} = window.require("electron")
-
-const imgTypes = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".tif", ".webp", ".svg"]
-
-const FileMaxSize = 1024 * 1024 * 1024
 
 interface BlockViewProps {
     type: HttpUploadImgBaseRequest["type"]
@@ -100,120 +92,47 @@ export const BlockView: React.FC<BlockViewProps> = (props) => {
     }, [])
 
     const onAddContent = useMemoizedFn(({label}) => {
-        const {dispatch, state} = view
         switch (label) {
             case "一级标题":
-                action((ctx) => {
-                    const command = clearContentAndSetBlockType(headingSchema.type(ctx), {level: 1})
-                    command(state, dispatch)
-                })
+                createBlankHeading1(action, view)
                 break
             case "二级标题":
-                action((ctx) => {
-                    const command = clearContentAndSetBlockType(headingSchema.type(ctx), {level: 2})
-                    command(state, dispatch)
-                })
+                createBlankHeading2(action, view)
                 break
 
             case "三级标题":
-                action((ctx) => {
-                    const command = clearContentAndSetBlockType(headingSchema.type(ctx), {level: 3})
-                    command(state, dispatch)
-                })
+                createBlankHeading3(action, view)
                 break
 
             case "有序列表":
-                action((ctx) => {
-                    const command = clearContentAndWrapInBlockType(orderedListSchema.type(ctx))
-                    command(state, dispatch)
-                })
+                createBlankOrderedList(action, view)
                 break
 
             case "无序列表":
-                action((ctx) => {
-                    const command = clearContentAndWrapInBlockType(bulletListSchema.type(ctx))
-                    command(state, dispatch)
-                })
+                createBlankUnorderedList(action, view)
                 break
             case "任务":
-                action((ctx) => {
-                    const command = clearContentAndWrapInBlockType(listItemSchema.type(ctx), {checked: false})
-                    command(state, dispatch)
-                })
+                createBlankTask(action, view)
                 break
             case "代码块":
-                action((ctx) => {
-                    const command = clearContentAndAddBlockType(codeBlockSchema.type(ctx))
-                    command(state, dispatch)
-                })
+                createBlankCodeBlock(action, view)
                 break
             case "引用":
-                action((ctx) => {
-                    const command = clearContentAndWrapInBlockType(blockquoteSchema.type(ctx))
-                    command(state, dispatch)
-                })
+                createBlankQuote(action, view)
                 break
             case "高亮":
-                action((ctx) => {
-                    const command = clearContentAndWrapInBlockType(alterCustomSchema.type(ctx))
-                    command(state, dispatch)
-                })
+                createBlankHighLight(action, view)
                 break
             case "上传文件":
-                ipcRenderer
-                    .invoke("openDialog", {
-                        title: "请选择文件",
-                        properties: ["openFile"]
-                    })
-                    .then((data: {filePaths: string[]}) => {
-                        const filesLength = data.filePaths.length
-                        if (filesLength) {
-                            const path = data.filePaths[0].replace(/\\/g, "\\")
-                            getLocalFileLinkInfo(path).then((res) => {
-                                if (res.size > FileMaxSize) {
-                                    yakitNotify("error", "文件大小不能超过1G")
-                                    return
-                                }
-                                const index = path.lastIndexOf(".")
-                                const fileType = path.substring(index, path.length)
-                                if (imgTypes.includes(fileType)) {
-                                    if (res.size > ImgMaxSize) {
-                                        yakitNotify("error", "图片大小不能超过1M")
-                                        return
-                                    }
-                                    httpUploadImgPath({path, type, filedHash: notepadHash})
-                                        .then((src) => {
-                                            action(
-                                                callCommand(insertImageBlockCommand.key, {
-                                                    src,
-                                                    alt: path,
-                                                    title: ""
-                                                })
-                                            )
-                                        })
-                                        .catch((e) => {
-                                            yakitNotify("error", `上传图片失败:${e}`)
-                                        })
-                                } else {
-                                    action(
-                                        callCommand(fileCommand.key, {
-                                            fileId: "0",
-                                            path,
-                                            notepadHash,
-                                            uploadUserId: userInfo.user_id
-                                        })
-                                    )
-                                }
-                            })
-                        }
-                    })
+                uploadFileInMilkdown(action, {
+                    type,
+                    notepadHash: notepadHash || "",
+                    userId: userInfo.user_id || 0
+                })
 
                 break
             case "分割线":
-                action((ctx) => {
-                    const command = clearContentAndAddBlockType(hrSchema.type(ctx))
-                    command(state, dispatch)
-                })
+                createDivider(action, view)
                 break
 
             default:
