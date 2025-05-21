6cf0556b1ab06da2a5345abb625aa66dba430d48
song_xiao_lin <song_xiao_lin@126.com>
2025-05-21 17:24:55 +0800

    del

--- app/renderer/src/main/src/pages/risks/YakitRiskTable/YakitRiskTable.tsx
+++ app/renderer/src/main/src/pages/risks/YakitRiskTable/YakitRiskTable.tsx
@@ -340,7 +340,6 @@ export const YakitRiskTable: React.FC<YakitRiskTableProps> = React.memo((props) 
         () => {
             // 初次不通过此处请求数据
             if (!isInitRequestRef.current) {
-                console.log(query);
                 onRefRiskList()
             }
         },
