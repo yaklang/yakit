1c33ab06169c3494f497aec0b5336b30b1d58fba
song_xiao_lin <song_xiao_lin@126.com>
2025-07-31 15:17:42 +0800

    pref: 定义

--- app/renderer/src/main/src/pages/fuzzer/MatcherAndExtractionCard/MatcherAndExtractionCardType.d.ts
+++ app/renderer/src/main/src/pages/fuzzer/MatcherAndExtractionCard/MatcherAndExtractionCardType.d.ts
@@ -24,7 +24,7 @@ export interface MatcherAndExtractionProps {
     defActiveKey: string
     defActiveType: MatchingAndExtraction
     defActiveKeyAndOrder: MatcherActiveKey
-    pageType: "webfuzzer" | "History_Analysis"
+    pageType: MatcherAndExtractionDrawerPageType
 }
 
 export interface MatcherAndExtractionRefProps {
