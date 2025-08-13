d627ef7aa5bb58e40f2ab98d65b1860f35bfacba
luoluo <1425735414@qq.com>
2025-08-13 15:13:25 +0800

    add import:YakitCombinationSearchType文件引入缺失的类型

--- app/renderer/src/main/src/components/YakitCombinationSearch/YakitCombinationSearchType.d.ts
+++ app/renderer/src/main/src/components/YakitCombinationSearch/YakitCombinationSearchType.d.ts
@@ -1,3 +1,4 @@
+import {YakitInputSearchProps} from "../yakitUI/YakitInput/YakitInputType"
 import {YakitSelectProps} from "../yakitUI/YakitSelect/YakitSelectType"
 interface OptionProps {
     label: string
