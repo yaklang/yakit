e0d7f2347a6f483f0a8c399b06118a197de1c9c0
luoluo <1425735414@qq.com>
2025-08-14 10:30:21 +0800

    feat:tags增加border和fullRadius控制;style:修复一些错误的颜色变量

--- app/renderer/src/main/src/components/yakitUI/YakitTag/YakitTagType.d.ts
+++ app/renderer/src/main/src/components/yakitUI/YakitTag/YakitTagType.d.ts
@@ -37,6 +37,10 @@ export interface YakitTagProps extends Omit<TagProps, "color"> {
     onAfterCopy?: (e: MouseEvent) => void
     copyText?: string
     iconColor?: string
+    /**是否全圆角 */
+    fullRadius?: boolean
+    /**是否有边框 */
+    border?: boolean
 }
 /**
  * @description: 复制文字
