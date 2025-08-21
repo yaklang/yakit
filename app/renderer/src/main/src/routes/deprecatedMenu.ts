2d7e94cbca75da6c3d4331b72144363d51926d69
song_xiao_lin <63901989+song-xiao-lin@users.noreply.github.com>
2025-08-21 15:58:58 +0800

    fix: 修复企业版菜单 (#2831)

--- app/renderer/src/main/src/routes/deprecatedMenu.ts
+++ app/renderer/src/main/src/routes/deprecatedMenu.ts
@@ -8,7 +8,7 @@ export const CommunityDeprecatedFirstMenu: string[] = []
  * @description 社区版
  * 废弃的二级菜单(桔子要求删除部分)
  * */
-export const CommunityDeprecatedSecondMenu: string[] = ["网站树", "插件商店", "我的插件", "本地插件"]
+export const CommunityDeprecatedSecondMenu: string[] = ["网站树", "插件商店", "我的插件", "本地插件", "MITM 交互式劫持 v2"]
 
 /**
  * @description 企业版
@@ -26,5 +26,6 @@ export const EnterpriseDeprecatedSecondMenu: string[] = [
     "网站树",
     "插件商店",
     "我的插件",
-    "本地插件"
+    "本地插件",
+    "MITM 交互式劫持 v2"
 ]
