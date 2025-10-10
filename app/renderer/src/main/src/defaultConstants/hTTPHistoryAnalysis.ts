df50fed4d23db8a2d0f5e3be5d1fabdbe2f01704
intSheep <intSheep@qq.com>
2025-10-10 14:16:07 +0800

    更新流量分析热加载模板

--- app/renderer/src/main/src/defaultConstants/hTTPHistoryAnalysis.ts
+++ app/renderer/src/main/src/defaultConstants/hTTPHistoryAnalysis.ts
@@ -7,13 +7,18 @@ export const defaultHTTPHistoryAnalysisPageInfo: HTTPHistoryAnalysisPageInfo = {
     verbose: "",
     pageId: ""
 }
-export const HotPatchDefaultContent = `analyzeHTTPFlow = func(flow /* *yakit.HTTPFlow */ , extract /*func(ruleName string, flow *yakit.HTTPFlow,contents ...string)*/) {
+export const HotPatchDefaultContent = `
+// f = file.OpenFile(tempFile, file.O_APPEND|file.O_CREATE, 0o777)~
+// m = sync.NewMutex() //因为analyzeHTTPFlow是并发的，所以需要加锁
+
+analyzeHTTPFlow = func(flow /* *yakit.HTTPFlow */ , extract /*func(ruleName string, flow *yakit.HTTPFlow,contents ...string)*/) {
     // flow 从数据库查询的流量数据
     // extract 将数据进行提取,ruleName做为提取流量的规则名，contents为提取的内容
-
+    
     // Example:
     // 在响应内容中搜索手机号
-    // respStr := string(flow.Response)
+    // 需要使用GetResponse()方法拿到响应体，直接使用flow.Response拿到的是被转移存在数据库的响应体。
+    // respStr := flow.GetResponse() 
     // // 使用正则匹配手机号
     // phoneRegex := re.MustCompile(\`\\b1[3-9]d{9}\\b\`)
     // phoneMatches := phoneRegex.FindAllString(respStr, -1)
@@ -23,8 +28,19 @@ export const HotPatchDefaultContent = `analyzeHTTPFlow = func(flow /* *yakit.HTT
     //     flow.Red()
     //     // 提取流量并标注发现的手机号数量
     //     extract("手机号提取", flow,sprintf("手机号%v",phoneMatches ))
+    //     // 提取的内容写入文件内
+    //     m.Lock()
+	//	   f.WriteLine(sprintf("手机号%v",phoneMatches ))
+	//	   m.Unlock()
     // }
 }
+
+// onAnalyzeHTTPFlowFinish将会在流量分析的时候被调用
+onAnalyzeHTTPFlowFinish = func(totalCount, matchedCount) {
+	// 释放文件句柄
+	// f.Close()
+}
+
 `
 
 export const footerTabs = (t: (text: string) => string) => {
