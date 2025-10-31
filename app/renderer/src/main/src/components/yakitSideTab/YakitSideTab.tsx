de1e0a7b2ccf24f2dbbf6b53eae9da758af510a9
luoluo <1425735414@qq.com>
2025-10-31 16:01:57 +0800

    feat:YakitSideTab新增类型

--- app/renderer/src/main/src/components/yakitSideTab/YakitSideTab.tsx
+++ app/renderer/src/main/src/components/yakitSideTab/YakitSideTab.tsx
@@ -1,7 +1,7 @@
-import React from "react"
+import React, {ReactNode} from "react"
 import {YakitSideTabProps, YakitTabsItemProps} from "./YakitSideTabType"
 import classNames from "classnames"
-import {useControllableValue, useMemoizedFn} from "ahooks"
+import {useControllableValue, useCreation, useMemoizedFn} from "ahooks"
 import styles from "./YakitSideTab.module.scss"
 
 export const YakitSideTab: React.FC<YakitSideTabProps> = React.memo((props) => {
@@ -43,38 +43,65 @@ export const YakitSideTab: React.FC<YakitSideTabProps> = React.memo((props) => {
         switch (type) {
             case "vertical":
                 return (
-                    <div className={styles["yakit-side-tab"]}>
-                        {yakitTabs.map((item) => (
-                            <YakitTabsItem
-                                key={item.value}
-                                item={item}
-                                onChange={onChange}
-                                className={classNames(styles["yakit-side-tab-item"], {
-                                    [styles["yakit-side-tab-item-active"]]: item.value === activeKey,
-                                    [styles["yakit-side-tab-item-show"]]:
-                                        item.value === activeKey && (item.show === false || !show)
-                                })}
-                                onTabPaneRender={onTabPaneRender}
-                            />
-                        ))}
-                    </div>
+                    <>
+                        <div className={styles["yakit-side-tab"]}>
+                            {yakitTabs.map((item) => (
+                                <YakitTabsItem
+                                    key={item.value}
+                                    item={item}
+                                    onChange={onChange}
+                                    className={classNames(styles["yakit-side-tab-item"], {
+                                        [styles["yakit-side-tab-item-active"]]: item.value === activeKey,
+                                        [styles["yakit-side-tab-item-show"]]:
+                                            item.value === activeKey && (item.show === false || !show)
+                                    })}
+                                    onTabPaneRender={onTabPaneRender}
+                                    rotate={"left"}
+                                />
+                            ))}
+                        </div>
+                        {children}
+                    </>
                 )
 
+            case "vertical-right":
+                return (
+                    <>
+                        {children}
+                        <div className={styles["yakit-side-tab"]}>
+                            {yakitTabs.map((item) => (
+                                <YakitTabsItem
+                                    key={item.value}
+                                    item={item}
+                                    onChange={onChange}
+                                    className={classNames(styles["yakit-side-tab-item"], {
+                                        [styles["yakit-side-tab-item-active"]]: item.value === activeKey
+                                    })}
+                                    onTabPaneRender={onTabPaneRender}
+                                    rotate={"right"}
+                                />
+                            ))}
+                        </div>
+                    </>
+                )
             case "horizontal":
                 return (
-                    <div className={styles["yakit-side-tab-horizontal"]}>
-                        {yakitTabs.map((item) => (
-                            <YakitTabsItem
-                                key={item.value}
-                                item={item}
-                                onChange={onChange}
-                                className={classNames(styles["yakit-side-tab-horizontal-item"], {
-                                    [styles["yakit-side-tab-horizontal-item-active"]]: item.value === activeKey
-                                })}
-                                onTabPaneRender={onTabPaneRender}
-                            />
-                        ))}
-                    </div>
+                    <>
+                        <div className={styles["yakit-side-tab-horizontal"]}>
+                            {yakitTabs.map((item) => (
+                                <YakitTabsItem
+                                    key={item.value}
+                                    item={item}
+                                    onChange={onChange}
+                                    className={classNames(styles["yakit-side-tab-horizontal-item"], {
+                                        [styles["yakit-side-tab-horizontal-item-active"]]: item.value === activeKey
+                                    })}
+                                    onTabPaneRender={onTabPaneRender}
+                                />
+                            ))}
+                        </div>
+                        {children}
+                    </>
                 )
             default:
                 return <div>未知type</div>
@@ -85,27 +112,42 @@ export const YakitSideTab: React.FC<YakitSideTabProps> = React.memo((props) => {
             className={classNames(
                 styles["tab-wrap"],
                 {
-                    [styles["tab-wrap-vertical"]]: type === "vertical"
+                    [styles["tab-wrap-vertical"]]: type === "vertical",
+                    [styles["tab-wrap-vertical-right"]]: type === "vertical-right"
                 },
                 className
             )}
         >
             {renderContent()}
-            {children}
         </div>
     )
 })
 
 const YakitTabsItem: React.FC<YakitTabsItemProps> = React.memo((props) => {
-    const {item, onChange, className = "", onTabPaneRender} = props
+    const {item, onChange, className = "", onTabPaneRender, rotate} = props
+    const node: ReactNode[] = useCreation(() => {
+        return [
+            <span
+                key={item.value}
+                className={classNames({
+                    [styles["item-text-left"]]: rotate === "left",
+                    [styles["item-text-right"]]: rotate === "right"
+                })}
+            >
+                {item.label}
+            </span>,
+            item.icon
+        ]
+    }, [item.label, item.icon, rotate])
+    const [label, icon] = node
     return (
         <div key={item.value} className={className} onClick={() => onChange(item)}>
             {onTabPaneRender ? (
-                onTabPaneRender(item)
+                onTabPaneRender(item, node)
             ) : (
                 <>
-                    <span className={styles["item-text"]}>{item.label}</span>
-                    {item.icon}
+                    {icon}
+                    {label}
                 </>
             )}
         </div>
