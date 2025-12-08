2f9b0b5e14071516b7b2bd7f684aed833f395f97
Youngster_yj <907985037@qq.com>
2025-12-08 17:08:33 +0800

    fix: markdown如若崩溃则显示其要展示的内容

--- app/renderer/src/main/src/pages/assetViewer/reportRenders/markdownRender.tsx
+++ app/renderer/src/main/src/pages/assetViewer/reportRenders/markdownRender.tsx
@@ -5,6 +5,7 @@ import MDEditor from "@uiw/react-md-editor"
 import styles from "./markdownRender.module.scss"
 import {useTheme} from "@/hook/useTheme"
 import classNames from "classnames"
+import {ErrorBoundary} from "react-error-boundary"
 const {Markdown} = MDEditor
 
 export interface ReportMarkdownBlockProp {
@@ -15,10 +16,16 @@ export interface ReportMarkdownBlockProp {
 export const ReportMarkdownBlock: React.FC<ReportMarkdownBlockProp> = (props) => {
     const {theme} = useTheme()
     return (
-        <Markdown
-            source={props.item.content}
-            className={classNames(styles["markdown-block"], props.className)}
-            warpperElement={{"data-color-mode": theme}}
-        />
+        <ErrorBoundary
+            FallbackComponent={() => {
+                return <div>{props.item.content}</div>
+            }}
+        >
+            <Markdown
+                source={props.item.content}
+                className={classNames(styles["markdown-block"], props.className)}
+                warpperElement={{"data-color-mode": theme}}
+            />
+        </ErrorBoundary>
     )
 }
