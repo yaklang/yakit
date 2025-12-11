53b2e631205a576a75211bbc835baa8609916f7c
HJH <75428400+hjhke@users.noreply.github.com>
2025-12-11 11:26:59 +0800

    feat: MITM发包到webfuzzer的时候同步配置 流量分析器样式bug修复

--- app/renderer/src/main/src/components/HTTPFlowTable/HTTPFlowTable.tsx
+++ app/renderer/src/main/src/components/HTTPFlowTable/HTTPFlowTable.tsx
@@ -4959,6 +4959,13 @@ export const RangeInputNumberTable: React.FC<RangeInputNumberProps> = React.memo
 
 // 发送web fuzzer const
 export const onSendToTab = async (rowData, openFlag?: boolean, downstreamProxyStr?: string) => {
+    const stateSecretHijacking = await getRemoteValue(MITMConsts.MITMDefaultEnableGMTLS)
+    let params = {}
+    if (["enableGMTLS","1"].includes(stateSecretHijacking)) {
+        Object.assign(params, {enableGMTLS: true})
+    }else if(stateSecretHijacking === "randomJA3"){
+        Object.assign(params, {randomJA3: true})
+    }
     ipcRenderer
         .invoke("send-to-tab", {
             type: "fuzzer",
@@ -4966,6 +4973,7 @@ export const onSendToTab = async (rowData, openFlag?: boolean, downstreamProxySt
                 openFlag,
                 isHttps: rowData.IsHTTPS,
                 downstreamProxyStr,
+                ...params,
                 request: rowData.InvalidForUTF8Request
                     ? rowData.SafeHTTPRequest!
                     : new Buffer(rowData.Request).toString("utf8")
