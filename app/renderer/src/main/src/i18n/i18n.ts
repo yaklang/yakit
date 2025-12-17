363712d9148f43538f9e7bcfa3788dbaf0ef9f18
luoluo <1425735414@qq.com>
2025-12-17 17:58:53 +0800

    fix:i18n中预加载增加aiagent

--- app/renderer/src/main/src/i18n/i18n.ts
+++ app/renderer/src/main/src/i18n/i18n.ts
@@ -7,7 +7,7 @@ i18n.use(HttpBackend)
     .init({
         lng: "zh",
         fallbackLng: "en",
-        ns: ["yakitUi", "yakitRoute", "layout", "customizeMenu", "home", "history", "webFuzzer"], // 这几个需要预加载
+        ns: ["yakitUi", "yakitRoute", "layout", "customizeMenu", "home", "history", "webFuzzer", "aiAgent"], // 这几个需要预加载
         defaultNS: "",
         interpolation: {
             escapeValue: false
