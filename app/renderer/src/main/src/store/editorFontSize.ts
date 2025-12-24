457d4c548a2ff7dcb7c01c2d1ac76e59385bdf70
HJH <75428400+hjhke@users.noreply.github.com>
2025-12-24 16:44:16 +0800

    feat: 编辑器字体大小配置新增输入fontsize 编辑器字体大小配置统一

---
+++ app/renderer/src/main/src/store/editorFontSize.ts
@@ -0,0 +1,36 @@
+import { create } from "zustand"
+import { getRemoteValue, setRemoteValue } from "@/utils/kv"
+import { HTTP_PACKET_EDITOR_FONT_SIZE } from "@/utils/editors"
+
+
+interface EditorFontSizeStore {
+    /**@name 编辑器全局字体大小 */
+    fontSize: number
+    /**@name 设置字体大小 */
+    setFontSize: (size: number) => void
+    /**@name 初始化字体大小(从缓存读取) */
+    initFontSize: () => Promise<void>
+}
+
+export const fontSizeOptions = [12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+export const useEditorFontSize = create<EditorFontSizeStore>((set, get) => ({
+    fontSize: 12,
+    setFontSize: (fontSize) => {
+        set({ fontSize })
+        setRemoteValue(HTTP_PACKET_EDITOR_FONT_SIZE, JSON.stringify({ fontSize }))
+    },
+    initFontSize: async () => {
+        try {
+            const data = await getRemoteValue(HTTP_PACKET_EDITOR_FONT_SIZE)
+            if (data) {
+                const obj = JSON.parse(data)
+                if (obj?.fontSize && obj.fontSize >= 12 && obj.fontSize <= 20) {
+                    set({ fontSize: obj.fontSize })
+                }
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}))
