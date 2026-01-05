2294d4122ef88797e004480cc9155b0b2ba08969
whale <hale@wdeMac-mini.local>
2026-01-05 17:40:06 +0800

    wip" 1

--- app/renderer/src/main/src/components/playground/Vitest__Test__/index.tsx
+++ app/renderer/src/main/src/components/playground/Vitest__Test__/index.tsx
@@ -41,10 +41,10 @@ const Vitest__Test__: FC<VitestTestProps> = (props) => {
                 .finally(() => {
                     setTypeLoading(false)
                 })
+        } else {
+            setTypeLoading(true)
         }
-        // else {
-        //     setTypeLoading(false)
-        // }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [editorOperationRecord])
 
     return (
