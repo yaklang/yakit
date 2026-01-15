ee433cf7a0798fa8c6125aeac2b627bb125e7827
luoluo <52566643+luoluoTH@users.noreply.github.com>
2026-01-15 15:08:44 +0800

    style:图标属性修改

--- app/renderer/src/main/src/pages/ai-agent/aiModelList/icon.tsx
+++ app/renderer/src/main/src/pages/ai-agent/aiModelList/icon.tsx
@@ -13,7 +13,7 @@ const Memfit = () => {
     const id1 = uuidv4()
     return (
         <svg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'>
-            <g clip-path={`url(#${id})`}>
+            <g clipPath={`url(#${id})`}>
                 <path
                     d='M16 32C24.8366 32 32 24.8366 32 16C32 7.16344 24.8366 0 16 0C7.16344 0 0 7.16344 0 16C0 24.8366 7.16344 32 16 32Z'
                     fill='#4373BB'
