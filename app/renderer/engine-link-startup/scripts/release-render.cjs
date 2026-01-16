3ccb3ea84016317b96d7e7aad61d51d91dab69eb
nonight <378099757@qq.com>
2026-01-16 14:35:32 +0800

    修改render生成tag脚本，将基础版号进行更新 (#3280)

--- app/renderer/engine-link-startup/scripts/release-render.cjs
+++ app/renderer/engine-link-startup/scripts/release-render.cjs
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 const {execSync} = require("child_process")
 
+/**
+ * 生成的 tag 中, 第一位数字版号, 2025年时为1, 按序计算, 2026年则为2
+ * 注: 需要开发者根据每年定时将第一位数字版号进行修改, 预防不同年下, 后两位版号相同导致的 tag 冲突
+ */
+
 try {
-    // 用户手动输入的版本号 例: 1.04.30
+    // 用户手动输入的版本号 例: 2.04.30
     const inputVersion = process.argv.slice(2)[0]
 
     const now = new Date()
@@ -11,7 +16,7 @@ try {
     // 日期直接获取
     const day = String(now.getDate()).padStart(2, "0")
 
-    const version = inputVersion || `1.${month}.${day}`
+    const version = inputVersion || `2.${month}.${day}`
 
     if (version.startsWith("v")) {
         console.error("Version number cannot start with 'v'")
@@ -22,6 +27,8 @@ try {
     execSync(`git fetch origin`, {stdio: "inherit"})
     execSync(`git checkout -b update/link-render-${version} origin/master`, {stdio: "inherit"})
 
+    console.log("\x1b[32m%s\x1b[0m", `### Creating render branch: update/link-render-${version} ###`)
+
     // 生成版本号修改
     execSync(`yarn version --new-version ${version} --no-git-tag-version`, {stdio: "inherit"})
     // 将 package.json 的修改加入暂存区
@@ -30,6 +37,8 @@ try {
     execSync(`git commit -m "Link-Render Version To v${version}"`, {stdio: "inherit"})
     // 生成 tag，格式为 v<version>-render
     execSync(`git tag v${version}-link-render`, {stdio: "inherit"})
+
+    console.log("\x1b[32m%s\x1b[0m", `### Creating render tag: v${version}-link-render ###`)
 } catch (err) {
     console.error("生成 link-render 版本号命令失败:", err)
     process.exit(1)
--- app/renderer/src/main/scripts/release-render.js
+++ app/renderer/src/main/scripts/release-render.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 const {execSync} = require("child_process")
 
+/**
+ * 生成的 tag 中, 第一位数字版号, 2025年时为1, 按序计算, 2026年则为2
+ * 注: 需要开发者根据每年定时将第一位数字版号进行修改, 预防不同年下, 后两位版号相同导致的 tag 冲突
+ */
+
 try {
-    // 用户手动输入的版本号 例: 1.04.30
+    // 用户手动输入的版本号 例: 2.04.30
     const inputVersion = process.argv.slice(2)[0]
 
     const now = new Date()
@@ -11,7 +16,7 @@ try {
     // 日期直接获取
     const day = String(now.getDate()).padStart(2, "0")
 
-    const version = inputVersion || `1.${month}.${day}`
+    const version = inputVersion || `2.${month}.${day}`
 
     if (version.startsWith("v")) {
         console.error("Version number cannot start with 'v'")
@@ -23,6 +28,8 @@ try {
     // 生成更新版本号分支
     execSync(`git checkout -b update/render-${version} origin/master`, {stdio: "inherit"})
 
+    console.log("\x1b[32m%s\x1b[0m", `### Creating render branch: update/render-${version} ###`)
+
     // 生成版本号修改
     execSync(`yarn version --new-version ${version} --no-git-tag-version`, {stdio: "inherit"})
     // 将 package.json 的修改加入暂存区
@@ -31,6 +38,8 @@ try {
     execSync(`git commit -m "Render Version To v${version}"`, {stdio: "inherit"})
     // 生成 tag，格式为 v<version>-render
     execSync(`git tag v${version}-render`, {stdio: "inherit"})
+
+    console.log("\x1b[32m%s\x1b[0m", `### Creating render tag: v${version}-render ###`)
 } catch (err) {
     console.error("生成 render 版本号命令失败:", err)
     process.exit(1)
