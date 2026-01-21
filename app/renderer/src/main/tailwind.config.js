43a90feafe7aec54f95c653476752058f7d88a88
Youngster_yj <907985037@qq.com>
2026-01-21 21:48:47 +0800

    feat: 接入streamdown
    feat: StreamMarkdown完善及SafeMarkdown接入
    feat: streamdown接入完成，提测

---
+++ app/renderer/src/main/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,tsx}",
+    "./node_modules/streamdown/dist/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
