34e7d5fe6ddac2d73389662bfe0c33cc5a098775
song_xiao_lin <63901989+song-xiao-lin@users.noreply.github.com>
2026-01-23 17:22:05 +0800

    Song/fix/xterm color (#3331)
    * fix: 终端颜色
    
    * 颜色替换

--- app/renderer/src/main/src/hook/useXTermOptions/useXTermOptions.ts
+++ app/renderer/src/main/src/hook/useXTermOptions/useXTermOptions.ts
@@ -6,25 +6,55 @@ import {defaultTerminalOptions} from "@/components/yakitUI/YakitXterm/YakitXterm
 export const getXtermTheme = () => {
     const vars = getAllYakitColorVars()
     return {
-        foreground: vars["--Colors-Use-Neutral-Text-2-Primary"],
-        background: vars["--Colors-Use-Neutral-Bg-Hover"],
-        cursor: vars["--Colors-Use-Neutral-Text-2-Primary"],
-        black: vars["--Colors-Use-Basic-Black"],
-        brightBlack: vars["--yakit-colors-Neutral-100"],
-        red: vars["--yakit-colors-Error-80"],
-        brightRed: vars["--yakit-colors-Error-60"],
+        foreground: vars["--Colors-Use-Neutral-Text-1-Title"],
+        background: vars["--Colors-Use-Neutral-Bg"],
+
+        cursor: vars["--Colors-Use-Main-Primary"],
+        cursorAccent: vars["--Colors-Use-Neutral-Bg"],
+
+        selectionBackground: vars["--Colors-Use-Main-Focus"],
+        selectionForeground: vars["--Colors-Use-Neutral-Text-1-Title"],
+        selectionInactiveBackground: vars["--Colors-Use-Neutral-Disable"],
+
+        black: vars["--Colors-Use-Neutral-Bg"],
+        red: vars["--Colors-Use-Error-Primary"],
         green: vars["--yakit-colors-Green-80"],
-        brightGreen: vars["--yakit-colors-Green-60"],
-        yellow: vars["--yakit-colors-Yellow-80"],
-        brightYellow: vars["--yakit-colors-Yellow-60"],
+        yellow: vars["--yakit-colors-Orange-80"],
         blue: vars["--yakit-colors-Blue-80"],
-        brightBlue: vars["--yakit-colors-Blue-60"],
         magenta: vars["--yakit-colors-Magenta-80"],
-        brightMagenta: vars["--yakit-colors-Magenta-60"],
-        cyan: vars["--yakit-colors-Cyan-80"],
-        brightCyan: vars["--yakit-colors-Cyan-60"],
-        white: vars["--yakit-colors-Orange-50"],
-        brightWhite: vars["--yakit-colors-Neutral-0"]
+        cyan: vars["--yakit-colors-Lake-blue-80"],
+        white: vars["--Colors-Use-Neutral-Text-1-Title"],
+
+        brightBlack: vars["--Colors-Use-Neutral-Disable"],
+        brightRed: vars["--yakit-colors-Error-80"],
+        brightGreen: vars["--yakit-colors-Green-100"],
+        brightYellow: vars["--yakit-colors-Orange-100"],
+        brightBlue: vars["--yakit-colors-Blue-100"],
+        brightMagenta: vars["--yakit-colors-Magenta-100"],
+        brightCyan: vars["--yakit-colors-Lake-blue-100"],
+        brightWhite: vars["--Colors-Use-Neutral-Text-1-Title"],
+
+        extendedAnsi: [
+            vars["--Colors-Use-Neutral-Bg"],
+            vars["--Colors-Use-Neutral-Bg-Hover"],
+            vars["--Colors-Use-Neutral-Disable"],
+            vars["--Colors-Use-Neutral-Border"],
+
+            vars["--yakit-colors-Blue-80"],
+            vars["--yakit-colors-Green-80"],
+            vars["--yakit-colors-Orange-80"],
+            vars["--yakit-colors-Error-80"],
+
+            vars["--yakit-colors-Blue-100"],
+            vars["--yakit-colors-Magenta-80"],
+            vars["--yakit-colors-Lake-blue-80"],
+            vars["--yakit-colors-Green-100"],
+
+            vars["--yakit-colors-Orange-100"],
+            vars["--yakit-colors-Error-100"],
+            vars["--Colors-Use-Blue-Primary"],
+            vars["--Colors-Use-Main-Primary"]
+        ]
     }
 }
 interface UseXTermOptionsParams {
