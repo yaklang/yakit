37726c8add42abfbb2674c5b1ef93ca88f9c3f2e
luoluo <1425735414@qq.com>
2026-01-27 14:15:31 +0800

    feat:记忆库的UI和路由搭建

---
+++ app/renderer/src/main/src/pages/memoryBase/type.ts
@@ -0,0 +1,14 @@
+import {QSInputTextareaProps} from "../ai-agent/template/type"
+
+export interface MemoryBaseProps {
+    pageId: string
+}
+
+export interface MemoryQueryProps {
+}
+
+export interface AITextareaProps {
+    textProps?: QSInputTextareaProps
+}
+
+export interface MemoryTableProps {}
