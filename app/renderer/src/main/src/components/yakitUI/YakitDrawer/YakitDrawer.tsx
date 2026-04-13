175b77da5862883685bf575bd3c1eaab4b94d641
HJH <75428400+hjhke@users.noreply.github.com>
2026-04-13 14:53:01 +0800

    fix: yakitDrawer销毁时把可拖拽设置成true( return () => emiter.emit('setYakitHeaderDraggable', true) )

--- app/renderer/src/main/src/components/yakitUI/YakitDrawer/YakitDrawer.tsx
+++ app/renderer/src/main/src/components/yakitUI/YakitDrawer/YakitDrawer.tsx
@@ -1,119 +1,124 @@
-import {Drawer} from "antd"
-import React, {useEffect, useState} from "react"
-import {YakitDrawerProps} from "./YakitDrawerType"
-import styles from "./YakitDrawer.module.scss"
-import classNames from "classnames"
-import {RemoveIcon} from "@/assets/newIcon"
-import {ShowDrawerProps} from "@/utils/showModal"
-import {ErrorBoundary} from "react-error-boundary"
-import {createRoot} from "react-dom/client"
-import emiter from "@/utils/eventBus/eventBus"
-import {DndProvider} from "react-dnd"
-import {HTML5Backend} from "react-dnd-html5-backend"
+import { Drawer } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { YakitDrawerProps } from './YakitDrawerType'
+import styles from './YakitDrawer.module.scss'
+import classNames from 'classnames'
+import { RemoveIcon } from '@/assets/newIcon'
+import { ShowDrawerProps } from '@/utils/showModal'
+import { ErrorBoundary } from 'react-error-boundary'
+import { createRoot } from 'react-dom/client'
+import emiter from '@/utils/eventBus/eventBus'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 /**
  * @description:YakitDrawer  抽屉 placement === "bottom" heard有背景色
  * @augments DrawerProps 继承antd的 DrawerProps 默认属性
  */
 export const YakitDrawer: React.FC<YakitDrawerProps> = (props) => {
-    const {visible, ...restProps} = props
-    useEffect(() => {
-        if (visible) {
-            emiter.emit("setYakitHeaderDraggable", false)
-        } else {
-            emiter.emit("setYakitHeaderDraggable", true)
-        }
-    }, [visible])
-    return (
-        <Drawer
-            visible={visible}
-            {...restProps}
-            closeIcon={<div className={styles["yakit-drawer-icon"]}>{props.closeIcon || <RemoveIcon className={styles["yakit-drawer-remove-icon"]}/>}</div>}
-            className={classNames(
-                styles["yakit-drawer"],
-                {[styles["yakit-drawer-bottom"]]: props.placement === "bottom"},
-                props.className
-            )}
-        >
-            {props.children}
-        </Drawer>
-    )
+  const { visible, ...restProps } = props
+  useEffect(() => {
+    if (visible) {
+      emiter.emit('setYakitHeaderDraggable', false)
+    } else {
+      emiter.emit('setYakitHeaderDraggable', true)
+    }
+    return () => emiter.emit('setYakitHeaderDraggable', true)
+  }, [visible])
+  return (
+    <Drawer
+      visible={visible}
+      {...restProps}
+      closeIcon={
+        <div className={styles['yakit-drawer-icon']}>
+          {props.closeIcon || <RemoveIcon className={styles['yakit-drawer-remove-icon']} />}
+        </div>
+      }
+      className={classNames(
+        styles['yakit-drawer'],
+        { [styles['yakit-drawer-bottom']]: props.placement === 'bottom' },
+        props.className,
+      )}
+    >
+      {props.children}
+    </Drawer>
+  )
 }
 
 const YakitBaseDrawer: React.FC<ShowDrawerProps> = (props) => {
-    const {onVisibleSetter,...resProps}=props
-    const [visible, setVisible] = useState<boolean>(true)
+  const { onVisibleSetter, ...resProps } = props
+  const [visible, setVisible] = useState<boolean>(true)
 
-    useEffect(() => {
-        if (visible && onVisibleSetter) {
-            onVisibleSetter(setVisible)
-        }
-    }, [visible])
+  useEffect(() => {
+    if (visible && onVisibleSetter) {
+      onVisibleSetter(setVisible)
+    }
+  }, [visible])
 
-    return (
-        <YakitDrawer
-            onClose={(e) => {
-                if (props.onCancel) props.onCancel(e)
-                setVisible(false)
-            }}
-            visible={visible}
-            closable={true}
-            destroyOnClose={true}
-            {...resProps}
-        />
-    )
+  return (
+    <YakitDrawer
+      onClose={(e) => {
+        if (props.onCancel) props.onCancel(e)
+        setVisible(false)
+      }}
+      visible={visible}
+      closable={true}
+      destroyOnClose={true}
+      {...resProps}
+    />
+  )
 }
 
 export const showYakitDrawer = (props: ShowDrawerProps) => {
-    const div = document.createElement("div")
-    document.body.appendChild(div)
+  const div = document.createElement('div')
+  document.body.appendChild(div)
 
-    let setter: (r: boolean) => any = () => {}
-    let yakitDrawerRootDiv
-    const render = (targetConfig: ShowDrawerProps) => {
-        setTimeout(() => {
-            if (!yakitDrawerRootDiv) {
-                yakitDrawerRootDiv = createRoot(div)
-            }
-            yakitDrawerRootDiv.render(
-                <DndProvider backend={HTML5Backend}>
-                    <YakitBaseDrawer
-                        {...(targetConfig as YakitDrawerProps)}
-                        onVisibleSetter={(r) => {
-                            setter = r
-                        }}
-                    >
-                        <ErrorBoundary
-                            FallbackComponent={({error, resetErrorBoundary}) => {
-                                if (!error) {
-                                    return <div>未知错误</div>
-                                }
-                                return (
-                                    <div>
-                                        <p>弹框内逻辑性崩溃，请关闭重试！</p>
-                                        <pre>{error?.message}</pre>
-                                    </div>
-                                )
-                            }}
-                        >
-                            {targetConfig.content}
-                        </ErrorBoundary>
-                    </YakitBaseDrawer>
-                </DndProvider>
-            )
-        })
-    }
-    render(props)
-    return {
-        destroy: () => {
-            if (setter) {
-                setter(false)
-            }
-            setTimeout(() => {
-                if (yakitDrawerRootDiv) {
-                    yakitDrawerRootDiv.unmount()
+  let setter: (r: boolean) => any = () => {}
+  let yakitDrawerRootDiv
+  const render = (targetConfig: ShowDrawerProps) => {
+    setTimeout(() => {
+      if (!yakitDrawerRootDiv) {
+        yakitDrawerRootDiv = createRoot(div)
+      }
+      yakitDrawerRootDiv.render(
+        <DndProvider backend={HTML5Backend}>
+          <YakitBaseDrawer
+            {...(targetConfig as YakitDrawerProps)}
+            onVisibleSetter={(r) => {
+              setter = r
+            }}
+          >
+            <ErrorBoundary
+              FallbackComponent={({ error, resetErrorBoundary }) => {
+                if (!error) {
+                  return <div>未知错误</div>
                 }
-            }, 400)
+                return (
+                  <div>
+                    <p>弹框内逻辑性崩溃，请关闭重试！</p>
+                    <pre>{error?.message}</pre>
+                  </div>
+                )
+              }}
+            >
+              {targetConfig.content}
+            </ErrorBoundary>
+          </YakitBaseDrawer>
+        </DndProvider>,
+      )
+    })
+  }
+  render(props)
+  return {
+    destroy: () => {
+      if (setter) {
+        setter(false)
+      }
+      setTimeout(() => {
+        if (yakitDrawerRootDiv) {
+          yakitDrawerRootDiv.unmount()
         }
-    }
+      }, 400)
+    },
+  }
 }
