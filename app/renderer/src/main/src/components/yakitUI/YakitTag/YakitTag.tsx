ac6b52c36fdb2e3c97a755f1a2943c8b79c9b9b3
luoluo <1425735414@qq.com>
2026-05-29 15:13:54 +0800

    feat:yakitTag增加新的color:main

--- app/renderer/src/main/src/components/yakitUI/YakitTag/YakitTag.tsx
+++ app/renderer/src/main/src/components/yakitUI/YakitTag/YakitTag.tsx
@@ -21,7 +21,7 @@ import { useI18nNamespaces } from '@/i18n/useI18nNamespaces'
  * @description: tag
  * @augments TagProps 继承antd的TagProps默认属性
  * @param {middle|large|small} size 默认middle 16 20 24
- * @param {"danger" | "info" | "success" | "warning"|"serious" |"yellow"| "purple" | "blue" | "cyan" | "bluePurple"} color 颜色
+ * @param {"main"|"danger" | "info" | "success" | "warning"|"serious" |"yellow"| "purple" | "blue" | "cyan" | "bluePurple"} color 颜色
  * @param {boolean} disable
  * @param {boolean} enableCopy 是否可复制
  * @param {e} onAfterCopy 复制后的回调
@@ -45,6 +45,7 @@ export const YakitTag: React.FC<YakitTagProps> = (props) => {
           [styles['yakit-tag-small']]: size === 'small',
           [styles['yakit-tag-large']]: size === 'large',
           [styles['yakit-tag-default-color']]: !color,
+          [styles['yakit-tag-main']]: color === 'main',
           [styles['yakit-tag-danger']]: color === 'danger',
           [styles['yakit-tag-info']]: color === 'info',
           [styles['yakit-tag-success']]: color === 'success' || color === 'green',
