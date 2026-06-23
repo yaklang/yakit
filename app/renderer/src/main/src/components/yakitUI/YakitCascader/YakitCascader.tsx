aa16d18a9e1f328a4fc17108000bd81cb3fd442f
songxiaolin <song_xiao_lin@126.com>
2026-06-23 17:57:56 +0800

    feat: Cascader组件调整

--- app/renderer/src/main/src/components/yakitUI/YakitCascader/YakitCascader.tsx
+++ app/renderer/src/main/src/components/yakitUI/YakitCascader/YakitCascader.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Cascader } from 'antd'
 import styles from './YakitCascader.module.scss'
+import classNames from 'classnames'
 
 type YakitCascaderProps = React.ComponentProps<typeof Cascader>
 
 const YakitCascader = (props: YakitCascaderProps) => {
   return (
     <div className={styles['yakit-cascader']}>
-      <Cascader {...props} dropdownClassName={styles['yakit-cascader-popup']} />
+      <Cascader
+        {...props}
+        classNames={{
+          popup: {
+            root: classNames(styles['yakit-cascader-popup'], props.classNames?.popup?.root),
+          },
+        }}
+      />
     </div>
   )
 }
