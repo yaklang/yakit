1b1a206e0e2905cd94efecbcc653778e27a7720c
Youngster_yj <907985037@qq.com>
2026-07-16 16:53:26 +0800

    feat: ccb共享桌面
    rename: 登录文案修改
    
    fix: 英文文案修改

--- app/renderer/engine-link-startup/src/pages/StartupPage/components/SoftwareBasics/index.tsx
+++ app/renderer/engine-link-startup/src/pages/StartupPage/components/SoftwareBasics/index.tsx
@@ -258,6 +258,7 @@ export const SoftwareBasics: React.FC<SoftwareBasicsProps> = React.memo((props) 
         </div>
         <div className={styles['softwareBasics-item-cont']}>
           <YakitDragger
+            disabled
             value={currentPath}
             isShowPathNumber={false}
             selectType="folder"
