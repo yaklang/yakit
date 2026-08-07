5b13a95ab8f971454b6949a45b72d3c85546cdae
hubo <hubo@4dogs.cn>
2026-08-07 17:17:24 +0800

    fix: 修改打包样式丢失

--- app/renderer/src/main/src/index.tsx
+++ app/renderer/src/main/src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom'
 /** 该样式必须放在APP组件的前面，因为里面有antd样式，放后面会把APP组件内的样式覆盖 */
+import 'antd/dist/antd.css'
 import './styles/index.css'
 import NewApp from './newApp/NewApp'
 import { HTML5Backend } from 'react-dnd-html5-backend'
