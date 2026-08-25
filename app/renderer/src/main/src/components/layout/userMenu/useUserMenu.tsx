8028f1f53b2c32a307c2aab81a5388820ae1f2b8
Youngster_yj <907985037@qq.com>
2026-08-25 16:37:54 +0800

    fix: 企业版不需要支付逻辑的获取

--- app/renderer/src/main/src/components/layout/userMenu/useUserMenu.tsx
+++ app/renderer/src/main/src/components/layout/userMenu/useUserMenu.tsx
@@ -517,10 +517,14 @@ export const useUserMenu = (params: UseUserMenuParams): UseUserMenuResult => {
   })
 
   const onUpdateApiKey = useMemoizedFn((isLoading: boolean = false) => {
+    // 仅ce社区版可以获取apiKey，企业版不需要
+    if (userInfo.platform === 'company') return
     apiFetchApiKeys(isLoading)
   })
 
   useEffect(() => {
+    // 仅ce社区版可以获取apiKey，企业版不需要
+    if (userInfo.platform === 'company') return
     if (userInfo.isLogin) {
       apiFetchApiKeys()
     } else {
